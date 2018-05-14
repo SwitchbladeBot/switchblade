f435c0183c5f26b07b7080394ed9a34a26e37cdf
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-05-14 15:09:26 -0300

    Add Avatar command

---
+++ src/commands/misc/avatar.js
@@ -0,0 +1,18 @@
+const { Command } = require('../../')
+
+module.exports = class Avatar extends Command {
+  constructor (client) {
+    super(client)
+    this.name = 'avatar'
+    this.aliases = ['profilepicture', 'pfp']
+  }
+
+  run (message) {
+    message.channel.startTyping()
+    const embed = this.client.getDefaultEmbed(message.author)
+    const user = message.mentions.users.first() || message.author
+    embed.setImage(user.displayAvatarURL).setDescription(user + "'s avatar")
+    message.channel.send({embed})
+    message.channel.stopTyping()
+  }
+}
