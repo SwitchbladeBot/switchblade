91ee4fedb7a5b616381f05df9f12afb2df4dee41
Doges <31023616+xDoges@users.noreply.github.com>
2018-05-14 19:39:06 -0300

    thonking

--- src/commands/memes/thinking.js
+++ src/commands/memes/thinking.js
@@ -10,13 +10,13 @@ module.exports = class Thinking extends Command {
 
   run (message) {
     message.channel.startTyping()
-    let thonkembed = this.client.getDefaultEmbed(message.author)
+    let embed = this.client.getDefaultEmbed(message.author)
     request.get('https://reddit.com/r/thinking/random/.json').then(data => {
-      let requested = data.body[0].data.children[0].data
-      thonkembed.setImage(requested.url)
-      thonkembed.setTitle(':thinking:')
-      thonkembed.setURL(`https://reddit.com${requested.permalink}`)
-      message.channel.send({thonkembed})
+      let req = data.body[0].data.children[0].data
+      embed.setImage(req.url)
+      embed.setTitle(':thinking:')
+      embed.setURL(`https://reddit.com${req.permalink}`)
+      message.channel.send({embed})
     })
     message.channel.stopTyping()
   }
