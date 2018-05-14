183a78a6c807f236d4fbd3a0ab4468e16dfc1341
Doges <31023616+xDoges@users.noreply.github.com>
2018-05-14 18:44:05 -0300

    Added dog command

---
+++ src/commands/misc/dog.js
@@ -0,0 +1,20 @@
+const { Command } = require('../../')
+const request = require('snekfetch')
+
+module.exports = class Dog extends Command {
+  constructor (client) {
+    super(client)
+    this.name = 'dog'
+    this.aliases = ['doggo', 'dogpics', 'randomdog']
+  }
+
+  run (message) {
+    message.channel.startTyping()
+    request.get('https://random.dog/woof.json').then(r => {
+      const embed = this.client.getDefaultEmbed(message.author)
+      embed.setImage(r.body.url).setDescription('Here is your dog <:DoggoF:445701839564963840>')
+      message.channel.send({embed})
+    })
+    message.channel.stopTyping()
+  }
+}
