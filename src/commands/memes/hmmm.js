afb403253767298e8a6c021f46d71bb2ba07f782
PillGP <pillgp@icloud.com>
2018-05-14 23:35:12 +0100

    Codeclimate's yelling at us, what are we going to do ¯\_(ツ)_/¯

--- src/commands/memes/hmmm.js
+++ src/commands/memes/hmmm.js
@@ -11,11 +11,11 @@ module.exports = class Hmmm extends Command {
 
   run (message) {
     let embed = this.client.getDefaultEmbed(message.author)
-    request.get('https://reddit.com/r/hmmm/random/.json').then(data => {
-      let req = data.body[0].data.children[0].data
-      embed.setImage(req.url)
+    request.get('https://reddit.com/r/hmmm/random/.json').then(req => {
+      let res = req.body[0].data.children[0].data
+      embed.setImage(res.url)
       embed.setTitle('hmmm')
-      embed.setURL(`https://reddit.com${req.permalink}`)
+      embed.setURL(`https://reddit.com${res.permalink}`)
       message.channel.send({embed})
     })
   }
