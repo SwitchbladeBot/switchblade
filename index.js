8583202d8ba08d8112f77409c583f2af4cdff4dd
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-05-14 15:09:36 -0300

    Add better logging to login

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const CLIENT_OPTIONS = {
 
 const Switchblade = require('./src/Switchblade.js')
 const client = new Switchblade(CLIENT_OPTIONS)
-client.login()
+client.login().then(client.log('Logged in successfully!', 'Discord')).catch(e => client.logError(e, true))
