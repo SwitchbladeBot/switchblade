0308a301d5922b0c5390fde6ce9a8e6ab52e1deb
davipatury <davi.patury@yahoo.com.br>
2018-05-14 11:49:16 -0300

    Enable autoreconnect

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const CLIENT_OPTIONS = {}
+const CLIENT_OPTIONS = {
+  autoReconnect: true
+}
+
 const Switchblade = require('./src/Switchblade.js')
 const client = new Switchblade(CLIENT_OPTIONS)
 client.login()
