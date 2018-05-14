c1ea431d274c2b7d8fa2fc98c75ed419915f7ebe
DoctorVWA <doctorvwa43@gmail.com>
2018-05-14 20:59:28 -0300

    Moved

---
+++ src/commands/misc/adorable.js
@@ -0,0 +1,16 @@
+const { Command } = require('../../')
+
+module.exports = class Adorable extends Command {
+  constructor (client) {
+    super(client)
+
+    this.name = 'adorable'
+  }
+
+  run (message) {
+    const template = 'https://api.adorable.io/avatars/256'
+    let embed = this.client.getDefaultEmbed(message.author)
+    embed.setImage(`${template}/${message.author.avatar}.png`)
+    message.channel.send({embed})
+  }
+}
