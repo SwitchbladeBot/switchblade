2c952eeabf37d8a5b0675c86f3ce3de7451f2571
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-05-14 15:38:35 -0300

    SEMICOLONS SUCC

--- src/commands/memes/smart.js
+++ src/commands/memes/smart.js
@@ -3,15 +3,15 @@ const { Command } = require('../../')
 module.exports = class Smart extends Command {
 
   constructor(client) {
-    super(client);
+    super(client)
 
-    this.name = "smart";
+    this.name = "smart"
   }
 
   run(message) {
-    let embed = this.client.getDefaultEmbed(message.author);
-    embed.setImage('https://i.imgur.com/MzwiZg8.png');
-    message.channel.send({embed});
+    let embed = this.client.getDefaultEmbed(message.author)
+    embed.setImage('https://i.imgur.com/MzwiZg8.png')
+    message.channel.send({embed})
   }
 
 }
