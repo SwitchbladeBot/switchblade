2e15a688d9a295ea32e3aea36acc656d7166fc34
Doges <31023616+xDoges@users.noreply.github.com>
2018-05-14 20:11:13 -0300

    Added invite command

---
+++ src/commands/misc/invite.js
@@ -0,0 +1,19 @@
+const { Command } = require('../../')
+
+module.exports = class Invite extends Command {
+  constructor (client) {
+    super(client)
+    this.name = 'invite'
+    this.aliases = []
+  }
+
+  run (message) {
+    message.channel.startTyping()
+    let embed = this.client.getDefaultEmbed()
+    var perm = '``MANAGE_SERVER``'
+    embed.setThumbnail(this.client.user.displayAvatarURL)
+    embed.setDescription(`[Click here to invite me to your server](https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&permissions=0&scope=bot)\nNote that you need the ${perm} permission to add bots to servers.`)
+    message.channel.send({embed})
+    message.channel.stopTyping()
+  }
+}
