e0a3e93668b8cbfb09d2dbfce22f375699df916c
Doges <31023616+xDoges@users.noreply.github.com>
2018-05-15 12:53:08 -0300

    Update Again

--- src/commands/misc/dog.js
+++ src/commands/misc/dog.js
@@ -18,9 +18,13 @@ module.exports = class Dog extends Command {
         message.channel.send({embed})
       } else {
         request.get('https://random.dog/woof.json').then(r => {
-          const embed = this.client.getDefaultEmbed(message.author)
-          embed.setImage(r.body.url).setDescription('Here is your dog <:DoggoF:445701839564963840>')
-          message.channel.send({embed})
+          if (r.body.url.endsWith(notsupported)) {
+            request.get('https://random.dog/woof.json').then(r => {
+              const embed = this.client.getDefaultEmbed(message.author)
+              embed.setImage(r.body.url).setDescription('Here is your dog <:DoggoF:445701839564963840>')
+              message.channel.send({embed})
+            })
+          }
         })
       }
     })
