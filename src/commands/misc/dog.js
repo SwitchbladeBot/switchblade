8f4c909984ff75ce0396a284a7c46a087ad917b6
David Paiva <pillgp@icloud.com>
2018-05-15 19:49:16 +0100

    Update dog.js
    Moved message.channel.stopTyping() to the end of this.sendDoggo()

--- src/commands/misc/dog.js
+++ src/commands/misc/dog.js
@@ -11,13 +11,13 @@ module.exports = class Dog extends Command {
   async run (message) {
     message.channel.startTyping()
     await this.requestNewDoggo(message)
-    message.channel.stopTyping()
   }
 
   sendDoggo (message, r) {
     const embed = this.client.getDefaultEmbed(message.author)
     embed.setImage(r.body.url).setDescription('Here is your dog <:DoggoF:445701839564963840>')
     message.channel.send({embed})
+    message.channel.stopTyping()
   }
 
   requestNewDoggo (message) {
