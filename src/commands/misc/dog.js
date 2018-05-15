7af76872d06a9607a48840d418b2516bf0b69a08
David Paiva <pillgp@icloud.com>
2018-05-15 19:46:00 +0100

    Update dog.js
    Added await, and it now requests / sends a doggo from outside of the .run

--- src/commands/misc/dog.js
+++ src/commands/misc/dog.js
@@ -8,16 +8,23 @@ module.exports = class Dog extends Command {
     this.aliases = ['doggo', 'dogpics', 'randomdog']
   }
 
-  run (message) {
+  async run (message) {
     message.channel.startTyping()
-    request.get('https://random.dog/woof.json').then(r => {
-      var notsupported = ['.mp4']
-      if (!r.body.url.endsWith(notsupported)) {
-        const embed = this.client.getDefaultEmbed(message.author)
-        embed.setImage(r.body.url).setDescription('Here is your dog <:DoggoF:445701839564963840>')
-        message.channel.send({embed})
-      } else this.run(message)
-    })
+    await this.requestNewDoggo(message)
     message.channel.stopTyping()
+  }
+
+  sendDoggo (message, r) {
+    const embed = this.client.getDefaultEmbed(message.author)
+    embed.setImage(r.body.url).setDescription('Here is your dog <:DoggoF:445701839564963840>')
+    message.channel.send({embed})
+  }
+
+  requestNewDoggo (message) {
+    request.get('https://random.dog/woof.json').then(r => {
+      const notSupported = ['.mp4']
+      if (!r.body.url.endsWith(notSupported)) this.sendDoggo(message, r)
+      else this.requestNewDoggo(message)
+    })
   }
 }
