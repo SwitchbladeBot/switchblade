981ed00c52879ac2b825fddc78f00a86867f48ad
PillGP <pillgp@icloud.com>
2018-05-15 20:46:17 +0100

    Using actual strings > Defining 'perm' as one just to show up coded

--- src/commands/misc/invite.js
+++ src/commands/misc/invite.js
@@ -10,9 +10,8 @@ module.exports = class Invite extends Command {
   run (message) {
     message.channel.startTyping()
     let embed = this.client.getDefaultEmbed()
-    var perm = '``MANAGE_SERVER``'
     embed.setThumbnail(this.client.user.displayAvatarURL)
-    embed.setDescription(`[Click here to invite me to your server](https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&permissions=0&scope=bot)\nNote that you need the ${perm} permission to add bots to servers.`)
+    embed.setDescription(`[Click here to invite me to your server](https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&permissions=0&scope=bot)\nNote that you need the \`MANAGE_SERVER\` permission to add bots to servers.`)
     message.channel.send({embed})
     message.channel.stopTyping()
   }
