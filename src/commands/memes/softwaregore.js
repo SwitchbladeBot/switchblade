9cbac5fe6b95fc75bf6b5809c40e44cf7d28e198
Doges <31023616+xDoges@users.noreply.github.com>
2018-05-16 17:36:42 -0300

    Adding SoftwareGore

---
+++ src/commands/memes/softwaregore.js
@@ -0,0 +1,20 @@
+const { Command, Reddit, SwitchbladeEmbed } = require('../../')
+
+module.exports = class SoftwareGore extends Command {
+  constructor (client) {
+    super(client)
+    this.name = 'softwaregore'
+    this.aliases = ['sg']
+  }
+
+  async run (message) {
+    message.channel.startTyping()
+    const { url, permalink } = await Reddit.getRandomPostFromSubreddit('/r/softwaregore')
+    message.channel.send(
+      new SwitchbladeEmbed(message.author)
+        .setTitle('<:meowthonkang:446407184373383169>')
+        .setImage(url)
+        .setURL(`https://reddit.com${permalink}`)
+    ).then(() => message.channel.stopTyping())
+  }
+}
