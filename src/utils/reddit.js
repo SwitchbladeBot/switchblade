863860090a3be5e9a7df926e4336a5be612a00fc
davipatury <davi.patury@yahoo.com.br>
2018-05-15 21:05:21 -0300

    Anti-error reddit

--- src/utils/reddit.js
+++ src/utils/reddit.js
@@ -4,6 +4,6 @@ module.exports = class Reddit {
   static async getRandomPostFromSubreddit (subReddit) {
     subReddit = subReddit.replace('/r/', '')
     let { body } = await snekfetch.get(`https://reddit.com/r/${subReddit}/random/.json`)
-    return body[0].data.children[0].data
+    return body[0] && body[0].data.children[0].data
   }
 }
