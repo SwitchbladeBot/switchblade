5f52ffb2ecf290a987b0d6f934efe3f3654f449f
Leander West Furumo <leanderfurumo@gmail.com>
2018-05-24 18:04:05 +0200

    Update dicksize.js

--- src/commands/misc/dicksize.js
+++ src/commands/misc/dicksize.js
@@ -1,15 +1,19 @@
 const { Command } = require('../../')
 
-module.exports = class Triggered extends Command {
+module.exports = class DickSize extends Command {
 
   run (message) {
     constructor (client) {
     super(client)
-    this.name = 'DickSize'
+    this.name = 'dicksize'
   }
-  
+    
+  if const endpoint = message.channel.nsfw = True {
   var randnumb = Math.floor((Math.random() * 20) + 1);
     
-    message.channel.send(rannumb + ' cm')
+    message.channel.send(rannumb + ' cm') 
+  } else {
+    message.channel.send('This is a NSFW-only command. It only works in NSFW channels.') 
+  }
   }
 }
