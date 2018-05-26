6ad429e9603f89583a140d58dffe0cd9e2add312
David Paiva <pillgp@icloud.com>
2018-05-26 17:10:31 +0100

    Update owo.js

--- src/commands/memes/owo.js
+++ src/commands/memes/owo.js
@@ -10,21 +10,17 @@ module.exports = class OwO extends Command {
   }
 
   async run (message, args) {
-    // If theres something to owoify, it gonna return, else, not.
-    if (args.join(' ') > 0) {
+    const embed = new SwitchbladeEmbed(message.author)
+    if (args.length > 0) {
       message.channel.startTyping()
       const { body } = await snekfetch.get(OWOapi + args.join(' '))
-      message.channel.send(
-        new SwitchbladeEmbed(message.author)
-          .setTitle(body.owo)
-      ).then(() => message.channel.stopTyping())
+      embed.setTitle(body.owo)
     } else {
-      message.channel.send(
-        new SwitchbladeEmbed(message.author)
-          .setColor(this.client.colors.error)
-          .setTitle('You need to give me something to owoify')
-          .setDescription(`**Usage:** ${process.env.PREFIX}${this.name} <sentence>`)
-      ).then(() => message.channel.stopTyping())
+      message.channel.startTyping()
+      embed.setColor(this.client.colors.error)
+      embed.setTitle('You need to give me a sentence to owoify')
+      embed.setDescription(`**Usage:** ${process.env.PREFIX}${this.name} <sentence>`)
     }
+    message.channel.send(embed).then(() => message.channel.stopTyping())
   }
 }
