75539c8974902cfa44cc3152cae19c46e5b5b7af
davipatury <davi.patury@yahoo.com.br>
2018-05-26 19:52:01 -0300

    Schemas

--- src/database/mongo/Schemas.js
+++ src/database/mongo/Schemas.js
@@ -4,12 +4,14 @@ module.exports = {
   // User Schema
   User: new Schema({
     _id: String,
-    xp: { type: Number, default: 0 }
+    globalXp: { type: Number, default: 0 },
+    money: { type: Number, default: 0 }
   }),
 
   // Guild Schema
   Guild: new Schema({
     _id: String,
-    prefix: { type: String, default: process.env.PREFIX }
+    prefix: { type: String, default: process.env.PREFIX },
+    language: { type: String, default: 'en_EN' }
   })
 }
