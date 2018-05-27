47312abc98a86d5593af4594f619ab63bfc98172
davipatury <davi.patury@yahoo.com.br>
2018-05-26 21:23:32 -0300

    Command listener improvement

--- src/listeners/MainListener.js
+++ src/listeners/MainListener.js
@@ -12,14 +12,16 @@ module.exports = class MainListener extends EventListener {
 
   async onMessage (message) {
     if (message.author.bot) return
-
+    
     const guildDB = message.guild && this.database && await this.database.guilds.get(message.guild.id)
     const prefix = (guildDB && guildDB.prefix) || process.env.PREFIX
-    if (message.content.startsWith(prefix)) {
-      let fullCmd = message.content.split(/\s+/g).filter(a => a).map(s => s.trim())
-      let args = fullCmd.slice(1)
-      let cmd = fullCmd[0].substring(prefix.length).toLowerCase()
-      let command = this.commands.find(c => c.name.toLowerCase() === cmd || c.aliases.includes(cmd))
+    const prefixRegex = new RegExp(`^(${this.user}[ ]?|${prefix}).+`)
+    const prefixUsed = prefixRegex.exec(message.content)[1]
+    if (prefixUsed) {
+      const fullCmd = message.content.substring(prefixUsed.length).split(/\s+/g).filter(a => a).map(s => s.trim())
+      const args = fullCmd.slice(1)
+      const cmd = fullCmd[0].toLowerCase().trim()
+      const command = this.commands.find(c => c.name.toLowerCase() === cmd || c.aliases.includes(cmd))
 
       if (command) {
         this.runCommand(command, message, args)
