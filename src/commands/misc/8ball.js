9509322644291acf8c5232585b5115d1cb50e7cd
David Paiva <pillgp@icloud.com>
2018-05-31 15:20:14 +0100

    Update 8ball.js

--- src/commands/misc/8ball.js
+++ src/commands/misc/8ball.js
@@ -8,7 +8,7 @@ module.exports = class EightBall extends Command {
   }
 
   run (message, args) {
-    const replies = ['Yes', 'No', 'I don\'t know', 'Ask again later', 'I\'m not so sure', 'You tell me']
+    const replies = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
     const embed = new SwitchbladeEmbed(message.author)
     message.channel.startTyping()
     if (!args[0]) {
