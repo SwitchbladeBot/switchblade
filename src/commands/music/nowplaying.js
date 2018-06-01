81180b4e2da2cb61a1905e235fd8ac792461611a
davipatury <davi.patury@yahoo.com.br>
2018-05-31 22:44:33 -0300

    np and queue improvements

--- src/commands/music/nowplaying.js
+++ src/commands/music/nowplaying.js
@@ -10,32 +10,30 @@ module.exports = class NowPlaying extends Command {
 
   async run (message, args) {
     let embed = new SwitchbladeEmbed(message.author)
-    if (message.member.voiceChannel) {
-      const playerManager = this.client.playerManager
-      const guildPlayer = playerManager.get(message.guild.id)
-      if (guildPlayer && guildPlayer.playing) {
-        const song = guildPlayer.playingSong
-        embed = new SwitchbladeEmbed(song.requestedBy)
+    const playerManager = this.client.playerManager
+    const guildPlayer = playerManager.get(message.guild.id)
+    if (guildPlayer && guildPlayer.playing) {
+      const song = guildPlayer.playingSong
+      embed = new SwitchbladeEmbed(song.requestedBy)
 
-        const format = song.length >= 3600000 ? 'hh:mm:ss' : 'mm:ss'
-        const elapsed = moment.duration(guildPlayer.state.position).format(format, { trim: false })
-        const duration = moment.duration(song.length).format(format, { trim: false })
+      const format = song.length >= 3600000 ? 'hh:mm:ss' : 'mm:ss'
+      const elapsed = this.formatDuration(guildPlayer.state.position, format)
+      const duration = this.formatDuration(song.length, format)
 
-        embed.setDescription([
-          `**Now playing:** [${song.title}](${song.uri}) \`(${elapsed}/${duration})\``
-        ].join('\n'))
-      } else {
-        embed
-          .setColor(Constants.ERROR_COLOR)
-          .setTitle('I ain\'t playing anything!')
-      }
+      embed.setDescription([
+        `**Now playing:** [${song.title}](${song.uri}) \`(${elapsed}/${duration})\``
+      ].join('\n'))
     } else {
       embed
         .setColor(Constants.ERROR_COLOR)
-        .setTitle('You need to be in a voice channel to use this command!')
+        .setTitle('I ain\'t playing anything!')
     }
 
     message.channel.send(embed)
+  }
+
+  formatDuration (duration, format) {
+    return moment.duration(duration).format(format, { trim: false })
   }
 
   canRun (message, args) {
--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -10,30 +10,24 @@ module.exports = class Queue extends Command {
   async run (message, args) {
     const embed = new SwitchbladeEmbed(message.author)
 
-    if (message.member.voiceChannel) {
-      const playerManager = this.client.playerManager
-      const guildPlayer = playerManager.get(message.guild.id)
-      if (guildPlayer && guildPlayer.playing) {
-        const npSong = guildPlayer.playingSong
-        const description = [`**Now playing:** [${npSong.title}](${npSong.uri})`]
+    const playerManager = this.client.playerManager
+    const guildPlayer = playerManager.get(message.guild.id)
+    if (guildPlayer && guildPlayer.playing) {
+      const npSong = guildPlayer.playingSong
+      const description = [`**Now playing:** [${npSong.title}](${npSong.uri})`]
 
-        if (guildPlayer.queue.length > 0) {
-          description.push('', '**Songs queued:**')
-          description.push(...guildPlayer.queue.map((song, i) => `${i + 1}. [${song.title}](${song.uri}) (added by ${song.requestedBy})`))
-        } else {
-          description.push('There are no songs after the current one.')
-        }
-
-        embed.setDescription(description.join('\n'))
+      if (guildPlayer.queue.length > 0) {
+        description.push('', '**Songs queued:**')
+        description.push(...guildPlayer.queue.map((song, i) => `${i + 1}. [${song.title}](${song.uri}) (added by ${song.requestedBy})`))
       } else {
-        embed
-          .setColor(Constants.ERROR_COLOR)
-          .setTitle('I ain\'t playing anything!')
+        description.push('There are no songs after the current one.')
       }
+
+      embed.setDescription(description.join('\n'))
     } else {
       embed
         .setColor(Constants.ERROR_COLOR)
-        .setTitle('You need to be in a voice channel to use this command!')
+        .setTitle('I ain\'t playing anything!')
     }
 
     message.channel.send(embed)
