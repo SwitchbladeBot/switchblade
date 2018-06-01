2c24ba6aa8cbc0413d9936027066110995729af1
perronosaurio <waxtz@hotmail.com>
2018-05-31 22:10:21 -0400

    Add files via upload

--- src/commands/misc/npm.js
+++ src/commands/misc/npm.js
@@ -1,5 +1,5 @@
-const { Command, SwitchbladeEmbed, Constants } = require('../../')
-const npm = require('api-npm')
+const { Command, SwitchbladeEmbed, Constants } = require('../../');
+const npm = require('api-npm');
 
 module.exports = class Npm extends Command {
   constructor (client) {
@@ -8,24 +8,24 @@ module.exports = class Npm extends Command {
   }
 
   run (message, args) {
-    const embed = new SwitchbladeEmbed(message.author)
+    const embed = new SwitchbladeEmbed(message.author);
     if (args[0]) {
       npm.getdetails(args.join('-'), data => {
         if (data.name) {
-          embed.setAuthor(data.name, 'https://i.imgur.com/24yrZxG.png', 'https://www.npmjs.com/package/' + data.name)
-          embed.setDescription(`${data.description}\nhttps://www.npmjs.com/package/${data.name}\n\n\`npm i ${data.name} --save\``)
+          embed.setAuthor(data.name, 'https://i.imgur.com/24yrZxG.png', `https://www.npmjs.com/package/${data.name}`);
+          embed.setDescription(`${data.description}\nhttps://www.npmjs.com/package/${data.name}\n\n\`npm i ${data.name} --save\``);
         } else {
-          embed.setColor(Constants.ERROR_COLOR)
-          embed.setAuthor('Package not found!')
-          embed.setDescription(`**Usage:** \`${process.env.PREFIX}${this.name} <package name>\``)
+          embed.setColor(Constants.ERROR_COLOR);
+          embed.setAuthor('Package not found!');
+          embed.setDescription(`**Usage:** \`${process.env.PREFIX}${this.name} <package name>\``);
         }
-        message.channel.send({ embed })
-      })
+        message.channel.send({ embed });
+      });
     } else {
-      embed.setColor(Constants.ERROR_COLOR)
-      embed.setAuthor('You need to give me a package name.')
-      embed.setDescription(`**Usage:** \`${process.env.PREFIX}${this.name} <package name>\``)
-      message.channel.send({ embed })
+      embed.setColor(Constants.ERROR_COLOR);
+      embed.setAuthor('You need to give me a package name.');
+      embed.setDescription(`**Usage:** \`${process.env.PREFIX}${this.name} <package name>\``);
+      message.channel.send({ embed });
     }
   }
 }
