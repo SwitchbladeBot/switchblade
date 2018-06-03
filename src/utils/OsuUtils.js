0cf76e5b4e2ebf9f278b78ae43c5b16c86a091ce
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-06-02 23:34:02 -0300

    Add two functions that will be needed in the future

--- src/utils/OsuUtils.js
+++ src/utils/OsuUtils.js
@@ -2,4 +2,12 @@ module.exports = class OsuUtils {
   static get modes() {
     return ['osu', 'taiko', 'catch', 'mania']
   }
+
+  static calculateAccuracy(mode, score, beatmap) {
+    // TODO
+  }
+
+  static calculateMods(number) {
+    // TODO
+  }
 }
