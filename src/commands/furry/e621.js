d8540f80dbf57fd6bc46444945084f9376939ff6
Doges <31023616+Doges@users.noreply.github.com>
2018-06-09 22:22:30 -0300

    Update e621.js

--- src/commands/furry/e621.js
+++ src/commands/furry/e621.js
@@ -12,14 +12,12 @@ module.exports = class E621 extends Command {
     message.channel.startTyping()
     const embed = new SwitchbladeEmbed(message.author)
     if (message.channel.nsfw) {
-      booru.search('e621.net', ['rating:e'], {limit: 1, random: true})
+      const images = await booru.search('e621.net', ['rating:e'], {limit: 1, random: true})
         .then(booru.commonfy)
-        .then(images => {
-          for (let image of images) {
-            embed.setImage(image.common.file_url)
-            embed.setDescription('Here\'s your yiff!')
-          }
-        })
+      for (let image of images) {
+        embed.setImage(image.common.file_url)
+        embed.setDescription('Here\'s your yiff!')
+      }
     } else {
       embed.setColor(Constants.ERROR_COLOR)
       embed.setTitle('You need to use this command in a NSFW channel')
