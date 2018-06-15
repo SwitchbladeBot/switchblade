623ee301a028767d6309bb19e3e1a17e68bba132
Doges <31023616+Doges@users.noreply.github.com>
2018-06-15 10:29:48 -0300

    Add files via upload

--- src/commands/furry/e621.js
+++ src/commands/furry/e621.js
@@ -1,3 +1,30 @@
+// const { Command, SwitchbladeEmbed, Constants } = require('../../')
+// const booru = require('booru')
+
+// module.exports = class E621 extends Command {
+//   constructor (client) {
+//     super(client)
+//     this.name = 'e621'
+//     this.aliases = ['yiff']
+//   }
+
+//   async run (message, t) {
+//     message.channel.startTyping()
+//     const embed = new SwitchbladeEmbed(message.author)
+//     if (message.channel.nsfw) {
+//       const [ image ] = await booru.search('e621.net', ['rating:e'], {limit: 1, random: true}).then(booru.commonfy)
+//       embed.setImage(image.common.file_url)
+//       embed.setDescription(`${t('commands:e621.hereIsYour_yiff')}`)
+//       embed.setColor(Constants.E621_COLOR)
+//     } else {
+//       embed.setColor(Constants.ERROR_COLOR)
+//       embed.setTitle(t('errors:nsfwOnly'))
+//     }
+//     message.channel.send(embed).then(() => message.channel.stopTyping())
+//   }
+// }
+
+
 const { Command, SwitchbladeEmbed, Constants } = require('../../')
 const booru = require('booru')
 
@@ -8,19 +35,21 @@ module.exports = class E621 extends Command {
     this.aliases = ['yiff']
   }
 
-  async run (message) {
+  async run (message, args, t) {
     message.channel.startTyping()
     const embed = new SwitchbladeEmbed(message.author)
     if (message.channel.nsfw) {
-      const [ image ] = await booru.search('e621.net', ['rating:e'], {limit: 1, random: true}).then(booru.commonfy)
-      embed.setImage(image.common.file_url)
-      embed.setDescription('Here\'s your yiff!')
-      embed.setColor(Constants.E621_COLOR)
-    } else {
-      embed.setColor(Constants.ERROR_COLOR)
-      embed.setTitle('You need to use this command in a NSFW channel')
-      embed.setDescription(`**Usage:** ${process.env.PREFIX}${this.name}`)
-    }
-    message.channel.send(embed).then(() => message.channel.stopTyping())
+    const [ image ] = await booru.search('e621.net', ['rating:e'], {limit: 1, random: true}).then(booru.commonfy)
+    // Embed Yiff
+    embed.setImage(image.common.file_url)
+    embed.setDescription(t('commands:e621.hereIsYour_yiff'))
+    embed.setColor(Constants.E621_COLOR)
+  } else {
+    // NSFW Error
+    embed.setDescription(t('errors:nsfwOnly'))
+    embed.setColor(Constants.ERROR_COLOR)
   }
+  message.channel.send(embed).then(() => message.channel.stopTyping())
 }
+}
+
