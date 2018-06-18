142b86c391582f63a23e379d89b3838365335df0
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-06-18 20:39:03 -0300

    Standard weeee

--- src/commands/search/appstore.js
+++ src/commands/search/appstore.js
@@ -1,6 +1,6 @@
 const { CommandStructures, SwitchbladeEmbed, Constants } = require('../../index')
 const { Command, CommandParameters, StringParameter } = CommandStructures
-const itunesSearchApi = require('itunes-search-api');
+const itunesSearchApi = require('itunes-search-api')
 
 module.exports = class AppStore extends Command {
   constructor (client) {
@@ -53,7 +53,11 @@ function ratingToStarEmoji (rating) {
   return '<:ratingstar:458381544357101580>'.repeat(Math.floor(rating)) + ((rating % 1).toFixed(1) !== 0 ? '<:ratinghalfstar:458381544571273224>' : '')
 }
 
-function formatBytes (a,b) {
-  if (0===a) return "0 Bytes"
-  const c=1024,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c))
-  return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]}
+function formatBytes (a, b) {
+  if (a === 0) return '0 Bytes'
+  const c = 1024
+  const d = b || 2
+  const e = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  const f = Math.floor(Math.log(a) / Math.log(c))
+  return parseFloat((a / Math.pow(c, f)).toFixed(d)) + ' ' + e[f]
+}
