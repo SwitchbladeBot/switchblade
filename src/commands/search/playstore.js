2b0463afe24d55004af4d70a4be2b525cec27efc
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-06-18 21:18:49 -0300

    Add Google Play Store command

---
+++ src/commands/search/playstore.js
@@ -0,0 +1,41 @@
+const { CommandStructures, SwitchbladeEmbed, Constants, MiscUtils } = require('../../index')
+const { Command, CommandParameters, StringParameter } = CommandStructures
+const gplay = require('google-play-scraper');
+
+module.exports = class PlayStore extends Command {
+  constructor (client) {
+    super(client)
+
+    this.name = 'playstore'
+    this.aliases = ['googleplay', 'gplay']
+    this.parameters = new CommandParameters(this,
+      new StringParameter({full: false, missingError: 'errors:noCountryCode'}),
+      new StringParameter({full: false, missingError: 'errors:noAppName'})
+    )
+  }
+
+  async run ({t, author, channel, language}, country, term) {
+    channel.startTyping()
+    const embed = new SwitchbladeEmbed(author)
+    const response = await gplay.search({term, country, num: 1, lang: language.substring(0, 1)})
+    if (response.length > 0) {
+      const app = response[0]
+      embed
+        .setColor(0x518FF5)
+        .setThumbnail('http://' + app.icon)
+        .setAuthor('Google Play Store', 'https://i.imgur.com/tkTq2bi.png')
+        .setDescription([
+          `**[${app.title}](${app.url})**`,
+          app.summary,
+          '',
+          MiscUtils.ratingToStarEmoji(app.score),
+          app.priceText
+        ].join('\n'))
+    } else {
+      embed
+        .setColor(Constants.ERROR_COLOR)
+        .setTitle(t('errors:appNotFound'))
+    }
+    channel.send(embed).then(() => channel.stopTyping())
+  }
+}
