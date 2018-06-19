7b2ec134659d510f4129e9e62d6778a28354379d
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-06-18 21:18:39 -0300

    Update embed and remove unused functions

--- src/commands/search/appstore.js
+++ src/commands/search/appstore.js
@@ -19,11 +19,11 @@ module.exports = class AppStore extends Command {
     const response = await itunesSearchApi(appName, {query: {entity: 'software', limit: 1, country: country.toUpperCase()}})
     if (response.body.resultCount > 0) {
       const app = response.body.results[0]
-      console.log(app)
       embed
+        .setColor(0x1B9BF6)
         .setURL(app.trackViewUrl)
         .setThumbnail(app.artworkUrl60)
-        .setAuthor('App Store', 'https://i.imgur.com/yki1FZs.jpg')
+        .setAuthor('App Store', 'https://i.imgur.com/1MJCm1X.jpg')
         .setDescription([
           `**[${app.trackName}](${app.trackViewUrl})**`,
           app.genres.join(', '),
@@ -46,17 +46,4 @@ function getPriceString (app, t) {
   } else {
     return app.formattedPrice
   }
-}
-
-function ratingToStarEmoji (rating) {
-  return '<:ratingstar:458381544357101580>'.repeat(Math.floor(rating)) + ((rating % 1).toFixed(1) !== 0 ? '<:ratinghalfstar:458381544571273224>' : '')
-}
-
-function formatBytes (a, b) {
-  if (a === 0) return '0 Bytes'
-  const c = 1024
-  const d = b || 2
-  const e = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-  const f = Math.floor(Math.log(a) / Math.log(c))
-  return parseFloat((a / Math.pow(c, f)).toFixed(d)) + ' ' + e[f]
 }
