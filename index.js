fb201cc904110d65ac4688d20ba866b457d22486
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-06-20 18:11:29 -0300

    Just so we can create the PR

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('moment-duration-format')
 
 const CLIENT_OPTIONS = {}
 
+
 const Switchblade = require('./src/Switchblade.js')
 const client = new Switchblade(CLIENT_OPTIONS)
 client.login().then(() => client.log('Logged in successfully!', 'Discord')).catch(e => client.logError(e))
