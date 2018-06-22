a9e6ca34c7a6fe40efbd878171f4d13ebeac2182
David Paiva <pillgp@icloud.com>
2018-06-22 02:34:09 +0100

    Update isitup.js

--- src/commands/misc/isitup.js
+++ src/commands/misc/isitup.js
@@ -1,33 +1,28 @@
-const { Command, SwitchbladeEmbed, Constants } = require('../../')
+const { CommandStructures, SwitchbladeEmbed, Constants } = require('../../')
+const { Command, CommandParameters, StringParameter } = CommandStructures
 const snekfetch = require('snekfetch')
 
 module.exports = class IsItUp extends Command {
   constructor (client) {
     super(client)
     this.name = 'isitup'
+    this.parameters = new CommandParameters(this,
+      new StringParameter({ missingError: 'commands:isitup.noWebsite'})
+    )
   }
 
-  async run (message, args, t) {
-    const embed = new SwitchbladeEmbed(message.author)
-    if (args.length > 0) {
-      message.channel.startTyping()
-      const site = encodeURIComponent(args.join(' '))
-      const {body} = await snekfetch.get(`https://isitup.org/${site}.json`)
-      if (body.response_code) {
-        embed
-          .setTitle(t('commands:isitup.isUp', {body}))
-          .setDescription(t('commands:isitup.details', {body}))
-      } else {
-        embed
-          .setColor(Constants.ERROR_COLOR)
-          .setTitle(t('commands:isitup.isDown', {body}))
-      }
+  async run ({t, author, channel}, link) {
+    const embed = new SwitchbladeEmbed(author)
+    channel.startTyping()
+    const site = encodeURIComponent(link)
+    const { body } = await snekfetch.get(`https://isitup.org/${site}.json`)
+    if (body.response_code) {
+      embed.setTitle(t('commands:isitup.isUp', {body}))
+        .setDescription(t('commands:isitup.details', {body}))
     } else {
-      embed
-        .setColor(Constants.ERROR_COLOR)
-        .setTitle(t('commands:isitup.noWebsite'))
-        .setDescription(`**${t('commons:usage')}:** \`${process.env.PREFIX}${this.name} ${t('commands:npm.commandUsage')}\``)
+      embed.setColor(Constants.ERROR_COLOR)
+        .setTitle(t('commands:isitup.isDown', {body}))
     }
-    message.channel.send(embed).then(() => { message.channel.stopTyping() })
+    channel.send(embed).then(() => channel.stopTyping())
   }
 }
