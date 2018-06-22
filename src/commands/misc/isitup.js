46ade0373ebb957fcc0a1ef5fcb6c4ab2a4e5a3e
David Paiva <pillgp@icloud.com>
2018-06-22 12:38:49 +0100

    Update isitup.js

--- src/commands/misc/isitup.js
+++ src/commands/misc/isitup.js
@@ -1,19 +1,20 @@
 const { CommandStructures, SwitchbladeEmbed, Constants } = require('../../')
 const { Command, CommandParameters, StringParameter } = CommandStructures
 const snekfetch = require('snekfetch')
+const regex = new RegExp('([a-z0-9]+\\.)*[a-z0-9]+\\.[a-z]+')
 
 module.exports = class IsItUp extends Command {
   constructor (client) {
     super(client)
     this.name = 'isitup'
-    this.parameters = new CommandParameters(this, new StringParameter({ missingError: 'commands:isitup.noWebsite' }))
+    this.parameters = new CommandParameters(this, new StringParameter({ full: true, missingError: 'commands:isitup.noWebsite' }))
   }
 
-  async run ({t, author, channel}, link) {
+  async run ({t, author, channel}, url) {
     const embed = new SwitchbladeEmbed(author)
     channel.startTyping()
-    const site = encodeURIComponent(link)
-    const { body } = await snekfetch.get(`https://isitup.org/${site}.json`)
+    const site = regex.exec(url)
+    const { body } = await snekfetch.get(`https://isitup.org/${site[0]}.json`)
     if (body.response_code) {
       embed.setTitle(t('commands:isitup.isUp', {body}))
         .setDescription(t('commands:isitup.details', {body}))
