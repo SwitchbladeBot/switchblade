70c71a30157a0777ac424b413bed2c5fde229be2
pedrofracassi <pedroluizfracassi@gmail.com>
2018-06-30 12:56:49 -0300

    Some progress on the command

---
+++ src/commands/games/fortnite/fortnite.js
@@ -0,0 +1,32 @@
+const { CommandStructures, SwitchbladeEmbed, Constants } = require('../../../')
+const { Command, CommandParameters, StringParameter } = CommandStructures
+const Fortnite = require('fortnite-api')
+
+module.exports = class Deezer extends Command {
+  constructor (client) {
+    super(client)
+
+    this.name = 'fortnite'
+    this.aliases = ['ftn']
+    this.parameters = new CommandParameters(this,
+      new StringParameter({full: false, missingError: 'commands:fortnite.missingPlatform'}),
+      new StringParameter({full: true, missingError: 'commands:fortnite.missingNickname'})
+    )
+  }
+
+  canLoad () {
+    return !!this.client.apis.fortnite
+  }
+
+  async run ({ t, author, channel }, platform, nickname) {
+    channel.startTyping()
+    const embed = new SwitchbladeEmbed(author)
+    try {
+      const player = await this.client.apis.fortnite.checkPlayer(nickname, platform)
+      console.log(player)
+    } catch (e) {
+      console.log(e)
+    }
+    channel.send(embed).then(() => channel.stopTyping())
+  }
+}
