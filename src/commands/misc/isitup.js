dec0ae1fe5542099b5a2c20720f85e6f1fc526e0
davipatury <davi.patury@yahoo.com.br>
2018-07-01 19:04:48 -0300

    Is it up fix

--- src/commands/misc/isitup.js
+++ src/commands/misc/isitup.js
@@ -1,7 +1,8 @@
 const { CommandStructures, SwitchbladeEmbed, Constants } = require('../../')
 const { Command, CommandParameters, StringParameter } = CommandStructures
 const snekfetch = require('snekfetch')
-const regex = new RegExp('([a-z0-9]+\\.)*[a-z0-9]+\\.[a-z]+')
+
+const PROTOCOL_REGEX = /^[a-z]+:\/\//
 
 module.exports = class IsItUp extends Command {
   constructor (client) {
@@ -11,13 +12,14 @@ module.exports = class IsItUp extends Command {
   }
 
   async run ({t, author, channel}, url) {
+    url = url.replace(PROTOCOL_REGEX, '').replace(/\/$/, '')
     const embed = new SwitchbladeEmbed(author)
     channel.startTyping()
-    const site = regex.exec(url)
-    const { body } = await snekfetch.get(`https://isitup.org/${site[0]}.json`)
+    const { body } = await snekfetch.get(`https://isitup.org/${url}.json`)
     if (body.response_code) {
+      body.response_time *= 1000
       embed.setTitle(t('commands:isitup.isUp'))
-        .setDescription(t('commands:isitup.details', {body}))
+        .setDescription(t('commands:isitup.details', { body }))
     } else {
       embed.setColor(Constants.ERROR_COLOR)
         .setTitle(t('commands:isitup.isDown'))
