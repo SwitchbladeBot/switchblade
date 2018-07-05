716560168117f926dfb0aa17b4700814ac4e745e
Doges <31023616+Doges@users.noreply.github.com>
2018-07-05 11:07:34 -0300

    colors v2

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -7,11 +7,20 @@ module.exports = {
   NPM_COLOR: '#CB3837',
   E621_COLOR: '258CF5',
   XKCD_COLOR: '96A8C8',
-  MAL_COLOR: '2E51A2',
   DEEZER_COLOR: 'E3231D',
+  MAL_COLOR: '2E51A2',
 
   // Emojis
   PLAY_BUTTON: '<:playbutton:450443946041016320>',
   PAUSE_BUTTON: '<:pausebutton:450443973752782889>',
-  STOP_BUTTON: '<:stopbutton:450444121924960256>'
+  STOP_BUTTON: '<:stopbutton:450444121924960256>',
+
+  // Assets
+
+  // Icons
+  COINS_PNG: 'src/assets/png/coins.png',
+  DAILY_CLOCK_PNG: 'src/assets/png/daily_clock.png',
+
+  // Backgrounds
+  DEFAULT_BACKGROUND_PNG: 'src/assets/png/backgrounds/default.png'
 }
