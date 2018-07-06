b47913f0834880a782f4f86b1c66575c50dd6cef
Doges <31023616+Doges@users.noreply.github.com>
2018-07-06 10:05:47 -0300

    fix travis shit

--- src/commands/anime/myanimelist.js
+++ src/commands/anime/myanimelist.js
@@ -18,24 +18,24 @@ module.exports = class MyAnimeList extends Command {
     channel.startTyping()
 
     try {
-    const data = await malScraper.getInfoFromName(anime)
-    if (data) {
-      embed
-        .setThumbnail(data.picture)
-        .setDescription(data.synopsis.split('\n\n')[0])
-        .setColor(Constants.MAL_COLOR)
-        .setTitle(data.title + (data.japaneseTitle ? ` (${data.japaneseTitle})` : ''))
-        .setURL(data.url)
-        .setAuthor('MyAnimeList', 'https://myanimelist.cdn-dena.com/img/sp/icon/apple-touch-icon-256.png')
-        .addField(t('commands:myanimelist.score'), `${data.score} (${data.ranked})`, true)
-        .addField(t('commands:myanimelist.studio', {count: data.studios.length}), data.studios.join(', '), true)
-        .addField(t('commands:myanimelist.episodes'), data.episodes, true)
-        .addField(t('commands:myanimelist.aired'), data.aired, true)
-    }
+      const data = await malScraper.getInfoFromName(anime)
+      if (data) {
+        embed
+          .setThumbnail(data.picture)
+          .setDescription(data.synopsis.split('\n\n')[0])
+          .setColor(Constants.MAL_COLOR)
+          .setTitle(data.title + (data.japaneseTitle ? ` (${data.japaneseTitle})` : ''))
+          .setURL(data.url)
+          .setAuthor('MyAnimeList', 'https://myanimelist.cdn-dena.com/img/sp/icon/apple-touch-icon-256.png')
+          .addField(t('commands:myanimelist.score'), `${data.score} (${data.ranked})`, true)
+          .addField(t('commands:myanimelist.studio', {count: data.studios.length}), data.studios.join(', '), true)
+          .addField(t('commands:myanimelist.episodes'), data.episodes, true)
+          .addField(t('commands:myanimelist.aired'), data.aired, true)
+      }
     } catch (e) {
       embed
-    .setColor(Constants.ERROR_COLOR)
-    .setTitle(t('commands:myanimelist.animeNotFound'))
+        .setColor(Constants.ERROR_COLOR)
+        .setTitle(t('commands:myanimelist.animeNotFound'))
     }
 
     channel.send(embed).then(() => channel.stopTyping())
