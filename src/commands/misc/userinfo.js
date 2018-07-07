694a887d3c695e941531731c504f4ba912a485a2
PillGP <pillgp@icloud.com>
2018-07-07 18:04:51 +0100

    Added a userinfo command

---
+++ src/commands/misc/userinfo.js
@@ -0,0 +1,34 @@
+const { CommandStructures, SwitchbladeEmbed, Constants } = require('../../')
+const { Command, CommandRequirements, CommandParameters, MemberParameter } = CommandStructures
+const moment = require('moment')
+
+module.exports = class UserInfo extends Command {
+  constructor (client) {
+    super(client)
+    this.name = 'userinfo'
+    this.aliases = ['user', 'ui', 'uinfo']
+    this.requirements = new CommandRequirements(this, { guildOnly: true })
+    this.parameters = new CommandParameters(this,
+      new MemberParameter({full: true, required: false, acceptBot: true})
+    )
+  }
+
+  run ({ t, member: author, channel, guildDocument }, member) {
+    const embed = new SwitchbladeEmbed(author)
+    member = member || author
+    moment.locale(guildDocument.language)
+    const filter = this.client.guilds.filter(g => g.members.get(member.id)).map(g => g.name)
+    channel.startTyping()
+
+    embed.setTitle(member.displayName)
+      .setThumbnail(member.user.displayAvatarURL)
+      .addField(t('commands:userinfo.tag'), member.user.tag, true)
+      .addField(t('commands:userinfo.id'), member.id, true)
+      .addField(t('commands:userinfo.status'), t(`commands:userinfo.${member.presence.status}`, { Constants }), true)
+      .addField(t('commands:userinfo.createdAt'), moment(member.user.createdTimestamp).format('LLL'), true)
+      .addField(t('commands:userinfo.joinedAt'), moment(member.joinedTimestamp).format('LLL'), true)
+      .addField(t('commands:userinfo.serversInCommon', { count: filter.length }), filter.join(', '))
+
+    channel.send(embed).then(() => channel.stopTyping())
+  }
+}
