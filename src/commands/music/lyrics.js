ef311c940f98ff1645dbe4be29d2197f00b8b0fe
perronosaurio <waxtz@hotmail.com>
2018-07-17 00:00:25 -0400

    Add files via upload

--- src/commands/music/lyrics.js
+++ src/commands/music/lyrics.js
@@ -19,26 +19,23 @@ module.exports = class Lyrics extends Command {
     const embed = new SwitchbladeEmbed(author)
     const request = await lyrics.getLyrics(`search?q=${song}`)
     const data = request.response.hits[0]
-    if (!data) {
-      embed.setColor(Constants.ERROR_COLOR).setTitle(t('commands:lyrics.noLyricsFound'))
-      return channel.send(embed).then(() => channel.stopTyping())
-    }
+    if (data) {
+      const img = data.result.song_art_image_thumbnail_url
+      const extendedsong = data.result.title_with_featured
+      const artist = data.result.primary_artist.name
+      const body = await lyrics.loadLyrics(data.result.url)
 
-    const img = data.result.song_art_image_thumbnail_url
-    const extendedsong = data.result.title_with_featured
-    const artist = data.result.primary_artist.name
-    const body = await lyrics.loadLyrics(data.result.url)
-    if (!body) {
-      embed.setColor(Constants.ERROR_COLOR).setTitle(t('commands:lyrics.noLyricsFound'))
+      embed.setAuthor('Genius', 'https://cdn.discordapp.com/attachments/445647209892020234/467733917798236202/b7b6ada09725d7a97a01bbef815b74d2.png')
+        .setDescription(body.length >= 1900 ? `${body.substr(0, 1900)}\n\n[Click here to see the full lyrics](http://genius.com${data.result.path})` : body)
+        .setThumbnail(img)
+        .setColor(Constants.GENIUS_COLOR)
+        .setTitle(`${extendedsong} - ${artist}`)
+        .setURL(`http://genius.com${data.result.path}`)
       return channel.send(embed).then(() => channel.stopTyping())
+    } else {
+      embed.setColor(Constants.ERROR_COLOR)
+        .setTitle(t('commands:lyrics.noLyricsFound'))
+      channel.send(embed).then(() => channel.stopTyping())
     }
-
-    embed.setAuthor(`Genius`, 'https://cdn.discordapp.com/attachments/445647209892020234/467733917798236202/b7b6ada09725d7a97a01bbef815b74d2.png')
-      .setDescription(body.length >= 1900 ? `${body.substr(0, 1900)}\n\n[Click here to see the full lyrics](http://genius.com${data.result.path})` : body)
-      .setThumbnail(img)
-      .setColor('#FFFB66')
-      .setTitle(`${extendedsong} - ${artist}`)
-      .setURL(`http://genius.com${data.result.path}`)
-    return channel.send(embed).then(() => channel.stopTyping())
   }
 }
