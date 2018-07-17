c4976888dbe3d11efcd89aee5cb0ff2de1b2aba6
perronosaurio <waxtz@hotmail.com>
2018-07-16 20:53:33 -0400

    fixed

--- src/utils/LyricsUtils.js
+++ src/utils/LyricsUtils.js
@@ -1,18 +1,18 @@
-const snekfetch = require("snekfetch")
-const cheerio = require("cheerio")
+const snekfetch = require('snekfetch')
+const cheerio = require('cheerio')
 const key = process.env.GENIUS_API
 
 module.exports = class Lyrics {
-  static async getLyrics(path) {
+  static async getLyrics (path) {
     return snekfetch.get(`https://api.genius.com/${path}`)
-     .set("Authorization", `Bearer ${key}`)
-     .then(res => res.body)
-     .catch(err => console.err)
+      .set('Authorization', `Bearer ${key}`)
+      .then(res => res.body)
+      .catch(err => throw new Error(err))
   }
-  static loadLyrics(url) {
+  static loadLyrics (url) {
     return snekfetch.get(url).then(res => {
       const $ = cheerio.load(res.body)
-      return $(".lyrics") ? $(".lyrics").text().trim() : null
+      return $('.lyrics') ? $('.lyrics').text().trim() : null
     })
   }
 }
