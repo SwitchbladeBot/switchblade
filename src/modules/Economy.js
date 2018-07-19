9a66e84e8d3f7e8376bf4605aa4d52d72aaa707b
David Paiva <pillgp@icloud.com>
2018-07-19 16:01:13 +0100

    add missing 's'

--- src/modules/Economy.js
+++ src/modules/Economy.js
@@ -4,7 +4,7 @@ module.exports = class Economy extends Module {
   constructor (client) {
     super(client)
     this.name = 'economy'
-    this.requireDatabase = true
+    this.requiresDatabase = true
   }
 
   load () {
