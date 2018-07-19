3639465035953a742f8ab2de6468ab480a04590f
Doges <31023616+Doges@users.noreply.github.com>
2018-07-19 18:43:27 -0300

    Add files via upload

--- src/commands/social/favcolor.js
+++ src/commands/social/favcolor.js
@@ -18,17 +18,17 @@ module.exports = class Favcolor extends Command {
     // Regex
     const REGEX_PATH = /^#(([0-9a-f]){3}){1,2}$/i
     const hexcode = hexcolor.match(REGEX_PATH)
-    if(hexcode == null) {
-        embed.setColor(Constants.ERROR_COLOR)
+    if (hexcode == null) {
+      embed.setColor(Constants.ERROR_COLOR)
         .setTitle(t('commands:favcolor.invalidHex'))
     } else {
-        // Database
-        const userData = await this.client.database.users.get(author.id)
-        const favColor = userData.favColor
-        userData.favColor = hexcode.input
-        userData.save()
-        embed
-        .setTitle(t('commands:favcolor.changedSuccessfully'))
+      // Database
+      const userData = await this.client.database.users.get(author.id)
+      const favColor = userData.favColor
+      userData.favColor = hexcode.input
+      userData.save()
+      embed
+        .setTitle(t('commands:favcolor.changedSuccessfully') + favColor)
     }
     channel.send(embed).then(() => channel.stopTyping())
   }
