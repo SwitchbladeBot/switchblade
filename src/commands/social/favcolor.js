1e25bd9073c00721095981a6afd0f0fb664a2326
Doges <31023616+Doges@users.noreply.github.com>
2018-07-19 16:28:56 -0300

    first things first

---
+++ src/commands/social/favcolor.js
@@ -0,0 +1,34 @@
+const { CommandStructures, SwitchbladeEmbed, Constants } = require('../../')
+const { Command, CommandParameters, StringParameter } = CommandStructures
+
+module.exports = class Favcolor extends Command {
+  constructor (client) {
+    super(client)
+    this.name = 'favcolor'
+    this.aliases = ['favoritecolor', 'sethex', 'setcolor']
+
+    this.parameters = new CommandParameters(this,
+      new StringParameter({full: true, missingError: 'commands:favcolor.invalidhex'})
+    )
+  }
+
+  async run ({ t, author, channel }, hexcolor) {
+    const embed = new SwitchbladeEmbed(author)
+    channel.startTyping()
+    // Regex
+    const REGEX_PATH = /^#(([0-9a-f]){3}){1,2}$/i
+    const hexcode = hexcolor.match(REGEX_PATH)
+    if(hexcode == null) {
+        embed.setColor(Constants.ERROR_COLOR)
+        .setTitle(t('commands:favcolor.invalidHex'))
+    } else {
+        // Database
+        const userData = await this.client.database.users.get(author.id)
+        const favColor = userData.favColor
+        userDoc.lastDaily = hexcode.input
+        userDoc.save()
+        .setTitle(t('commands:favcolor.changedSuccessfully'))
+    }
+    channel.send(embed).then(() => channel.stopTyping())
+  }
+}
