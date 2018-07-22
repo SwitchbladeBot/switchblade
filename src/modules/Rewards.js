f2c11b2bc4b1ce42373c9a95727578b8cf4dcd05
davipatury <davi.patury@yahoo.com.br>
2018-07-21 23:18:27 -0300

    fuck me

--- src/modules/Rewards.js
+++ src/modules/Rewards.js
@@ -16,7 +16,7 @@ class Rewards extends Module {
   async collectListcord ({ user, doc }) {
     doc = doc || await this.client.database.users.get(user.id)
 
-    const { lastBonus: lastListcordBonusClaim } = doc
+    const { lastListcordBonusClaim: lastBonus } = doc
     const now = Date.now()
     const LISTCORD_INTERVAL = this.constructor.LISTCORD_INTERVAL
     if (now - lastBonus < LISTCORD_INTERVAL) {
