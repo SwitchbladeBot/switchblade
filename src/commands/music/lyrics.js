814e8f2a8dfce124da3446330b180eeeab130ced
perronosaurio <waxtz@hotmail.com>
2018-07-21 22:23:04 -0400

    Update some shit for the lyrics command

--- src/commands/music/lyrics.js
+++ src/commands/music/lyrics.js
@@ -1,8 +1,6 @@
 const { CommandStructures, SwitchbladeEmbed, Constants } = require('../../')
 const { Command, CommandParameters, StringParameter } = CommandStructures
 
-const lyrics = require('../../utils/LyricsUtils.js')
-
 module.exports = class Lyrics extends Command {
   constructor (client) {
     super(client)
@@ -17,13 +15,13 @@ module.exports = class Lyrics extends Command {
   async run ({ t, author, channel }, song) {
     channel.startTyping()
     const embed = new SwitchbladeEmbed(author)
-    const request = await lyrics.getLyrics(`search?q=${song}`)
+    const request = await this.client.apis.genius.findTrack(song)
     const data = request.response.hits[0]
     if (data) {
       const img = data.result.song_art_image_thumbnail_url
       const extendedsong = data.result.title_with_featured
       const artist = data.result.primary_artist.name
-      const body = await lyrics.loadLyrics(data.result.url)
+      const body = await this.client.apis.genius.loadLyrics(data.result.url)
 
       embed.setAuthor('Genius', 'https://cdn.discordapp.com/attachments/445647209892020234/467733917798236202/b7b6ada09725d7a97a01bbef815b74d2.png')
         .setDescription(body.length >= 1900 ? `${body.substr(0, 1900)}\n\n[Click here to see the full lyrics](http://genius.com${data.result.path})` : body)
