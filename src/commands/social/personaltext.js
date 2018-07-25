82419b0a9227cd2c8379fa73451d56e235aecc67
Doges <31023616+Doges@users.noreply.github.com>
2018-07-25 18:31:01 -0300

    personaltext

---
+++ src/commands/social/personaltext.js
@@ -0,0 +1,31 @@
+const { CommandStructures, SwitchbladeEmbed, Constants } = require('../../')
+const { Command, CommandParameters, StringParameter } = CommandStructures
+
+module.exports = class Personaltext extends Command {
+  constructor (client) {
+    super(client)
+    this.name = 'personaltext'
+    this.aliases = ['profiletext']
+
+    this.parameters = new CommandParameters(this,
+      new StringParameter({full: true, missingError: 'commands:personaltext.noText'})
+    )
+  }
+
+  async run ({ t, author, channel }, profileText) {
+    const embed = new SwitchbladeEmbed(author)
+    channel.startTyping()
+    if (profileText > 150) {
+      embed.setColor(Constants.ERROR_COLOR)
+        .setTitle(t('commands:personaltext.tooLongText'))
+    } else {
+      // Database
+      const userData = await this.client.database.users.get(author.id)
+      userData.personalText = profileText
+      userData.save()
+      embed
+        .setTitle(t('commands:personaltext.changedSuccessfully') + profileText)
+    }
+    channel.send(embed).then(() => channel.stopTyping())
+  }
+}
