d94adbe462fff3b6b022f860bea59710ce7bb982
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-07-31 16:49:07 -0300

    Error handling

--- src/commands/anime/whatanime.js
+++ src/commands/anime/whatanime.js
@@ -24,53 +24,59 @@ module.exports = class WhatAnime extends Command {
     channel.startTyping()
     const embed = new SwitchbladeEmbed(author)
     const imageUrl = urlFromParams || await lastImageFromChannel(channel)
-    const image = await snekfetch.get(imageUrl)
 
-    try {
-      if (image.body.length < sizeLimit) {
-        const response = await snekfetch.post(`https://whatanime.ga/api/search?token=${process.env.WHATANIME_API_KEY}`).attach('image', image.body.toString('base64'))
-        if (response.body.docs && response.body.docs[0].similarity > 0.80) {
-          const bestMatch = response.body.docs[0]
+    if (imageUrl) {
+      if (new RegExp(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/).exec(imageUrl)) {
+        const image = await snekfetch.get(imageUrl)
+        try {
+          if (image.body.length < sizeLimit) {
+            const response = await snekfetch.post(`https://whatanime.ga/api/search?token=${process.env.WHATANIME_API_KEY}`).attach('image', image.body.toString('base64'))
+            if (response.body.docs && response.body.docs[0].similarity > 0.80) {
+              const bestMatch = response.body.docs[0]
 
-          if (!(bestMatch.is_adult && !channel.nsfw)) {
-            embed
-              .setTitle(t('commands:whatanime.matchFound'))
-              .setImage(`https://whatanime.ga/thumbnail.php?anilist_id=${bestMatch.anilist_id}&file=${encodeURIComponent(bestMatch.filename)}&t=${bestMatch.at}&token=${bestMatch.tokenthumb}`)
-              .setDescription([
-                t('commands:whatanime.matchName', {animeName: bestMatch.title_romaji, episodeNumber: bestMatch.episode}),
-                t('commands:whatanime.matchDuration', {matchStart: moment.duration(bestMatch.from * 1000).format('h[h] m[m] s[s]'), matchEnd: moment.duration(bestMatch.to * 1000).format('h[h] m[m] s[s]')}),
-                '',
-                `[AniList](https://anilist.co/anime/${bestMatch.anilist_id}) - [MyAnimeList](https://myanimelist.net/anime/${bestMatch.mal_id})`
-              ].join('\n'))
+              if (!(bestMatch.is_adult && !channel.nsfw)) {
+                embed
+                  .setTitle(t('commands:whatanime.matchFound'))
+                  .setImage(`https://whatanime.ga/thumbnail.php?anilist_id=${bestMatch.anilist_id}&file=${encodeURIComponent(bestMatch.filename)}&t=${bestMatch.at}&token=${bestMatch.tokenthumb}`)
+                  .setDescription([
+                    t('commands:whatanime.matchName', {animeName: bestMatch.title_romaji, episodeNumber: bestMatch.episode}),
+                    t('commands:whatanime.matchDuration', {matchStart: moment.duration(bestMatch.from * 1000).format('h[h] m[m] s[s]'), matchEnd: moment.duration(bestMatch.to * 1000).format('h[h] m[m] s[s]')}),
+                    '',
+                    `[AniList](https://anilist.co/anime/${bestMatch.anilist_id}) - [MyAnimeList](https://myanimelist.net/anime/${bestMatch.mal_id})`
+                  ].join('\n'))
+              } else {
+                embed
+                  .setTitle()
+                  .setDescription(t('commands:whatanime.channelIsNotNSFWDescription'))
+              }
+            } else {
+              embed.setTitle(t('commands:whatanime.noMatchFound'))
+            }
           } else {
             embed
-              .setTitle()
-              .setDescription(t('commands:whatanime.channelIsNotNSFWDescription'))
+              .setTitle(t('commands:whatanime.imageTooBigTitle'))
+              .setDescription(t('commands:whatanime.imageTooBigDescription', {sizeLimit: sizeLimitHumanReadable}))
           }
-        } else {
-          embed.setTitle('Not found')
+        } catch (e) {
+          embed.setColor(Constants.ERROR_COLOR)
+          switch (e.message) {
+            case '429 Too Many Requests':
+              embed
+                .setTitle(t('commands:whatanime.rateLimitExceededTitle'))
+                .setDescription(t('commands:whatanime.rateLimitExceededDescription'))
+              break
+            case '413 Request Entity Too Large':
+              embed
+                .setTitle(t('commands:whatanime.imageTooBigTitle'))
+                .setDescription(t('commands:whatanime.imageTooBigDescription', {sizeLimit: sizeLimitHumanReadable}))
+              break
+          }
         }
       } else {
-        embed
-          .setTitle(t('commands:whatanime.imageTooBigTitle'))
-          .setDescription(t('commands:whatanime.imageTooBigDescription', {sizeLimit: sizeLimitHumanReadable}))
+        embed.setTitle(t('commands:whatanime.invalidUrl'))
       }
-    } catch (e) {
-      embed.setColor(Constants.ERROR_COLOR)
-      console.log(e.message)
-
-      switch (e.message) {
-        case '429 Too Many Requests':
-          embed
-            .setTitle(t('commands:whatanime.rateLimitExceededTitle'))
-            .setDescription(t('commands:whatanime.rateLimitExceededDescription'))
-          break
-        case '413 Request Entity Too Large':
-          embed
-            .setTitle(t('commands:whatanime.imageTooBigTitle'))
-            .setDescription(t('commands:whatanime.imageTooBigDescription', {sizeLimit: sizeLimitHumanReadable}))
-          break
-      }
+    } else {
+      embed.setTitle(t('commands:whatanime.noImage'))
     }
 
     channel.send(embed).then(() => channel.stopTyping())
@@ -79,5 +85,10 @@ module.exports = class WhatAnime extends Command {
 
 async function lastImageFromChannel (channel) {
   const channelMessages = await channel.fetchMessages()
-  return channelMessages.find(m => m.attachments.filter(a => a.height).first()).attachments.first().url
+  const message = channelMessages.find(m => m.attachments.filter(a => a.height).first())
+  if (message) {
+    return message.attachments
+  } else {
+    return null
+  }
 }
