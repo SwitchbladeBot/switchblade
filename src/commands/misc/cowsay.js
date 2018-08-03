677485277183f19f76cbf1efe402f1c19aee4ad4
LogGame-HU-DEaN <35978189+LogGame-HU-DEaN@users.noreply.github.com>
2018-08-03 12:34:43 +0200

    travis fix

--- src/commands/misc/cowsay.js
+++ src/commands/misc/cowsay.js
@@ -7,8 +7,8 @@ module.exports = class Cowsay extends Command {
     super(client)
     this.name = 'cowsay'
     this.aliases = ['cs']
-	
-	this.parameters = new CommandParameters(this,
+
+    this.parameters = new CommandParameters(this,
       new StringParameter({full: true, missingError: 'commands:cowsay.noText'})
     )
   }
