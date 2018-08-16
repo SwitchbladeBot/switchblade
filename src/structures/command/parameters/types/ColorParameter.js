03d11d6e2d0f129a357a890a942bfa6a7b4594ad
davipatury <davi.patury@yahoo.com.br>
2018-08-16 19:37:07 -0300

    Standard fixes

--- src/structures/command/parameters/types/ColorParameter.js
+++ src/structures/command/parameters/types/ColorParameter.js
@@ -1,11 +1,13 @@
 const Parameter = require('./Parameter.js')
 const CommandError = require('../../CommandError.js')
 const Color = require('../../../../utils/Color.js')
- module.exports = class ColorParameter extends Parameter {
+
+module.exports = class ColorParameter extends Parameter {
   constructor (options = {}) {
     super(options)
   }
-   parse (arg, { t }) {
+
+  parse (arg, { t }) {
     const color = new Color(arg)
     return color.valid ? color : new CommandError(t('errors:invalidColor'))
   }
