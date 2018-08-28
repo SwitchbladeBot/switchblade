ed83c3b7fb8ce90725d99dc39110b957c21c497f
Doges <31023616+Doges@users.noreply.github.com>
2018-08-28 17:37:06 -0300

    Update Schemas.js

--- src/database/mongo/Schemas.js
+++ src/database/mongo/Schemas.js
@@ -9,6 +9,7 @@ module.exports = {
     globalXp: { type: Number, default: 0 },
     personalText: { type: String, default: 'Do you know that you can edit this in the future dashboard? :o' },
     blacklisted: {type: Boolean, default: false},
+    favColor: { type: String, default: process.env.EMBED_COLOR },
     rep: { type: Number, default: 0 },
     lastRep: { type: Number, default: 0 }
   }),
