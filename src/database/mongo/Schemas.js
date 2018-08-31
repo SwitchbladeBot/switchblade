f71696dbe3448299e765fb6d84867365c8199a91
Lireoy <34489456+Lireoy@users.noreply.github.com>
2018-09-01 01:53:18 +0200

    Update Schemas.js

--- src/database/mongo/Schemas.js
+++ src/database/mongo/Schemas.js
@@ -7,7 +7,7 @@ module.exports = {
     money: { type: Number, default: 0 },
     lastDaily: { type: Number, default: 0 },
     globalXp: { type: Number, default: 0 },
-    personalText: { type: String, default: 'Do you know that you can edit this in the future dashboard or using the personaltext command? :o' },
+    personalText: { type: String, default: 'Did you know you can edit this in the future dashboard or using the personaltext command? :o' },
     blacklisted: {type: Boolean, default: false},
     favColor: { type: String, default: process.env.EMBED_COLOR },
     rep: { type: Number, default: 0 },
