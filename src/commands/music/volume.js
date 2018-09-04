438374701fa1e4c0e6f17ca886f1bb5b41287089
Doges <31023616+Doges@users.noreply.github.com>
2018-09-04 17:47:31 -0300

    Add files via upload

--- src/commands/music/volume.js
+++ src/commands/music/volume.js
@@ -20,6 +20,7 @@ module.exports = class Volume extends Command {
     if (volume === maxVolume) embed.setImage('https://i.imgur.com/A6HWTqq.png')
     const guildPlayer = this.client.playerManager.get(guild.id)
     guildPlayer.volume(volume)
-    channel.send(embed.setTitle(`\uD83D\uDD0A ${t('commands:volume.volumeSet', {volume})}`))
+    embed.setTitle(`\uD83D\uDD0A ${t('commands:volume.volumeSet', {volume})}`)
+    channel.send(embed)
   }
 }
