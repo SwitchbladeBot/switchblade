a93693c5fe8e0499f5d41279406d3072f0178338
beescuit <rafael@beecoding.net>
2018-09-07 21:28:32 -0300

    Add guildicon command

---
+++ src/commands/misc/guildicon.js
@@ -0,0 +1,23 @@
+const { CommandStructures, SwitchbladeEmbed } = require('../../')
+const { Command, CommandRequirements } = CommandStructures
+
+module.exports = class GuildIcon extends Command {
+  constructor (client) {
+    super(client)
+    this.name = 'guildicon'
+    this.aliases = ['gicon', 'sicon', 'srvicn', 'servericon']
+    this.requirements = new CommandRequirements(this, { guildOnly: true })
+  }
+
+  run ({ t, author, channel, guild }) {
+    const embed = new SwitchbladeEmbed(author)
+    channel.startTyping()
+    if (guild.iconURL) {
+      embed.setImage(guild.iconURL)
+        .setDescription(t('commands:guildicon.iconDescription', {guild: guild.name}))
+    } else {
+      embed.setDescription(t('commands:guildicon.noIcon'))
+    }
+    channel.send(embed).then(() => channel.stopTyping())
+  }
+}
