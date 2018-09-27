5f9e518b9db7a4a866f7fec57d0ca470113a4d33
Doges <31023616+Doges@users.noreply.github.com>
2018-09-27 15:18:31 -0300

    f it v2

--- src/commands/misc/guildicon.js
+++ src/commands/misc/guildicon.js
@@ -1,5 +1,5 @@
-const { CommandStructures, SwitchbladeEmbed, Constants } = require('../../')
-const { Command, CommandRequirements } = CommandStructures
+const { CommandStructures, SwitchbladeEmbed, Constants, CommandParameters } = require('../../')
+const { Command, CommandRequirements, GuildParameter } = CommandStructures
 
 module.exports = class GuildIcon extends Command {
   constructor (client) {
@@ -7,11 +7,16 @@ module.exports = class GuildIcon extends Command {
     this.name = 'guildicon'
     this.aliases = ['gicon', 'sicon', 'srvicn', 'servericon']
     this.requirements = new CommandRequirements(this, { guildOnly: true })
+
+    this.parameters = new CommandParameters(this,
+      new GuildParameter({full: true, required: false})
+    )
   }
 
-  run ({ t, author, channel, guild }) {
+  run ({ t, author, channel }, guild) {
     const embed = new SwitchbladeEmbed(author)
     channel.startTyping()
+    guild = guild || channel.guild
     if (guild.iconURL) {
       embed.setImage(guild.iconURL)
         .setDescription(t('commands:guildicon.iconDescription', {guild: guild.name}))
