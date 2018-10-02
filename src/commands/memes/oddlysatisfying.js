865b43ff5eaedeebc6dbeaa81337fa5f21ee0caf
LogGame-HU-DEaN <35978189+LogGame-HU-DEaN@users.noreply.github.com>
2018-10-02 19:22:22 +0200

    Create oddlysatisfying.js

---
+++ src/commands/memes/oddlysatisfying.js
@@ -0,0 +1,19 @@
+const { Command, Reddit, SwitchbladeEmbed } = require('../../')
+
+module.exports = class OddlySatisfying extends Command {
+  constructor (client) {
+    super(client)
+    this.name = 'oddlysatisfying'
+    this.aliases = ['odds']
+  }
+
+  async run ({ author, channel }) {
+    const embed = new SwitchbladeEmbed(author)
+    channel.startTyping()
+    const { url, title, permalink } = await Reddit.getRandomPostFromSubreddit('/r/oddlysatisfying')
+    embed.setTitle(title)
+      .setImage(url)
+      .setURL(`https://reddit.com${permalink}`)
+    channel.send(embed).then(() => channel.stopTyping())
+  }
+}
