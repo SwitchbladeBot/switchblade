36cad99e859a14c7560a06c6057cf0cd97c4ecc2
Logan Arnett <logan@loganarnett.com>
2018-10-02 13:14:26 -0400

    adding in lmgtfy command

---
+++ src/commands/misc/lmgtfy.js
@@ -0,0 +1,21 @@
+const { CommandStructures, SwitchbladeEmbed, Constants } = require('../../')
+const { Command, CommandParameters, StringParameter } = CommandStructures
+
+module.exports = class LMGTFY extends Command {
+  constructor (client) {
+    super(client)
+    this.name = 'lmgtfy'
+    this.aliases = ['letmegooglethatforyou']
+
+    this.parameters = new CommandParameters(this,
+      new StringParameter({full: true, missingError: 'commands:lmgtfy.noQuery'})
+    )
+  }
+
+  run ({ t, channel, author }, query) {
+    const embed = new SwitchbladeEmbed(author)
+    channel.startTyping()
+    embed.setDescription(t('commands:lmgtfy.search', { link: `https://lmgtfy.com/?q=${encodeURIComponent(query)}` }))
+    channel.send(embed.setColor('#4285F4')).then(() => channel.stopTyping())
+  }
+}
