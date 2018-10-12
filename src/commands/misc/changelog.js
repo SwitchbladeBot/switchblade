6cca25cf36446c0ca5e840244b02e16c640cbcee
Doges <31023616+Doges@users.noreply.github.com>
2018-10-12 18:14:30 -0300

    Add files via upload

--- src/commands/misc/changelog.js
+++ src/commands/misc/changelog.js
@@ -5,20 +5,20 @@ module.exports = class Changelog extends Command {
   constructor (client) {
     super(client)
     this.name = 'changelog'
-    this.aliases = ['recentcommits']
+    this.aliases = ['commits']
   }
 
   async run ({ t, author, channel }) {
     const embed = new SwitchbladeEmbed(author)
     channel.startTyping()
-    const { body } = await snekfetch.get('https://api.github.com/repos/switchbladebot/switchblade/commits')
+    const { body } = await snekfetch.get('https://api.github.com/repos/switchbladebot/switchblade/commits ')
     const commits = body.slice(0, 10)
     embed.setTitle(`[switchblade:dev] ${t('commands:changelog.latest')}`)
-  		.setURL(`https://github.com/switchbladebot/switchblade/commits/dev`)
-  		.setDescription(commits.map(c => {
-  			let h = `[\`${c.sha.slice(0, 7)}\`](${c.html_url})`
-  			return `${h} ${this.shorten(c.commit.message.split('\n')[0], 50)} - ${c.author.login}`
-  		}).join('\n'))
+      .setURL(`https://github.com/switchbladebot/switchblade/commits/dev`)
+      .setDescription(commits.map(c => {
+        let h = `[\`${c.sha.slice(0, 7)}\`](${c.html_url})`
+        return `${h} ${this.shorten(c.commit.message.split('\n')[0], 50)} - ${c.author.login}`
+      }).join('\n'))
     channel.send(embed).then(() => channel.stopTyping())
   }
 
