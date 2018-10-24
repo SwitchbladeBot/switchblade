49b2b50cd9a36470c1accff4ede15cfcedb24b4e
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-10-24 19:23:27 -0300

    Translation!

--- src/commands/misc/videochat.js
+++ src/commands/misc/videochat.js
@@ -8,7 +8,7 @@ module.exports = class VideoChat extends Command {
     this.requirements = new CommandRequirements(this, {guildOnly: true, voiceChannelOnly: true})
   }
 
-  run ({ channel, guild, voiceChannel }) {
-    channel.send(`**Click on this link to join the video chat:** \`(You must already be in ${voiceChannel.name})\`\n<https://canary.discordapp.com/channels/${guild.id}/${voiceChannel.id}>`)
+  run ({ t, channel, guild, voiceChannel }) {
+    channel.send(`${t('commands:videochat.text', {channelName: voiceChannel.name})}\n<https://canary.discordapp.com/channels/${guild.id}/${voiceChannel.id}>`)
   }
 }
