11df69ccdad5a0c0ec518adb738c071ebeca7945
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-10-24 19:41:02 -0300

    why u fail

--- src/commands/misc/videochat.js
+++ src/commands/misc/videochat.js
@@ -9,6 +9,6 @@ module.exports = class VideoChat extends Command {
   }
 
   run ({ t, channel, guild, voiceChannel }) {
-    channel.send(`${t('commands:videochat.text', {channelName: voiceChannel.name})}\n<https://canary.discordapp.com/channels/${guild.id}/${voiceChannel.id}>`)
+    channel.send(`${t('commands:videochat.text', { channelName: voiceChannel.name })}\n<https://canary.discordapp.com/channels/${guild.id}/${voiceChannel.id}>`)
   }
 }
