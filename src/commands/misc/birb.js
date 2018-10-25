6c8d9aa74652ed0930c6adef08d3bec215632a90
ThePsionic <me@psi.codes>
2018-10-25 11:11:50 +0200

    Add birb command Closes #406

---
+++ src/commands/misc/birb.js
@@ -0,0 +1,19 @@
+const { Command, SwitchbladeEmbed } = require('../../')
+const snekfetch = require('snekfetch')
+
+module.exports = class Birb extends Command {
+  constructor (client) {
+    super(client)
+    this.name = 'birb'
+    this.aliases = ['bird', 'borb']
+  }
+
+  async run ({ t, author, channel }) {
+    const embed = new SwitchbladeEmbed(author)
+    channel.startTyping()
+    const { body } = await snekfetch.get('http://random.birb.pw/tweet.json/')
+    embed.setImage('http://random.birb.pw/img/' + body.file)
+      .setDescription(t('commands:birb.hereIsYourBirb'))
+    channel.send(embed).then(() => channel.stopTyping())
+  }
+}
