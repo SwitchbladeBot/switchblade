c15393b841581eb465a18ad97b24f00c35b05269
ThePsionic <me@psi.codes>
2018-10-25 10:59:32 +0200

    Add fox command Closes #407

---
+++ src/commands/misc/fox.js
@@ -0,0 +1,18 @@
+const { Command, SwitchbladeEmbed } = require('../../')
+const snekfetch = require('snekfetch')
+
+module.exports = class Fox extends Command {
+  constructor (client) {
+    super(client)
+    this.name = 'fox'
+  }
+
+  async run ({ t, author, channel }) {
+    const embed = new SwitchbladeEmbed(author)
+    channel.startTyping()
+    const { body } = await snekfetch.get('https://randomfox.ca/floof/')
+    embed.setImage(body.image)
+      .setDescription(t('commands:fox.hereIsYourFox'))
+    channel.send(embed).then(() => channel.stopTyping())
+  }
+}
