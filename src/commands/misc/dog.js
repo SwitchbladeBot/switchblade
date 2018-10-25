82ba50d16e37263a02931cbf151b1bc386d954d8
Doges <31023616+Doges@users.noreply.github.com>
2018-10-25 09:26:39 -0300

    Update dog.js

--- src/commands/misc/dog.js
+++ src/commands/misc/dog.js
@@ -6,6 +6,7 @@ module.exports = class Dog extends Command {
     super(client)
     this.name = 'dog'
     this.aliases = ['doggo', 'dogpics', 'randomdog']
+    this.category = 'misc'
   }
 
   async run ({ t, author, channel }) {
