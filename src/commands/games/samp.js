1020e5c233df8857f286b144d2267157cd72716a
Doges <31023616+Doges@users.noreply.github.com>
2018-11-02 10:10:21 -0300

    Add files via upload

--- src/commands/games/samp.js
+++ src/commands/games/samp.js
@@ -16,10 +16,11 @@ module.exports = class SAMP extends Command {
   async run ({ t, author, channel }, address) {
     const embed = new SwitchbladeEmbed(author)
     channel.startTyping()
-    const [ host, port = 7777 ] = address.split(':')
+    const host = address.split(':')[0]
+    const port = address.split(':')[1] || 7777
     try {
       const response = await this.queryPromise({ host, port })
-      embed.setAuthor('San Andreas: Multiplayer', 'https://i.imgur.com/QYeGxrV.png')
+      embed.setAuthor('San Andreas: Multiplayer', 'http://1.bp.blogspot.com/-AY3d9tMV8nM/Ton8LlgoJgI/AAAAAAAAAMg/VFID_9mI-Co/s1600/SAMP.png')
         .setColor(0xF07B0F)
         .setTitle(response.hostname)
         .setURL(this.cleanURL(response.rules.weburl))
@@ -30,28 +31,25 @@ module.exports = class SAMP extends Command {
         .addField(t('commands:samp.gameMode'), response.gamemode, true)
         .addField(t('commands:samp.time'), response.rules.worldtime, true)
     } catch (err) {
-      embed.setColor(Constants.ERROR_COLOR)
-      if (err === 'Host unavailable') {
-        embed.setTitle(t('commands:samp.serverUnreachableTitle'))
-          .setDescription(t('commands:samp.serverUnreachableDescription'))
-      } else {
-        embed.setTitle(t('errors:generic'))
-          .setDescription(`\`${err}\``)
-      }
+      embed
+        .setColor(Constants.ERROR_COLOR)
+        .setTitle(t('commands:samp.serverUnreachableTitle'))
+        .setDescription(t('commands:samp.serverUnreachableDescription'))
     }
     channel.send(embed).then(channel.stopTyping())
   }
 
-  async queryPromise (options) {
+  queryPromise (options) {
     return new Promise((resolve, reject) => {
       query(options, function (error, response) {
-        if (error) return reject(error)
-        resolve(response)
+        if (error) reject(error)
+        else resolve(response)
       })
     })
   }
 
   cleanURL (url) {
-    return /^https?:\/\//i.test(url) ? url : `http://${url}`
+    if (!/^https?:\/\//i.test(url)) return 'http://' + url
+    return url
   }
 }
