5e47a478a091bcdf123cc363edac109302cbe244
David Paiva <pillgp@icloud.com>
2018-11-02 14:12:23 +0000

    Added softban & reduced the message clear history days, because there's a 7 day limit

--- src/commands/moderation/softban.js
+++ src/commands/moderation/softban.js
@@ -1,30 +1,32 @@
 const { CommandStructures, Constants, SwitchbladeEmbed } = require('../../')
 const { Command, CommandRequirements, CommandParameters, StringParameter, MemberParameter } = CommandStructures
 
-module.exports = class Ban extends Command {
+module.exports = class Softban extends Command {
   constructor (client) {
     super(client)
-    this.name = 'ban'
-    this.aliases = ['banir']
+    this.name = 'softban'
+    this.aliases = ['softbanir']
     this.category = 'moderation'
 
     this.requirements = new CommandRequirements(this, { guildOnly: true, botPermissions: ['BAN_MEMBERS'], permissions: ['BAN_MEMBERS'] })
     this.parameters = new CommandParameters(this,
-      new MemberParameter({ acceptBot: true, missingError: 'commands:ban.missingUser' }),
-      new StringParameter({ full: true, missingError: 'commands:ban.missingReason' })
+      new MemberParameter({ acceptBot: true, missingError: 'commands:softban.missingUser' }),
+      new StringParameter({ full: true, missingError: 'commands:softban.missingReason' })
     )
   }
 
   async run ({ channel, guild, author, t }, member, reason) {
     const embed = new SwitchbladeEmbed(author)
-    await guild.ban(member, { days: 7, reason }).then(bannedMember => {
-      embed
-        .setTitle(t('commands:ban.successTitle'))
-        .setDescription(`${bannedMember} - \`${reason}\``)
+    await guild.ban(member, { days: 7, reason }).then(async bannedMember => {
+      await guild.unban(bannedMember).then(softbannedMember => {
+        embed
+          .setTitle(t('commands:softban.successTitle'))
+          .setDescription(`${softbannedMember} - \`${reason}\``)
+      })
     }).catch(err => {
       embed
         .setColor(Constants.ERROR_COLOR)
-        .setTitle(t('commands:ban.cantBan'))
+        .setTitle(t('commands:softban.cantSoftban'))
         .setDescription(`\`${err}\``)
     })
     channel.send(embed)
