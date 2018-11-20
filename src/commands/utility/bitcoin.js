4f6da9017f9813c9ad2846e61d112168c9adf6eb
Leander West Furumo <leanderfurumo@protonmail.com>
2018-11-20 19:13:53 +0000

    sorry guys

--- src/commands/utility/bitcoin.js
+++ src/commands/utility/bitcoin.js
@@ -17,4 +17,4 @@ module.exports = class Bitcoin extends Command {
     embed.setDescription('1 BTC = ' + body.data.amount + ' USD')
     channel.send(embed).then(() => channel.stopTyping())
   }
-} 
+}
