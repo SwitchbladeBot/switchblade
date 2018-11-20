fa75588ffba97e1c1437d44078668a78f7de62f8
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-11-20 00:23:00 -0300

    Check for all Lavalink variables

--- src/listeners/MainListener.js
+++ src/listeners/MainListener.js
@@ -12,7 +12,8 @@ module.exports = class MainListener extends EventListener {
     this.user.setPresence({ game: { name: `@${this.user.username} help` } })
 
     // Lavalink connection
-    if (process.env.LAVALINK_HOST) {
+    const lavalinkRequiredVariables = ['LAVALINK_HOST', 'LAVALINK_PORT', 'LAVALINK_PASSWORD']
+    if (lavalinkRequiredVariables.every(variable => !!process.env[variable])) {
       const nodes = [{
         'host': process.env.LAVALINK_HOST,
         'port': process.env.LAVALINK_PORT || '1337',
@@ -24,7 +25,7 @@ module.exports = class MainListener extends EventListener {
       })
       this.log('[92mLavalink connection established!', 'Music')
     } else {
-      this.log('[91mLavalink connection didn\'t establish - LAVALINK_HOST not set.', 'Music')
+      this.log(`[91mLavalink connection didn\'t establish - Rrequired environment variable(s) (${lavalinkRequiredVariables.filter(variable => !process.env[variable]).join(', ')}) not set.`, 'Music')
     }
 
     // TODO: Make stat posters modular
