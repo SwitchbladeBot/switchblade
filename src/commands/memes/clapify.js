70adf56d29b4408b25187243eca26f8256a00d83
Doges <xdoges@idonthave.life>
2018-11-24 12:57:00 -0200

    clapify fix

--- src/commands/memes/clapify.js
+++ src/commands/memes/clapify.js
@@ -1,7 +1,8 @@
-const { CommandStructures, SwitchbladeEmbed } = require('../../index')
+const { CommandStructures, SwitchbladeEmbed, Constants } = require('../../index')
 const { Command, CommandParameters, StringParameter } = CommandStructures
 
 const emoji = '\uD83D\uDC4F'
+const CLAPIFY_LIMIT = '128'
 
 module.exports = class Clapify extends Command {
   constructor (client) {
@@ -16,7 +17,13 @@ module.exports = class Clapify extends Command {
 
   async run ({ t, author, channel }, text) {
     const embed = new SwitchbladeEmbed(author)
-    embed.setTitle(`${emoji} ${text.toUpperCase().split(' ').join(` ${emoji} `)} ${emoji}`)
+    if (text.length > CLAPIFY_LIMIT) {
+      embed
+        .setTitle(t('commands:clapify.tooLongText', { limit: CLAPIFY_LIMIT }))
+        .setColor(Constants.ERROR_COLOR)
+    } else {
+      embed.setTitle(`${emoji} ${text.toUpperCase().split(' ').join(` ${emoji} `)} ${emoji}`)
+    }
     channel.send(embed)
   }
 }
