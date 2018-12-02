0dea9d8d1f01d2ccfd8be85067bc0f55d5c200e7
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-12-02 00:31:05 -0300

    Davi's suggestion

--- src/listeners/AutoModerator.js
+++ src/listeners/AutoModerator.js
@@ -8,11 +8,11 @@ module.exports = class MainListener extends EventListener {
 
   async onGuildMemberAdd (member) {
     const guildDocument = this.database && await this.database.guilds.findOne(member.guild.id)
-    const t = this.i18next.getFixedT(guildDocument.language)
     const guild = member.guild
 
     // TODO: Write a function to parse {placeholders} like the one used below
     if (guildDocument && guildDocument.joinLock && guild.me.hasPermission('KICK_MEMBERS') && member.kickable) {
+      const t = this.i18next.getFixedT(guildDocument.language)
       member.send(guildDocument.joinLockMessage ? guildDocument.joinLockMessage.replace('{server}', guild.name) : t('moderation:joinLock.defaultPrivateMessage', { guild })).catch(() => {})
       member.kick(t('moderation:joinLock.kickReason'))
     }
