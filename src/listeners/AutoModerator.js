63924ef8bb8f92d2fb3bf9c7692ad957e39d1a20
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-12-02 00:25:47 -0300

    Updates woo

--- src/listeners/AutoModerator.js
+++ src/listeners/AutoModerator.js
@@ -9,8 +9,12 @@ module.exports = class MainListener extends EventListener {
   async onGuildMemberAdd (member) {
     const guildDocument = this.database && await this.database.guilds.findOne(member.guild.id)
     const t = this.i18next.getFixedT(guildDocument.language)
+    const guild = member.guild
 
-    // Join Lock
-    if (guildDocument && guildDocument.joinLock && member.guild.me.hasPermission('KICK_MEMBERS') && member.kickable) member.kick(t('moderation:reasons.joinLockEnabled'))
+    // TODO: Write a function to parse {placeholders} like the one used below
+    if (guildDocument && guildDocument.joinLock && guild.me.hasPermission('KICK_MEMBERS') && member.kickable) {
+      member.send(guildDocument.joinLockMessage && guildDocument.joinLockMessage.replace('{server}', guild.name) || t('moderation:joinLock.defaultPrivateMessage', {guild})).catch(() => {})
+      member.kick(t('moderation:joinLock.kickReason'))
+    }
   }
 }
