acd3282514aa52434039facc16d4ac0ae462c71b
Tsugami <yslan3000@gmail.com>
2018-12-23 13:49:45 -0200

    Update index.js

--- src/utils/CanvasTemplates/index.js
+++ src/utils/CanvasTemplates/index.js
@@ -1,8 +1,7 @@
 module.exports = {
-    ButtSlap: require('./ButtSlap.js').render,
-    
-    tiggered: require("./Triggered.js").render,
-    profile: require("./Profile.js").render,
-    nowPlaying: require("./NowPlaying.js").render,
-    leaderboard: require("./LeaderBoard.js").render
+  ButtSlap: require('./ButtSlap.js').render,
+  triggered: require('./Triggered.js').render,
+  profile: require('./Profile.js').render,
+  nowPlaying: require('./NowPlaying.js').render,
+  leaderboard: require('./LeaderBoard.js').render
 }
