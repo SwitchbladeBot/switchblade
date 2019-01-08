472314d5c44084dc93d5eff085f540b6807a6948
davipatury <davi.patury@yahoo.com.br>
2019-01-08 21:29:07 -0200

    Queue clear. Closes #552

--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -9,26 +9,51 @@ module.exports = class Queue extends Command {
     this.aliases = ['playlist']
     this.category = 'music'
 
+    this.subcommands = [ new QueueClear(client, this) ]
     this.requirements = new CommandRequirements(this, { guildOnly: true, guildPlaying: true })
   }
 
-  async run ({ t, author, channel, guild }) {
-    const guildPlayer = this.client.playerManager.get(guild.id)
+  async run ({ t, aliase, author, channel, guild, prefix }) {
+    const { playingSong, queue } = this.client.playerManager.get(guild.id)
     const embed = new SwitchbladeEmbed(author)
 
-    const npSong = guildPlayer.playingSong
+    const npSong = playingSong
     const description = [`**${t('music:nowPlaying')}** [${npSong.title}](${npSong.uri})`]
 
-    if (guildPlayer.queue.length > 0) {
-      const queue = guildPlayer.queue.map((song, i) => `${i + 1}. [${song.title}](${song.uri}) *(${t('music:addedBy', { user: song.requestedBy })})*`).slice(0, MAX_PLAYLIST_LENGTH)
-      description.push('', `**${t('music:queue')}:**`, ...queue)
+    if (queue.length > 0) {
+      const queueParsed = queue.slice(0, MAX_PLAYLIST_LENGTH).map((song, i) => `${i + 1}. [${song.title}](${song.uri}) *(${t('music:addedBy', { user: song.requestedBy })})*`)
+      description.push('', `**${t('music:queue')}:**`, ...queueParsed)
 
-      const missing = guildPlayer.queue.length - MAX_PLAYLIST_LENGTH
+      const missing = queue.length - MAX_PLAYLIST_LENGTH
       if (missing > 0) description.push(`\`${t('music:andMore', { missing })}\``)
+
+      description.push('', t('commands:queue.clearAdvice', { usage: `${prefix}${aliase} clear` }))
     } else {
       description.push(t('music:noneAfterCurrent'))
     }
 
     channel.send(embed.setDescription(description.join('\n')))
+  }
+}
+
+class QueueClear extends Command {
+  constructor (client, parentCommand) {
+    super(client, parentCommand)
+    this.name = 'clear'
+    this.aliases = ['cl']
+  }
+
+  async run ({ t, author, channel, guild }) {
+    const { queue } = this.client.playerManager.get(guild.id)
+    const embed = new SwitchbladeEmbed(author)
+
+    if (queue.length > 0) {
+      queue.splice(0, queue.length)
+      embed.setTitle(t(`commands:${this.tPath}.queueCleared`))
+    } else {
+      embed.setTitle(t('music:noneAfterCurrent'))
+    }
+
+    channel.send(embed)
   }
 }
