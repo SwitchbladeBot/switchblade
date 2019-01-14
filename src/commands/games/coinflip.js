d88fd67cb807d5775f320d4e416ad55b059edd6e
Pedro Fracassi <pedroluizfracassi@gmail.com>
2019-01-14 16:54:25 -0300

    Clean coinflip.js

--- src/commands/games/coinflip.js
+++ src/commands/games/coinflip.js
@@ -11,10 +11,9 @@ module.exports = class Coinflip extends Command {
   run ({ channel, author, t }) {
     const sides = ['heads', 'tails']
     const chosenSide = sides[Math.floor(Math.random() * sides.length)]
-    const embed = new SwitchbladeEmbed(author)
-    channel.startTyping()
-    embed.setDescription(t('commands:coinflip.landed', { chosenSide }))
-      .setImage(`https://raw.githubusercontent.com/bolsomito/koi/master/bin/assets/${chosenSide}.png`)
-    channel.send(embed).then(() => channel.stopTyping())
+    channel.send(
+      new SwitchbladeEmbed(author)
+        .setDescription(t('commands:coinflip.landed', { chosenSide }))
+    )
   }
 }
