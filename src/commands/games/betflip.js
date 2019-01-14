48966828c4fd94db49cfdb28204366f654873360
Pedro Fracassi <pedroluizfracassi@gmail.com>
2019-01-14 16:52:17 -0300

    Clean betflip.js

--- src/commands/games/betflip.js
+++ src/commands/games/betflip.js
@@ -1,5 +1,5 @@
-const { CommandStructures, SwitchbladeEmbed, Constants } = require('../../')
-const { Command, CommandParameters, NumberParameter, StringParameter } = CommandStructures
+const { CommandStructures, SwitchbladeEmbed } = require('../../')
+const { Command, CommandParameters, NumberParameter, StringParameter, CommandError } = CommandStructures
 
 module.exports = class Betflip extends Command {
   constructor (client) {
@@ -13,24 +13,23 @@ module.exports = class Betflip extends Command {
   }
 
   async run ({ channel, author, t }, bet, side) {
-    const embed = new SwitchbladeEmbed(author)
     channel.startTyping()
 
     try {
       const { won, chosenSide } = await this.modules.economy.betflip(author.id, bet, side)
-      embed.setImage(`https://raw.githubusercontent.com/bolsomito/koi/master/bin/assets/${chosenSide}.png`)
-        .setDescription(t(`commands:betflip.${won ? 'victory' : 'loss'}`, { chosenSide, count: bet }))
+      channel.send(
+        new SwitchbladeEmbed(author)
+          .setDescription(t(`commands:betflip.${won ? 'victory' : 'loss'}`, { chosenSide, count: bet }))
+      )
     } catch (e) {
-      embed.setColor(Constants.ERROR_COLOR)
       switch (e.message) {
         case 'NOT_ENOUGH_MONEY':
-          embed.setTitle(t('errors:notEnoughMoney'))
-          break
+          throw new CommandError(t('errors:notEnoughMoney'))
         default:
-          embed.setTitle(t('errors:generic'))
+          throw new CommandError(t('errors:notEnoughMoney'))
       }
     }
 
-    channel.send(embed).then(() => channel.stopTyping())
+    channel.stopTyping()
   }
 }
