5956f7394e34bf17b465b9e9245b6129a7a5a3f6
Doges <xdoges@idonthave.life>
2019-01-14 17:59:03 -0200

    reversetext

--- src/commands/memes/reversetext.js
+++ src/commands/memes/reversetext.js
@@ -2,20 +2,20 @@ const { CommandStructures, SwitchbladeEmbed } = require('../../index')
 const { Command, CommandParameters, StringParameter } = CommandStructures
 
 module.exports = class ReverseText extends Command {
-    constructor (client) {
-        super(client)
-        this.name = 'reversetext'
-        this.category = 'memes'
+  constructor (client) {
+    super(client)
+    this.name = 'reversetext'
+    this.category = 'memes'
 
-        this.parameters = new CommandParameters(this,
-            new StringParameter({ full: true, missingError: 'commands:reversetext.missingSentence' })
-        )
-    }
+    this.parameters = new CommandParameters(this,
+      new StringParameter({ full: true, missingError: 'commands:reversetext.missingSentence' })
+    )
+  }
 
-    async run ({ t, author, channel }, text) {
-        channel.send(
-            new SwitchbladeEmbed(author)
-                .setDescription(`${text.split('').reverse().join('')}`)
-        )
-    }
+  async run ({ t, author, channel }, text) {
+    channel.send(
+      new SwitchbladeEmbed(author)
+        .setDescription(`${text.split('').reverse().join('')}`)
+    )
+  }
 }
