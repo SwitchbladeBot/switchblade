706cfbf3bb0d248537dd7bd9faf756d94228cfb7
Doges <infinitepolices@gmail.com>
2019-01-15 12:51:15 -0200

    Create reversetext.js

---
+++ src/commands/memes/reversetext.js
@@ -0,0 +1,21 @@
+const { CommandStructures, SwitchbladeEmbed } = require('../../index')
+const { Command, CommandParameters, StringParameter } = CommandStructures
+
+module.exports = class ReverseText extends Command {
+  constructor (client) {
+    super(client)
+    this.name = 'reversetext'
+    this.category = 'memes'
+
+    this.parameters = new CommandParameters(this,
+      new StringParameter({ full: true, missingError: 'commands:reversetext.missingSentence' })
+    )
+  }
+
+  async run ({ t, author, channel }, text) {
+    channel.send(
+      new SwitchbladeEmbed(author)
+        .setDescription(`${text.split('').reverse().join('')}`)
+    )
+  }
+}
