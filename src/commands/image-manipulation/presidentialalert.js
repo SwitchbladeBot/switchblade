976484ec1b408be9824bc3aadd2d4f99de018588
Doges <infinitepolices@gmail.com>
2019-01-21 21:36:32 -0300

    Update src/commands/image-manipulation/presidentialalert.js
    Co-Authored-By: pedrofracassi <pedroluizfracassi@gmail.com>

--- src/commands/image-manipulation/presidentialalert.js
+++ src/commands/image-manipulation/presidentialalert.js
@@ -19,7 +19,7 @@ module.exports = class PresidentialAlert extends Command {
   async run ({ t, author, channel }, text) {
     channel.startTyping()
     const presidential = await CanvasTemplates.presidentialAlert(text)
-    channel.send(new Attachment(triggered, 'president.jpg'))
+    channel.send(new Attachment(presidential, 'president.jpg'))
     channel.stopTyping()
   }
 }
