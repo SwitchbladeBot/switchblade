dff3137e6c5228575c868f958a31c76cb9f0f680
David Paiva <hello@c2g.space>
2019-01-22 13:21:14 +0000

    Update status.js

--- src/http/api/status.js
+++ src/http/api/status.js
@@ -10,17 +10,6 @@ module.exports = class Status extends Route {
   register (app) {
     const router = Router()
 
-    /**
-     * @api {get} /status Status of the API (200 = default)
-     * @apiName Status
-     * @apiGroup Status
-     *
-     * @apiSuccess {Object} will appear the 200 code
-     *
-     * @apiSuccessExample Success-Response:
-     *     HTTP/1.1 200 OK
-     *     {"code": 200, "message": "OK"}
-     */
     router.get('/', async (req, res) => {
       res.json({ code: 200, message: 'OK' })
     })
