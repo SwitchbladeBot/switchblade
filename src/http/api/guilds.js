0c04a4ff616365bf1edaa75aab930153fabc16e8
David Paiva <hello@c2g.space>
2019-01-22 13:21:34 +0000

    Update guilds.js

--- src/http/api/guilds.js
+++ src/http/api/guilds.js
@@ -22,35 +22,6 @@ module.exports = class Guilds extends Route {
       res.status(400).json({ error: 'Invalid request' })
     })
 
-    /**
-     * @api {get} /guilds/members/:guildId Request guild member count
-     * @apiName GetGuildMemberCount
-     * @apiGroup Guilds
-     *
-     * @apiParam {String} guildId Guild ID
-     *
-     * @apiSuccess {String} id Guild ID
-     * @apiSuccess {String} name Guild Name
-     * @apiSuccess {String} icon Guild Icon URL
-     * @apiSuccess {Number} memberCount Current guild's member count
-     *
-     * @apiSuccessExample Success-Response:
-     *     HTTP/1.1 200 OK
-     *     {
-     *       "id": "445203868624748555"
-     *       "name": "Switchblade Bot",
-     *       "icon": "ebdb8b08b366e6acc563e8d3e819bdf6",
-     *       "memberCount": 350
-     *     }
-     *
-     * @apiError GuildNotFound Guild not found
-     *
-     * @apiErrorExample Error-Response:
-     *    HTTP/1.1 404 Not Found
-     *    {
-     *      "error": "Guild not found"
-     *    }
-     */
     router.get('/:id/members/', async (req, res) => {
       const guild = this.client.guilds.get(req.params.id)
       if (guild) {
