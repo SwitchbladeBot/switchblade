10ddb0213322e50dc1c4eb929e2b856f138ae5fd
davipatury <davi.patury@yahoo.com.br>
2019-01-25 13:16:59 -0200

    Bassboost command. Closes #453

--- src/commands/music/bassboost.js
+++ src/commands/music/bassboost.js
@@ -1,7 +1,5 @@
 const { CommandStructures, SwitchbladeEmbed } = require('../../')
-const { Command, CommandRequirements, CommandParameters, NumberParameter } = CommandStructures
-
-const MAX_VOLUME = 150
+const { Command, CommandRequirements } = CommandStructures
 
 module.exports = class Bassboost extends Command {
   constructor (client) {
@@ -17,15 +15,7 @@ module.exports = class Bassboost extends Command {
     const embed = new SwitchbladeEmbed(author)
 
     const guildPlayer = this.client.playerManager.get(guild.id)
-    if (guildPlayer.bassboosted) {
-      guildPlayer.bassboost(false)
-      channel.send(embed
-        .setTitle(`\uD83D\uDD08 MUTE THE BASS \uD83D\uDD08`))
-    } else {
-      guildPlayer.bassboost()
-      channel.send(embed
-        .setTitle(`\uD83D\uDD0A DROP THE BASS \uD83D\uDD0A`)
-        .setImage('https://i.imgur.com/A6HWTqq.png'))
-    }
+    guildPlayer.bassboost(!guildPlayer.bassboosted)
+    channel.send(embed.setTitle(t(`commands:bassboost.bassboost_${guildPlayer.bassboosted}`)))
   }
 }
--- src/commands/music/volume.js
+++ src/commands/music/volume.js
@@ -18,11 +18,11 @@ module.exports = class Volume extends Command {
 
   async run ({ t, author, channel, guild }, volume) {
     const embed = new SwitchbladeEmbed(author)
+    const guildPlayer = this.client.playerManager.get(guild.id)
 
     if (volume === MAX_VOLUME) embed.setImage('https://i.imgur.com/A6HWTqq.png')
     else if (guildPlayer.bassboosted) guildPlayer.bassboost(false)
 
-    const guildPlayer = this.client.playerManager.get(guild.id)
     guildPlayer.volume(volume)
 
     channel.send(embed.setTitle(`\uD83D\uDD0A ${t('commands:volume.volumeSet', { volume })}`))
