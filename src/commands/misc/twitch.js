4e2cf5654d0944ad5a5fcfe3904829a6e6829317
David Paiva <pillgp@icloud.com>
2019-01-25 21:47:22 +0000

    Let's put this also inside the if, since it looks ugly outside

--- src/commands/misc/twitch.js
+++ src/commands/misc/twitch.js
@@ -36,8 +36,8 @@ module.exports = class Twitch extends Command {
             .addField(t('commands:twitch.streamingTitle', { gameName }), t('commands:twitch.streamingDescription', { title: stream.title, viewers: MiscUtils.formatNumber(stream.viewer_count, language) }))
             .setImage(stream.thumbnail_url.replace('{width}', 1920).replace('{height}', 1080))
         }
+        channel.send(embed).then(() => channel.stopTyping())
       } else throw new CommandError(t('commands:twitch.userNotFound'))
-      channel.send(embed).then(() => channel.stopTyping())
     } catch (e) {
       throw new CommandError(t('errors:generic'))
     }
