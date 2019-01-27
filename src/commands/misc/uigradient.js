124d8c28eafc86b85421346b23e5cc32e688cd81
metehus <methidezeeni@gmail.com>
2019-01-27 17:35:49 -0200

    Canvastemplate

--- src/commands/misc/uigradient.js
+++ src/commands/misc/uigradient.js
@@ -1,7 +1,6 @@
-const { Command, SwitchbladeEmbed } = require('../../')
+const { Command, SwitchbladeEmbed, CanvasTemplates } = require('../../')
 const { Attachment } = require('discord.js')
 const snekfetch = require('snekfetch')
-const { createCanvas } = require('canvas')
 
 module.exports = class UIGradient extends Command {
   constructor (client) {
@@ -17,25 +16,15 @@ module.exports = class UIGradient extends Command {
 
     const { body } = await snekfetch.get('https://cdn.jsdelivr.net/gh/ghosh/uiGradients/gradients.json')
     const { name, colors } = body[Math.floor(Math.random() * body.length)]
-    const WIDTH = 300
-    const HEIGHT = 100
-    const canvas = createCanvas(WIDTH, HEIGHT)
-    const ctx = canvas.getContext('2d')
-    const grd = ctx.createLinearGradient(0, 0, WIDTH, 0)
 
-    colors.forEach((color, i) => {
-      grd.addColorStop((i / (colors.length - 1)), color)
-    })
-
-    ctx.fillStyle = grd
-    ctx.fillRect(0, 0, WIDTH, HEIGHT)
+    const gradient = CanvasTemplates.gradient(colors, 300, 100)
 
     embed.setTitle(name)
       .setURL(`https://uigradients.com/#${name.replace(' ', '')}`)
       .setColor(colors[0])
       .setImage('attachment://gradient.png')
       .setDescription(`\`${colors.join(`\`, \``)}\``)
-      .attachFile(new Attachment(canvas.toBuffer(), 'gradient.png'))
+      .attachFile(new Attachment(gradient, 'gradient.png'))
     channel.send(embed).then(() => channel.stopTyping())
   }
 }
