54a475baf5e43f0aaf7607dfbf266661175ac542
Doges <xdoges@idonthave.life>
2019-02-02 15:26:53 -0200

    refactor hentai and yiff command (old e621 command)

---
+++ src/commands/nsfw/yiff.js
@@ -0,0 +1,13 @@
+const RandomRedditPostCommand = require('../../structures/command/RandomRedditPostCommand.js')
+const { CommandStructures } = require('../../')
+const { CommandRequirements } = CommandStructures
+
+module.exports = class Yiff extends RandomRedditPostCommand {
+  constructor (client) {
+    super(client)
+    this.name = 'yiff'
+    this.subreddit = 'yiff'
+    this.category = 'nsfw'
+    this.requirements = new CommandRequirements(this, { nsfwOnly: true })
+  }
+}
