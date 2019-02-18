9562bf165df791c68357008e30a17c72fc83af5a
Lireoy <34489456+Lireoy@users.noreply.github.com>
2019-02-18 20:13:23 +0100

    Fixing requested code changes

--- src/commands/image-manipulation/morejpeg.js
+++ src/commands/image-manipulation/morejpeg.js
@@ -17,10 +17,10 @@ module.exports = class MoreJpeg extends Command {
 
   async run ({ t, author, channel, message }, text) {
     channel.startTyping()
-    if (message.attachments.first() === undefined && text === undefined) {
+    if (!message.attachments.first() && !text) {
       channel.stopTyping()
       throw new CommandError(t('commands:morejpeg.missingImage'), true)
-    } else if (message.attachments.first() !== undefined) {
+    } else if (message.attachments.first()) {
       text = message.attachments.first().url
     }
     const jpeg = await CanvasTemplates.morejpeg(text)
