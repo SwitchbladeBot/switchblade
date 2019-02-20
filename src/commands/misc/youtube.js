bfab4fc26d3d28fddceb9d5db2a082cbd02b6905
metehus <methidezeeni@gmail.com>
2019-02-20 18:46:21 -0300

    things

--- src/commands/misc/youtube.js
+++ src/commands/misc/youtube.js
@@ -1,6 +1,7 @@
 const SearchCommand = require('../../structures/command/SearchCommand.js')
-const { SwitchbladeEmbed, CommandStructures, Constants } = require('../../')
+const { SwitchbladeEmbed, CommandStructures, Constants, MiscUtils } = require('../../')
 const { CommandRequirements, CommandParameters, StringParameter, BooleanFlagParameter } = CommandStructures
+const moment = require('moment')
 
 module.exports = class YouTube extends SearchCommand {
   constructor (client) {
@@ -36,11 +37,32 @@ module.exports = class YouTube extends SearchCommand {
     return `\`${type === 'video' ? 'V' : type === 'channel' ? 'C' : 'P'}\` ${title}`
   }
 
-  async handleResult ({ t, channel, author, language }, aa) {
-    // TODO
+  async handleResult (ctx, item) {
+    const { t, channel } = ctx
+    await channel.startTyping()
+    const type = this.getType(item.id)
+    const embed = await this[`get${MiscUtils.capitalizeFirstLetter(type)}`](ctx, item)
+    embed.setAuthor(t(`commands:youtube.${type}.info`), this.embedLogoURL)
+      .setColor(this.embedColor)
+    channel.send(embed).then(() => channel.stopTyping())
   }
 
-  static getType ({ kind }) {
+  async getVideo ({ t, author, language }, { id: videoID }) {
+    moment.locale(language)
+    const { id, snippet, statistics, contentDetails } = await this.client.apis.youtube.getVideo(videoID.videoId, 'snippet,statistics,contentDetails')
+    const { publishedAt, channelId, title, thumbnails, channelTitle, tags } = snippet
+    const description = `[${title}](https://youtu.be/${id.videoId}) `
+    const embed = new SwitchbladeEmbed(author)
+      .setDescription(description.size > 2040 ? description.slice(0, 2040) + '...' : description)
+      .addField(t('commands:youtube.likes'), MiscUtils.formatNumber(statistics.likeCount, language), true)
+      .addField(t('commands:youtube.dislikes'), MiscUtils.formatNumber(statistics.dislikeCount, language), true)
+      .addField(t('commands:youtube.comments'), MiscUtils.formatNumber(statistics.commentCount, language), true)
+      .addField(t('commands:youtube.channel'), `[${channelTitle}](https://www.youtube.com/channel/${channelId})`, true)
+      .addField(t('commands:youtube.publishedAt'), moment(publishedAt).format('LLL'), true)
+    return embed
+  }
+
+  getType ({ kind }) {
     return kind.split('#')[1]
   }
 }
