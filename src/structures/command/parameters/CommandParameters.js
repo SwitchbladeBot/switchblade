9c387c29adb4ef7015ba930707c819e191495149
davipatury <davi.patury@yahoo.com.br>
2019-02-21 21:25:17 -0300

    LUL

--- src/structures/command/parameters/CommandParameters.js
+++ src/structures/command/parameters/CommandParameters.js
@@ -6,7 +6,6 @@ const ParameterTypes = require('./types')
 const isNull = (n) => n === null || n === undefined
 const funcOrString = (f, sf, ...args) => typeof f === 'function' ? f(...args) : sf ? sf(f) : f
 const normalizeParam = (p) => {
-  console.log(p.type)
   const type = ParameterTypes[p.type] || p.type
   if (!type || !(type.prototype instanceof Parameter)) throw new TypeError('Invalid parameter type')
   return { ...type.parseOptions(p), type }
@@ -77,7 +76,7 @@ module.exports = class CommandParameters {
 
   static parseParameter (context, param, arg, missingErr) {
     const parsedArg = param.type._parse(arg, param, context)
-    console.log(param)
+    // console.log(param)
     if (isNull(parsedArg) && param.required) {
       throw new CommandError(missingErr, param.showUsage)
     }
