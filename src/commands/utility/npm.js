82ef25253ecc8f38fdb245223746c7579ac8cfe2
David Paiva <pillgp@icloud.com>
2019-03-05 15:52:37 +0000

    👌 Make NPM command extend SearchCommand
     #742

--- src/commands/utility/npm.js
+++ src/commands/utility/npm.js
@@ -1,8 +1,7 @@
-const { Command, SwitchbladeEmbed, Constants } = require('../../')
+const { SearchCommand, SwitchbladeEmbed, Constants } = require('../../')
+const npm = require('search-npm-registry')
 
-const npm = require('api-npm')
-
-module.exports = class Npm extends Command {
+module.exports = class Npm extends SearchCommand {
   constructor (client) {
     super(client, {
       name: 'npm',
@@ -12,23 +11,26 @@ module.exports = class Npm extends Command {
         full: true,
         fullJoin: '-',
         missingError: 'commands:npm.noNameProvided'
-      }]
+      }],
+      embedColor: Constants.NPM_COLOR,
+      embedLogoURL: 'https://i.imgur.com/24yrZxG.png'
     })
   }
 
-  run ({ t, author, channel }, pkg) {
+  async search (context, query) {
+    return npm().text(query).size(10).search()
+  }
+
+  searchResultFormatter (obj) {
+    return `[${obj.name}](${obj.links.npm})`
+  }
+
+  async handleResult ({ t, channel, author }, pkg) {
     const embed = new SwitchbladeEmbed(author)
     channel.startTyping()
-    npm.getdetails(pkg, data => {
-      if (data.name) {
-        const description = data.description || t('commands:npm.noDescription')
-        embed.setColor(Constants.NPM_COLOR)
-          .setAuthor(data.name, 'https://i.imgur.com/24yrZxG.png', `https://www.npmjs.com/package/${data.name}`)
-          .setDescription(`${description}\nhttps://www.npmjs.com/package/${data.name}\n\n\`npm i ${data.name} --save\``)
-      } else {
-        embed.setColor(Constants.ERROR_COLOR).setTitle(t('commands:npm.notFound'))
-      }
-      channel.send(embed).then(() => channel.stopTyping())
-    })
+    embed.setColor(Constants.NPM_COLOR)
+      .setAuthor(pkg.name, this.embedLogoURL, pkg.links.npm)
+      .setDescription(`${pkg.description || t('commands:npm.noDescription')}\n${pkg.links.npm}\n\n\`npm install --save ${pkg.name}\``)
+    channel.send(embed).then(() => channel.stopTyping())
   }
 }
