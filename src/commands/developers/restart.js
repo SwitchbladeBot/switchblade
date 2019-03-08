b2e2dd838d8f20d84bc6aaa2d1f6f40ca255a239
Jack Crispy <34707863+JackCrispy@users.noreply.github.com>
2019-03-09 09:04:20 +1100

    Update restart.js

--- src/commands/developers/restart.js
+++ src/commands/developers/restart.js
@@ -2,22 +2,22 @@ const { CommandStructures, SwitchbladeEmbed } = require('../../')
 const { Command, CommandRequirements } = CommandStructures
 
 module.exports = class Restart extends Command {
-    constructor(client) {
-        super(client)
-            this.name = 'restart'
-            this.aliases = ['rs']
-            this.category = 'developers'
-            this.hidden = true
+  constructor(client) {
+    super(client)
+    this.name = 'restart'
+    this.aliases = ['rs']
+    this.category = 'developers'
+    this.hidden = true
 
-            this.requirements = new CommandRequirements(this, { devOnly: true })
-    }
+    this.requirements = new CommandRequirements(this, { devOnly: true })
+  }
 
-    async run({ message }) {
-        const embed = new SwitchbladeEmbed();
+  async run({ message }) {
+    const embed = new SwitchbladeEmbed();
 
-        message.channel.send(embed
-            .setTitle("Service Restart")
-            .setDescription("Restarting...")
-        ).then(message.client.destroy()).then(message.client.login());
-    }
+    message.channel.send(embed
+      .setTitle('Service Restart')
+      .setDescription('Restarting...')
+    ).then(message.client.destroy()).then(message.client.login());
+  }
 }
