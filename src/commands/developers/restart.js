674c9dacc24262962d06d28ab05220677548ed88
Jack Crispy <34707863+JackCrispy@users.noreply.github.com>
2019-03-09 08:40:47 +1100

    Add files via upload

---
+++ src/commands/developers/restart.js
@@ -0,0 +1,23 @@
+const { CommandStructures, SwitchbladeEmbed } = require('../../')
+const { Command, CommandRequirements } = CommandStructures
+
+module.exports = class Restart extends Command {
+    constructor(client) {
+        super(client)
+            this.name = 'restart'
+            this.aliases = ['rs']
+            this.category = 'developers'
+            this.hidden = true
+
+            this.requirements = new CommandRequirements(this, { devOnly: true })
+    }
+
+    async run({ message }) {
+        const embed = new SwitchbladeEmbed();
+
+        message.channel.send(embed
+            .setTitle("Service Restart")
+            .setDescription("Restarting...")
+        ).then(message.client.destroy()).then(message.client.login());
+    }
+}
