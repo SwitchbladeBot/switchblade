a7c967a8195ecaa9b44733004746f2e10f7e1f91
David Paiva <pillgp@icloud.com>
2019-03-29 23:20:47 +0000

    🎨 Merging lolstatus and champion to a single command

--- src/commands/games/leagueoflegends/status.js
+++ src/commands/games/leagueoflegends/status.js
@@ -1,22 +1,24 @@
-const { Command, CommandError, SwitchbladeEmbed, Constants } = require('../../')
+const { Command, CommandError, SwitchbladeEmbed, Constants } = require('../../../')
 const snekfetch = require('snekfetch')
 
 const servers = ['na', 'euw', 'eune', 'lan', 'las', 'br', 'tr', 'ru', 'oce', 'jp', 'kr']
 
-module.exports = class LoLStatus extends Command {
+module.exports = class LeagueOfLegendsStatus extends Command {
   constructor (client) {
     super(client, {
-      name: 'lolstatus',
+      name: 'status',
+      aliases: ['s'],
+      parentCommand: 'leagueoflegends',
       category: 'games',
       parameters: [{
         type: 'string',
         whitelist: servers,
         missingError: ({ t, prefix }) => {
-          return new SwitchbladeEmbed().setTitle(t('commands:lolstatus.missingServer'))
+          return new SwitchbladeEmbed().setTitle(t('commands:leagueoflegends.subcommands.status.missingServer'))
             .setDescription([
               this.usage(t, prefix),
               '',
-              `__**${t('commands:lolstatus.availableServers')}:**__`,
+              `__**${t('commands:leagueoflegends.subcommands.status.availableServers')}:**__`,
               `**${servers.map(l => `\`${l}\``).join(', ')}**`
             ].join('\n'))
         }
@@ -27,14 +29,14 @@ module.exports = class LoLStatus extends Command {
   async run ({ t, author, channel, language }, server) {
     channel.startTyping()
     const { body } = await snekfetch.get(`https://status.leagueoflegends.com/shards/${server}/summary`)
-    if (!body.messages.length) throw new CommandError(t('commands:lolstatus.noStatusMessages'))
+    if (!body.messages.length) throw new CommandError(t('commands:leagueoflegends.subcommands.status.noStatusMessages'))
     channel.send(
       new SwitchbladeEmbed(author)
         .setDescription(
           [
-            `${Constants[`LOL_STATUS_${body.status.toUpperCase()}`]} **[${t(`lolservers:${server}`)} - ${t(`commands:lolstatus.${body.status}`)}](https://status.leagueoflegends.com/?${language.replace('-', '_')}#${server})**\n`,
+            `${Constants[`LOL_STATUS_${body.status.toUpperCase()}`]} **[${t(`lolservers:${server}`)} - ${t(`commands:leagueoflegends.subcommands.status.${body.status}`)}](https://status.leagueoflegends.com/?${language.replace('-', '_')}#${server})**\n`,
             body.messages.map(m => {
-              return `${Constants[`LOL_STATUS_${m.severity.toUpperCase()}`]} **${t(`commands:lolstatus.${m.severity}`)}:** ${this.getLocalizedContent(m, language)}`
+              return `${Constants[`LOL_STATUS_${m.severity.toUpperCase()}`]} **${t(`commands:leagueoflegends.subcommands.status.${m.severity}`)}:** ${this.getLocalizedContent(m, language)}`
             }).join('\n\n')
           ].join('\n')
         )
