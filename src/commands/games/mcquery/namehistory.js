bb7755c46990b09ded2ea7b2a58af28147931981
perronosaurio <waxtz@hotmail.com>
2019-03-30 10:45:12 -0400

    namehistory

--- src/commands/games/mcquery/namehistory.js
+++ src/commands/games/mcquery/namehistory.js
@@ -3,34 +3,36 @@ const { Command, CommandError, SwitchbladeEmbed } = require('../../../')
 const moment = require('moment')
 const snekfetch = require('snekfetch')
 
-module.exports = class MinecraftSkin extends Command {
+module.exports = class MinecraftNameHistory extends Command {
   constructor (client) {
     super(client, {
-      name: 'skin',
-      aliases: ['minecraftskin', 'mskin'],
+      name: 'namehistory',
+      aliases: ['nameh', 'nh'],
       parentCommand: 'minecraft',
       parameters: [{
         type: 'string',
-        missingError: 'commands:minecraft.subcommands.skin.noName'
+        missingError: 'commands:minecraft.subcommands.namehistory.noName'
       }]
     })
   }
 
   async run ({ t, author, channel, language }, name) {
     channel.startTyping()
+    moment.locale(language)
     const { body } = await snekfetch.get(`https://api.mojang.com/users/profiles/minecraft/${name}?at=${moment().format('x')}`)
 
     if (body && body.id) {
+      const nameHistory = await snekfetch.get(`https://api.mojang.com/user/profiles/${body.id}/names`)
       channel.send(
         new SwitchbladeEmbed(author)
-          .setImage(`https://visage.surgeplay.com/full/512/${body.id}.png`)
+          .setDescription(nameHistory.body.map(n => `\`${n.name}\` (${n.changedToAt ? moment(n.changedToAt).fromNow() : 'Original Name'})`).join('\n'))
           .setTitle('NameMC profile (click here)')
           .setURL(`https://namemc.com/profile/${body.id}`)
-          .setAuthor(`${body.name}'s Skin!`, `https://visage.surgeplay.com/head/512/${body.id}.png`)
+          .setAuthor(`${body.name}'s name history!`, `https://visage.surgeplay.com/head/512/${body.id}.png`)
       ).then(channel.stopTyping())
     } else {
       channel.stopTyping()
-      throw new CommandError(t('commands:minecraft.subcommands.skin.unknownName'))
+      throw new CommandError(t('commands:minecraft.subcommands.namehistory.unknownName'))
     }
   }
 }
