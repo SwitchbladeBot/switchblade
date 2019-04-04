e42a8dccf6b4f5675a86dfc1f05ceaadbf82f625
David Paiva <pillgp@icloud.com>
2019-04-04 21:38:15 +0100

    Paginated embed, owo!

---
+++ src/structures/ReactionHandler.js
@@ -0,0 +1,152 @@
+const { ReactionCollector } = require('discord.js')
+
+module.exports = class ReactionHandler extends ReactionCollector {
+  constructor (message, filter, options, display, emojis) {
+    super(message, filter, options)
+
+    this.display = display
+    this.methodMap = new Map(Object.entries(this.display.emojis).map(([key, value]) => [value, key]))
+    this.currentPage = this.options.startPage || 0
+    this.prompt = this.options.prompt || this.display.t('commons:whichPage')
+    this.time = typeof this.options.time === 'number' ? this.options.time : 30000
+    this.awaiting = false
+    this.selection = this.display.emojis.zero ? new Promise((resolve, reject) => {
+      this.reject = reject
+      this.resolve = resolve
+    }) : Promise.resolve(null)
+    this.reactionsDone = false
+
+    if (emojis.length) this._queueEmojiReactions(emojis.slice())
+    else return this.stop()
+
+    this.on('collect', (reaction, handler) => {
+      reaction.remove(reaction.users.last())
+      this[this.methodMap.get(reaction.emoji.id || reaction.emoji.name)](handler)
+    })
+
+    this.on('end', () => {
+      if (this.reactionsDone && !this.message.deleted) this.message.reactions.deleteAll()
+    })
+  }
+
+  first () {
+    this.currentPage = 0
+    this.update()
+  }
+
+  back () {
+    if (this.currentPage <= 0) return
+
+    this.currentPage--
+    this.update()
+  }
+
+  forward () {
+    if (this.currentPage > this.display.pages.length - 1) return
+    this.currentPage++
+    this.update()
+  }
+
+  last () {
+    this.currentPage = this.display.pages.length - 1
+    this.update()
+  }
+
+  async jump (user) {
+    if (this.awaiting) return
+    this.awaiting = true
+    const message = await this.message.channel.send(this.prompt)
+    const collected = await this.message.channel.awaitMessages(mess => mess.author === user, { max: 1, time: this.time })
+    this.awaiting = false
+    await message.delete()
+    if (!collected.size) return
+    const newPage = parseInt(collected.first().content)
+    collected.first().delete()
+    if (newPage && newPage > 0 && newPage <= this.display.pages.length) {
+      this.currentPage = newPage - 1
+      this.update()
+    }
+  }
+
+  info () {
+    this.message.edit(this.display.infoPage)
+  }
+
+  stop () {
+    if (this.resolve) this.resolve(null)
+    super.stop()
+  }
+
+  zero () {
+    if (this.display.options.length - 1 < this.currentPage * 10) return
+    this.resolve(this.currentPage * 10)
+    this.stop()
+  }
+
+  one () {
+    if (this.display.options.length - 1 < 1 + (this.currentPage * 10)) return
+    this.resolve(1 + (this.currentPage * 10))
+    this.stop()
+  }
+
+  two () {
+    if (this.display.options.length - 1 < 2 + (this.currentPage * 10)) return
+    this.resolve(2 + (this.currentPage * 10))
+    this.stop()
+  }
+
+  three () {
+    if (this.display.options.length - 1 < 3 + (this.currentPage * 10)) return
+    this.resolve(3 + (this.currentPage * 10))
+    this.stop()
+  }
+
+  four () {
+    if (this.display.options.length - 1 < 4 + (this.currentPage * 10)) return
+    this.resolve(4 + (this.currentPage * 10))
+    this.stop()
+  }
+
+  five () {
+    if (this.display.options.length - 1 < 5 + (this.currentPage * 10)) return
+    this.resolve(5 + (this.currentPage * 10))
+    this.stop()
+  }
+
+  six () {
+    if (this.display.options.length - 1 < 6 + (this.currentPage * 10)) return
+    this.resolve(6 + (this.currentPage * 10))
+    this.stop()
+  }
+
+  seven () {
+    if (this.display.options.length - 1 < 7 + (this.currentPage * 10)) return
+    this.resolve(7 + (this.currentPage * 10))
+    this.stop()
+  }
+
+  eight () {
+    if (this.display.options.length - 1 < 8 + (this.currentPage * 10)) return
+    this.resolve(8 + (this.currentPage * 10))
+    this.stop()
+  }
+
+  nine () {
+    if (this.display.options.length - 1 < 9 + (this.currentPage * 10)) return
+    this.resolve(9 + (this.currentPage * 10))
+    this.stop()
+  }
+
+  update () {
+    this.message.edit({ embed: this.display.pages[this.currentPage] })
+  }
+
+  async _queueEmojiReactions (emojis) {
+    if (this.message.deleted) return this.stop()
+    if (this.ended) return this.message.reactions.deleteAll()
+    await this.message.react(emojis.shift())
+    if (emojis.length) return this._queueEmojiReactions(emojis)
+    this.reactionsDone = true
+    return null
+  }
+}
