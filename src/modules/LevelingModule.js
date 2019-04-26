4b94a3b1b04594daf84e03bd9faf2a23d700dd77
perronosaurio <waxtz@hotmail.com>
2019-04-25 21:32:58 -0400

    only for testing

--- src/modules/LevelingModule.js
+++ src/modules/LevelingModule.js
@@ -23,22 +23,31 @@ module.exports = class LevelingModule extends Module {
 
   levelFromExperience (xp, modifier = 0.37) {
     const r = (-this.B + Math.sqrt(Math.pow(this.B, 2) - ((4 * this.A) * ((this.C * (1 + modifier)) - Math.ceil(xp / (1 + modifier)))))) / (2 * this.A)
-    return (r > 0 || !isNaN(r)) ? Math.floor(r) : 0
+    return (r < 0 || isNaN(r)) ? 0 : Math.floor(r)
   }
 
   experienceFromLevel (level, modifier = 0.37) {
     return Math.floor((this.A * Math.pow(level, 2) + (this.B * level) + (this.C * (1 + modifier))) * (1 + modifier))
   }
 
+  experienceInCurrentLevel (level, xp, modifier = 0.37) {
+    return xp - this.experienceFromLevel(level)
+  }
+
+  experienceBetweenCurrentAndNextLevel (level, modifier = 0.37) {
+    return this.experienceFromLevel(level + 1, modifier) - this.experienceFromLevel(level, modifier)
+  }
+
   async giveExperience (_user) {
     // TODO: change this system
     const { globalXp } = await this._users.findOne(_user, 'globalXp')
-    console.log(globalXp)
     const newExperience = Math.floor(Math.random() * (this.maxRandomExperience - this.minRandomExperience + 1) + this.minRandomExperience)
-    console.log(`old: ${this.levelFromExperience(globalXp)} new: ${this.levelFromExperience(globalXp + newExperience)}`)
+    console.log(`old xp: ${globalXp} new xp: ${globalXp + newExperience}`)
+    console.log(`to next: ${this.experienceInCurrentLevel(this.levelFromExperience(globalXp), globalXp)}/${this.experienceBetweenCurrentAndNextLevel(this.levelFromExperience(globalXp))} (total xp: ${globalXp})`)
+    console.log(`percent: ${((globalXp - this.experienceFromLevel(this.levelFromExperience(globalXp))) / (this.experienceFromLevel(this.levelFromExperience(globalXp) + 1) - this.experienceFromLevel(this.levelFromExperience(globalXp)))) * 100}%`)
+    console.log('-----')
     await this._users.update(_user, { $inc: { globalXp: newExperience } })
 
     if (this.levelFromExperience(globalXp) !== this.levelFromExperience(globalXp + newExperience)) console.log(`level up to ${this.levelFromExperience(globalXp + newExperience)}`)
-    console.log(globalXp + newExperience)
   }
 }
