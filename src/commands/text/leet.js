35956b6a88609c50429cec0786558e71fc3685e0
Fernando Piniani <fpiniani@gmail.com>
2019-10-04 23:58:08 -0300

    feat/leet-speak

---
+++ src/commands/text/leet.js
@@ -0,0 +1,58 @@
+const { Command } = require('../../')
+
+const leetMap = {
+  a: { soft: '4', hard: '4' },
+  b: { soft: 'b', hard: 'I3' },
+  c: { soft: 'c', hard: '[' },
+  d: { soft: 'd', hard: '|)' },
+  e: { soft: '3', hard: '3' },
+  f: { soft: 'f', hard: '|=' },
+  g: { soft: 'g', hard: '6' },
+  h: { soft: 'h', hard: '#' },
+  i: { soft: '1', hard: '1' },
+  j: { soft: 'j', hard: ']' },
+  k: { soft: 'k', hard: '|<' },
+  l: { soft: 'l', hard: '1' },
+  m: { soft: 'm', hard: '/\\/\\' },
+  n: { soft: 'n', hard: '|\\|' },
+  o: { soft: '0', hard: '0' },
+  p: { soft: 'p', hard: '|>' },
+  q: { soft: 'q', hard: '0_' },
+  r: { soft: 'r', hard: 'I2' },
+  s: { soft: 's', hard: '5' },
+  t: { soft: 't', hard: '7' },
+  u: { soft: 'u', hard: '(_)' },
+  v: { soft: 'v', hard: '\\/' },
+  w: { soft: 'w', hard: '\\/\\/' },
+  x: { soft: 'x', hard: '><' },
+  y: { soft: 'y', hard: 'j' },
+  z: { soft: 'z', hard: '2' }
+}
+
+module.exports = class Leet extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'leet',
+      category: 'memes',
+      parameters: [{
+        type: 'booleanFlag',
+        name: 'hard',
+        aliases: [ 'hc', '#4I2|D', 'hardcore' ]
+      },
+      {
+        type: 'string',
+        full: true,
+        missingError: 'commands:leet.missingSentence'
+      }
+      ]
+    })
+  }
+
+  async run ({ channel, flags }, text) {
+    const leetTranslation = text
+      .split('')
+      .map(char => leetMap[char][flags.hard ? 'hard' : 'soft'])
+      .join('')
+    channel.send(leetTranslation)
+  }
+}
