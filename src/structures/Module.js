693b190143398e27fa0bf02531cea9090bf42b98
davipatury <davi.patury@yahoo.com.br>
2019-10-09 19:42:04 -0300

    some changes

--- src/structures/Module.js
+++ src/structures/Module.js
@@ -23,6 +23,7 @@ module.exports = class Module {
 
   // Retrievers
   isActive (_guild) {
+    if (!this.client.database) return this.defaultState
     return this._guilds.findOne(_guild, this.buildProjection('active')).then(g => {
       const mod = g.modules.get(this.name)
       return this.toggleable ? mod ? mod.active : this.defaultState : true
@@ -30,14 +31,17 @@ module.exports = class Module {
   }
 
   retrieve (_guild, _projection = 'active values') {
+    if (!this.client.database) return { active: this.defaultState, values: this.defaultValues }
     return this._guilds.findOne(_guild, this.buildProjection(_projection)).then(g => g.modules.get(this.name))
   }
 
   retrieveValue (_guild, value) {
+    if (!this.client.database) return this.defaultValues[value]
     return this.asJSON(_guild, `values.${value}`).then(r => r.values[value])
   }
 
   retrieveValues (_guild, values) {
+    if (!this.client.database) return this.defaultValues
     return this.asJSON(_guild, values.map(v => `values.${v}`).join(' ')).then(r => r.values)
   }
 
@@ -63,6 +67,7 @@ module.exports = class Module {
 
   // Updaters
   async updateValues (_guild, values) {
+    if (!this.client.database) return
     return this.validateValues(values).then(entity => {
       const basePath = `modules.${this.name}.values`
       const updateEntries = Object.entries(values).reduce((o, [ k, v ]) => {
@@ -74,6 +79,7 @@ module.exports = class Module {
   }
 
   async updateState (_guild, state) {
+    if (!this.client.database) return
     return this._guilds.update(_guild, {
       [`modules.${this.name}.active`]: this.validateState(state)
     })
