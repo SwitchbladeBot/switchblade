bd23c11dabd0b1bf6bc3537729fcccf2ae48bb0f
Anna Backs <anna.backs@codecentric.de>
2019-10-10 22:47:56 +0200

    Random basic taco

---
+++ src/commands/misc/taco.js
@@ -0,0 +1,24 @@
+const { Command, SwitchbladeEmbed } = require("../../");
+
+const fetch = require("node-fetch");
+
+module.exports = class Taco extends Command {
+  constructor(client) {
+    super(client, {
+      name: "taco"
+    });
+  }
+
+  async run({ t, author, channel }) {
+    const embed = new SwitchbladeEmbed(author);
+    channel.startTyping();
+    const body = await fetch(
+      "http://taco-randomizer.herokuapp.com/random"
+    ).then(res => res.json());
+    const randomTaco = `${body.base_layer.name} with ${body.mixin.name} garnished with ${body.seasoning.name} and topped of with ${body.condiment.name} with and wrapped in ${body.shell.name}`;
+    embed
+      .setImage(randomTaco.toString("utf8"))
+      .setDescription(t("commands:taco.randomTaco"));
+    channel.send(embed).then(() => channel.stopTyping());
+  }
+};
