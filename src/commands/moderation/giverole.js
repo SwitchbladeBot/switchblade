69f4a8af230e1ea928cfcdc75dfc40b8b75bd2fe
Vinícius Estevam <estevamvinicius31@gmail.com>
2019-10-11 17:00:28 -0300

    fix code style

--- src/commands/moderation/giverole.js
+++ src/commands/moderation/giverole.js
@@ -14,17 +14,17 @@ module.exports = class Giverole extends Command {
     })
   }
 
-  async run({ channel, member, author, t }, targetMember, role) {
+  async run ({ channel, member, author, t }, targetMember, role) {
     const embed = new SwitchbladeEmbed(author)
     await targetMember.addRole(role).then(modifiedMember => {
-      if(!member.hasPermission('ADMINISTRATOR', false, true, false)) {
-        if(member.highestRole.comparePositionTo(role) <= 0) throw new CommandError(t('commands:giverole.roleAboveAuthors'))
-        if(member.highestRole.comparePositionTo(member.highestRole) <= 0) throw new CommandError(t('commands:giverole.roleBelow'))
+      if (!member.hasPermission('ADMINISTRATOR', false, true, false)) {
+        if (member.highestRole.comparePositionTo(role) <= 0) throw new CommandError(t('commands:giverole.roleAboveAuthors'))
+        if (member.highestRole.comparePositionTo(member.highestRole) <= 0) throw new CommandError(t('commands:giverole.roleBelow'))
       }
       embed
         .setTitle(t('commands:giverole.successTitle'))
         .setDescription(`${modifiedMember} - \`${role.name}\``)
-    }).catch(err =>{
+    }).catch(err => {
       embed
         .setColor(Constants.ERROR_COLOR)
         .setTitle(t('commands:giverole.cantGiverole'))
