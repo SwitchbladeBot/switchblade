4d487d1e8f38820eed22ea5aed0d326fcd2d9bef
Vinicius Estevam <vinicius.estevam@triersistemas.com.br>
2019-10-11 13:01:37 -0300

    added role hierarchy

--- src/commands/moderation/giverole.js
+++ src/commands/moderation/giverole.js
@@ -1,7 +1,7 @@
 const { Command, Constants, SwitchbladeEmbed } = require('../../')
 
 module.exports = class Giverole extends Command {
-  constructor(client) {
+  constructor (client) {
     super(client, {
       name: 'giverole',
       category: 'moderation',
@@ -14,8 +14,22 @@ module.exports = class Giverole extends Command {
     })
   }
 
-  async run({ channel, author, role }, member) {
+  async run({ channel, author, role, t }, member) {
     const embed = new SwitchbladeEmbed(author)
-    await member.addRole(role)
+    await member.addRole(role).then(modifiedMember => {
+      if(!author.hasPermision('MANAGE_ROLES', false, true, false)) {
+        if(author.highestRole.comparePositionTo(role) < 0) throw new CommandError(t('commands:giverole.roleAboveAuthors'))
+        if(author.highestRole.comparePositionTo(member.highestRole) < 0) throw new CommandError(t('commands:giverole.roleBelow'))
+      }
+      embed
+        .setTitle(t('commands:giverole.successTitle'))
+        .setDescription(`${modifiedMember} - \`${role}\``)
+    }).catch(err =>{
+      embed
+        .setColor(Constants.ERROR_COLOR)
+        .setTitle(t('commands:giverole.cantGiverole'))
+        .setDescription(`\`${err}\``)
+    })
+    channel.send(embed)
   }
 }
