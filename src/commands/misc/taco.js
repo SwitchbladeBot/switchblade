835cd8f2b22d2879670d63916d76df1b60331483
Anna Backs <anna.backs@codecentric.de>
2019-10-12 00:06:23 +0200

    Style fixes

--- src/commands/misc/taco.js
+++ src/commands/misc/taco.js
@@ -1,24 +1,24 @@
-const { Command, SwitchbladeEmbed } = require("../../");
+const { Command, SwitchbladeEmbed } = require('../../')
 
-const fetch = require("node-fetch");
+const fetch = require('node-fetch')
 
 module.exports = class Taco extends Command {
-  constructor(client) {
+  constructor (client) {
     super(client, {
-      name: "taco"
-    });
+      name: 'taco'
+    })
   }
 
-  async run({ t, author, channel }) {
-    const embed = new SwitchbladeEmbed(author);
-    channel.startTyping();
+  async run ({ t, author, channel }) {
+    this.log(taco)
+    const embed = new SwitchbladeEmbed(author)
+    channel.startTyping()
     const body = await fetch(
-      "http://taco-randomizer.herokuapp.com/random"
-    ).then(res => res.json());
-    const randomTaco = `${body.base_layer.name} with ${body.mixin.name} garnished with ${body.seasoning.name} and topped of with ${body.condiment.name} with and wrapped in ${body.shell.name}`;
-    embed
-      .setImage(randomTaco.toString("utf8"))
-      .setDescription(t("commands:taco.randomTaco"));
-    channel.send(embed).then(() => channel.stopTyping());
+      'http://taco-randomizer.herokuapp.com/random'
+    ).then(res => res.json())
+    const randomTaco = `${body.base_layer.name} with ${body.mixin.name} garnished with ${body.seasoning.name} and topped of with ${body.condiment.name} with and wrapped in ${body.shell.name}`
+    embed.setImage(randomTaco.toString('utf8'))
+    embed.setDescription(t('commands:taco.randomTaco'))
+    channel.send(embed).then(() => channel.stopTyping())
   }
-};
+}
