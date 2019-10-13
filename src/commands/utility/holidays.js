ede255e87d0a7978e5058f62845138365d261e19
Fernando Piniani <fpiniani@gmail.com>
2019-10-13 00:50:58 -0300

    countries i18n

--- src/commands/utility/holidays.js
+++ src/commands/utility/holidays.js
@@ -2,109 +2,17 @@ const { Command, CommandError, SwitchbladeEmbed } = require('../../')
 const moment = require('moment')
 const fetch = require('node-fetch')
 
-const countries = {
-  AD: 'Andorra',
-  AL: 'Albania',
-  AR: 'Argentina',
-  AT: 'Austria',
-  AU: 'Australia',
-  AX: 'Åland Islands',
-  BB: 'Barbados',
-  BE: 'Belgium',
-  BG: 'Bulgaria',
-  BJ: 'Benin',
-  BO: 'Bolivia',
-  BR: 'Brazil',
-  BS: 'Bahamas',
-  BW: 'Botswana',
-  BY: 'Belarus',
-  BZ: 'Belize',
-  CA: 'Canada',
-  CH: 'Switzerland',
-  CL: 'Chile',
-  CN: 'China',
-  CO: 'Colombia',
-  CR: 'Costa Rica',
-  CU: 'Cuba',
-  CY: 'Cyprus',
-  CZ: 'Czechia',
-  DE: 'Germany',
-  DK: 'Denmark',
-  DO: 'Dominican Republic',
-  EC: 'Ecuador',
-  EE: 'Estonia',
-  EG: 'Egypt',
-  ES: 'Spain',
-  FI: 'Finland',
-  FO: 'Faroe Islands',
-  FR: 'France',
-  GA: 'Gabon',
-  GB: 'United Kingdom',
-  GD: 'Grenada',
-  GL: 'Greenland',
-  GM: 'Gambia',
-  GR: 'Greece',
-  GT: 'Guatemala',
-  GY: 'Guyana',
-  HN: 'Honduras',
-  HR: 'Croatia',
-  HT: 'Haiti',
-  HU: 'Hungary',
-  ID: 'Indonesia',
-  IE: 'Ireland',
-  IM: 'Isle of Man',
-  IS: 'Iceland',
-  IT: 'Italy',
-  JE: 'Jersey',
-  JM: 'Jamaica',
-  JP: 'Japan',
-  LI: 'Liechtenstein',
-  LS: 'Lesotho',
-  LT: 'Lithuania',
-  LU: 'Luxembourg',
-  LV: 'Latvia',
-  MA: 'Morocco',
-  MC: 'Monaco',
-  MD: 'Moldova',
-  MG: 'Madagascar',
-  MK: 'Macedonia',
-  MN: 'Mongolia',
-  MT: 'Malta',
-  MX: 'Mexico',
-  MZ: 'Mozambique',
-  NA: 'Namibia',
-  NE: 'Niger',
-  NI: 'Nicaragua',
-  NL: 'Netherlands',
-  NO: 'Norway',
-  NZ: 'New Zealand',
-  PA: 'Panama',
-  PE: 'Peru',
-  PL: 'Poland',
-  PR: 'Puerto Rico',
-  PT: 'Portugal',
-  PY: 'Paraguay',
-  RO: 'Romania',
-  RS: 'Serbia',
-  RU: 'Russia',
-  SE: 'Sweden',
-  SI: 'Slovenia',
-  SJ: 'Svalbard and Jan Mayen',
-  SK: 'Slovakia',
-  SM: 'San Marino',
-  SR: 'Suriname',
-  SV: 'El Salvador',
-  TN: 'Tunisia',
-  TR: 'Turkey',
-  UA: 'Ukraine',
-  US: 'United States',
-  UY: 'Uruguay',
-  VA: 'Vatican City',
-  VE: 'Venezuela',
-  VN: 'Vietnam',
-  ZA: 'South Africa',
-  ZW: 'Zimbabwe'
-}
+const supportedCountries = [
+  'AD', 'AL', 'AR', 'AT', 'AU', 'AX', 'BB', 'BE', 'BG', 'BJ', 'BO', 'BR',
+  'BS', 'BW', 'BY', 'BZ', 'CA', 'CH', 'CL', 'CN', 'CO', 'CR', 'CU', 'CY',
+  'CZ', 'DE', 'DK', 'DO', 'EC', 'EE', 'EG', 'ES', 'FI', 'FO', 'FR', 'GA',
+  'GB', 'GD', 'GL', 'GM', 'GR', 'GT', 'GY', 'HN', 'HR', 'HT', 'HU', 'ID',
+  'IE', 'IM', 'IS', 'IT', 'JE', 'JM', 'JP', 'LI', 'LS', 'LT', 'LU', 'LV',
+  'MA', 'MC', 'MD', 'MG', 'MK', 'MN', 'MT', 'MX', 'MZ', 'NA', 'NE', 'NI',
+  'NL', 'NO', 'NZ', 'PA', 'PE', 'PL', 'PR', 'PT', 'PY', 'RO', 'RS', 'RU',
+  'SE', 'SI', 'SJ', 'SK', 'SM', 'SR', 'SV', 'TN', 'TR', 'UA', 'US', 'UY',
+  'VA', 'VE', 'VN', 'ZA', 'ZW'
+]
 
 module.exports = class Holidays extends Command {
   constructor (client) {
@@ -113,13 +21,15 @@ module.exports = class Holidays extends Command {
       category: 'utility',
       parameters: [{
         type: 'string',
-        whitelist: Object.keys(countries),
+        whitelist: supportedCountries,
         missingError: ({ t }) => {
           return new SwitchbladeEmbed()
             .setTitle(t('commands:holidays.invalidCountry'))
-            .setDescription(Object.entries(countries)
-              .map(([countryCode, countryName]) =>
-                `${countryCode} - ${countryName}`
+            .setDescription(supportedCountries
+              .map(countryCode => {
+                const countryName = t(`countries:${countryCode}`)
+                return `${countryCode} - ${countryName}`
+              }
               ).join('\n')
             )
         }
@@ -135,7 +45,7 @@ module.exports = class Holidays extends Command {
     try {
       channel.startTyping()
       const embed = new SwitchbladeEmbed(author)
-      const countryName = countries[countryCode.toUpperCase()]
+      const countryName = t(`countries:${countryCode.toUpperCase()}`)
       const url = `https://date.nager.at/api/v2/publicholidays/${year}/${countryCode}`
       const holidays = await fetch(url)
         .then(res => res.json())
