1dc31e60a3e769c9769203ddd398bef8a25d5205
Raheem Idowu <rzh@paradox.localdomain>
2019-10-14 16:56:10 +0400

    Fixed some style issues

--- src/commands/moderation/createrole.js
+++ src/commands/moderation/createrole.js
@@ -1,33 +1,33 @@
-const {Command, SwitchbladeEmbed, Constants} = require('../../');
+const { Command, SwitchbladeEmbed, Constants } = require('../../')
 
 module.exports = class CreateRole extends Command {
-    constructor (client){
-        super(client, {
-            name: 'createrole',
-            category: 'moderation',
-            requirements : {guildOnly: true, botPermissions: ["MANAGE_ROLES"], permissions: ["MANAGE_ROLES"]},
-            parameters: [
-                {type: 'color', missingError: 'commands:createrole.noParams'},
-                {type: 'string', full: true, missingError: 'commands:createrole.noParams'}
-            ]
+  constructor (client) {
+    super(client, {
+      name: 'createrole',
+      category: 'moderation',
+      requirements: { guildOnly: true, botPermissions: ['MANAGE_ROLES'], permissions: ['MANAGE_ROLES'] },
+      parameters: [
+        { type: 'color', missingError: 'commands:createrole.noParams' },
+        { type: 'string', full: true, missingError: 'commands:createrole.noParams' }
+      ]
 
-        })
-    }
+    })
+  }
 
-    async run ({ channel, guild, author, t}, color, name){
-        const hexcode = color.rgb(true)
-        const embed = new SwitchbladeEmbed(author)
-        await guild.createRole({name: name, color:hexcode}).then(role => {
-            embed
-                .setTitle(t('commands:createrole.successTitle'))
-                .setDescription(`Name of the role: ${name}`)
-                .setColor(hexcode)
-        }).catch(err => {
-            embed
-                .setColor(Constants.ERROR_COLOR)
-                .setTitle(t('commands:createrole.errorTitle'))
-                .setDescription(`\`${err}\``)
-        })
-        channel.send(embed)
-    }
+  async run ({ channel, guild, author, t }, color, name) {
+    const hexcode = color.rgb(true)
+    const embed = new SwitchbladeEmbed(author)
+    await guild.createRole({ name: name, color: hexcode }).then(role => {
+      embed
+        .setTitle(t('commands:createrole.successTitle'))
+        .setDescription(`Name of the role: ${name}`)
+        .setColor(hexcode)
+    }).catch(err => {
+      embed
+        .setColor(Constants.ERROR_COLOR)
+        .setTitle(t('commands:createrole.errorTitle'))
+        .setDescription(`\`${err}\``)
+    })
+    channel.send(embed)
+  }
 }
