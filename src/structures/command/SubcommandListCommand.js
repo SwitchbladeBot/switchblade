9f13bc21f3297fb4a0fae20e388ddad0379c0fd2
Pedro Fracassi <pedroluizfracassi@gmail.com>
2019-10-19 15:58:26 -0300

    Add SubcommandListCommand

---
+++ src/structures/command/SubcommandListCommand.js
@@ -0,0 +1,28 @@
+const Command = require('./Command.js')
+const SwitchbladeEmbed = require('../SwitchbladeEmbed.js')
+
+module.exports = class SubcommandListCommand extends Command {
+  constructor (client, options = {}) {
+    super(client, options)
+    this.embedColor = options.embedColor
+    this.authorString = options.authorString
+    this.authorImage = options.authorImage
+    this.authorURL = options.authorURL
+    this.embedColor = options.embedColor
+  }
+
+  async run ({ channel, author, t, prefix }) {
+    // TODO: Make this list the subcommands fathered by this command
+    channel.send(
+      new SwitchbladeEmbed(author)
+        .setAuthor(t(this.authorString), this.authorImage, this.authorURL)
+        .setDescriptionFromBlockArray(this.subcommands.map(subcommand => {
+          return [
+            `\`${subcommand.usage(t, prefix, false, true)}\``,
+            t([`commands:${subcommand.tPath}.commandDescription`, 'commands:help.noDescriptionProvided'])
+          ]
+        }))
+        .setColor(this.embedColor || process.env.EMBED_COLOR)
+    )
+  }
+}
