c46a384a81504be04329bf7fc069167cc1a43346
Raheem Idowu <rzh@paradox.localdomain>
2019-10-21 14:01:30 +0400

    Switching parameter order for spaces in role name

--- src/commands/moderation/createrole.js
+++ src/commands/moderation/createrole.js
@@ -7,27 +7,28 @@ module.exports = class CreateRole extends Command {
       category: 'moderation',
       requirements: { guildOnly: true, botPermissions: ['MANAGE_ROLES'], permissions: ['MANAGE_ROLES'] },
       parameters: [
-        { type: 'string', full: false, missingError: 'commands:createrole.noParams', required: true },
-        { type: 'color', required: false }
+        { type: 'color', required: false },
+        { type: 'string', full: true, missingError: 'commands:createrole.noParams', required: true }
       ]
-
     })
   }
 
-  async run ({ channel, guild, author, t }, name, color = new Color('#ffffff')) {
+  async run ({ channel, guild, author, t }, color = new Color('#ffffff'), name) {
     const hexcode = color.rgb(true)
     const embed = new SwitchbladeEmbed(author)
-    await guild.createRole({ name, color: hexcode }).then(role => {
+    
+    try {
+      await guild.createRole({ name, color: hexcode })
       embed
         .setTitle(t('commands:createrole.successTitle'))
         .setDescription(t('commands:createrole.successMessage', { name }))
         .setColor(hexcode)
-    }).catch(err => {
+    } catch(err) {
       embed
         .setColor(Constants.ERROR_COLOR)
         .setTitle(t('commands:createrole.errorTitle'))
         .setDescription(`\`${err}\``)
-    })
+    }
 
     channel.send(embed)
   }
