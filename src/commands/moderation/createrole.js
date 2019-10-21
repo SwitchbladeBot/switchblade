477b6516d4423d256ad477486d40f01986e4e536
Raheem Idowu <rzh@paradox.localdomain>
2019-10-21 14:06:51 +0400

    Never forget npm run fix

--- src/commands/moderation/createrole.js
+++ src/commands/moderation/createrole.js
@@ -16,14 +16,14 @@ module.exports = class CreateRole extends Command {
   async run ({ channel, guild, author, t }, color = new Color('#ffffff'), name) {
     const hexcode = color.rgb(true)
     const embed = new SwitchbladeEmbed(author)
-    
+
     try {
       await guild.createRole({ name, color: hexcode })
       embed
         .setTitle(t('commands:createrole.successTitle'))
         .setDescription(t('commands:createrole.successMessage', { name }))
         .setColor(hexcode)
-    } catch(err) {
+    } catch (err) {
       embed
         .setColor(Constants.ERROR_COLOR)
         .setTitle(t('commands:createrole.errorTitle'))
