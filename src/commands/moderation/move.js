7bc33d79ba4eb39996ca6d279453d2fe9768b4e4
moruzerinho6 <zerinho6@gmail.com>
2020-05-18 18:00:02 -0300

    removed comments and string fixes

--- src/commands/moderation/move.js
+++ src/commands/moderation/move.js
@@ -31,17 +31,15 @@ module.exports = class Move extends Command {
         destinationChannel.send(t('commands:move.messageMoved', { authorName: link.author.username, movedFrom: link.channel.name, movedBy: author.username }))
 
         if (link.content.length >= 1 || link.attachments.size >= 1) await destinationChannel.send(link.content.length >= 1 ? link.content : '', link.attachments.size >= 1 ? new Discord.Attachment(link.attachments.first().url) : {})
-        // Await is needed because the bot might delete the message before it's finally sent. Yes, the bot is that fast. ~ Zerinho6
+
         if (link.embeds.length >= 1) {
           if (!link.channel.permissionsFor(this.client.user.id).has('EMBED_LINKS')) throw new CommandError(t('commands:move.cantSendEmbed'));
 
           ['fields', 'title', 'description', 'url', 'timestamp', 'color', 'image', 'thumbnail', 'author'].forEach(p => {
             embed[p] = link.embeds[0][p]
           })
-          // Thanks for the suggestion, mete. ~ Zerinho6
 
           await destinationChannel.send(embed)
-          // Same reason in line 34
         }
         link.delete()
       } catch (e) {
