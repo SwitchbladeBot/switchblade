f0e848ba930671f9be68cba3e28c15a5c4ee888c
Pedro Fracassi <pedroluizfracassi@gmail.com>
2020-07-05 21:38:24 -0300

    Use SubcommandListcommand instead

--- src/commands/games/minecraft.js
+++ src/commands/games/minecraft.js
@@ -1,29 +1,14 @@
-const { Command, SwitchbladeEmbed } = require('../../')
+const { SubcommandListCommand } = require('../../')
 
-const types = ['server', 'sv', 'namehistory', 'skin', 's', 'nh', 'nameh', 'minecraftskin', 'mskin']
-
-module.exports = class Minecraft extends Command {
+module.exports = class Minecraft extends SubcommandListCommand {
   constructor (client) {
     super({
       name: 'minecraft',
       aliases: ['minecraftquery', 'mc', 'mcquery'],
       category: 'games',
-      parameters: [{
-        type: 'string',
-        full: true,
-        whitelist: types,
-        missingError: ({ t, prefix }) => {
-          return new SwitchbladeEmbed().setTitle(t('commons:search.noType'))
-            .setDescription([
-              this.usage(t, prefix),
-              '',
-              `__**${t('commons:search.types')}:**__`,
-              `\`${['server', 'namehistory', 'skin'].join('`, `')}\``
-            ].join('\n'))
-        }
-      }]
+      authorString: 'commands:minecraft.gameName',
+      authorImage: 'https://i.imgur.com/DBkQ0K5.png',
+      authorURL: 'https://minecraft.net'
     }, client)
-
-    this.MINECRAFT_LOGO = 'https://i.imgur.com/DBkQ0K5.png'
   }
 }
