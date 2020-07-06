03249b1eae673f8078d8d9ad74d1578ef984fd51
Pedro Fracassi <pedroluizfracassi@gmail.com>
2020-07-05 21:55:23 -0300

    Update Deezer and Spotify to use SubcommandListCommand

--- src/commands/misc/spotify.js
+++ src/commands/misc/spotify.js
@@ -1,29 +1,15 @@
-const { Command, SwitchbladeEmbed } = require('../../')
+const { SubcommandListCommand } = require('../../')
 
-const types = ['track', 'song', 't', 's', 'album', 'al', 'artist', 'ar', 'playlist', 'p', 'user', 'u']
-
-module.exports = class Spotify extends Command {
+module.exports = class Spotify extends SubcommandListCommand {
   constructor (client) {
     super({
       name: 'spotify',
       aliases: ['sp'],
       requirements: { apis: ['spotify'] },
-      parameters: [{
-        type: 'string',
-        full: true,
-        whitelist: types,
-        missingError: ({ t, prefix }) => {
-          return new SwitchbladeEmbed().setTitle(t('commons:search.noType'))
-            .setDescription([
-              this.usage(t, prefix),
-              '',
-              `__**${t('commons:search.types')}:**__`,
-              `\`${['track', 'album', 'artist', 'playlist', 'user'].join('`, `')}\``
-            ].join('\n'))
-        }
-      }]
+      authorString: 'commands:spotify.serviceName',
+      authorImage: 'https://i.imgur.com/vw8svty.png',
+      authorURL: 'https://spotify.com',
+      embedColor: '#18d860'
     }, client)
-
-    this.SPOTIFY_LOGO = 'https://i.imgur.com/vw8svty.png'
   }
 }
