e3a298feb7a69a5cea2a8e2a2fe0ce4719eb06d4
Doges <doges@switchblade.xyz>
2020-07-11 13:51:51 -0300

    Update src/commands/nsfw/e621.js
    Co-authored-by: Waxtz <waxtz@hotmail.com>

--- src/commands/nsfw/e621.js
+++ src/commands/nsfw/e621.js
@@ -27,7 +27,12 @@ module.exports = class e621 extends Command {
         .setImage(posts[0].file.url)
         .setTitle(`${t('commands:e621.id')}: ${posts[0].id}`)
         .setDescription(`**${t('commands:e621.artist')}**: "${posts[0].tags.artist[0] || 'Unknown artist'}"\n**${t('commands:e621.desc')}**: "${posts[0].description.replace(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/, '') || 'No Description'}"\n **ID**: ${posts[0].id}\n[${t('commands:e621.post')}](https://e621.net/posts/${posts[0].id})`)
-      channel.send(embed)
+      channel.send(
+        new SwitchbladeEmbed()
+          .setImage(posts[0].file.url)
+          .setTitle(`${t('commands:e621.id')}: ${posts[0].id}`)
+          .setDescription(`**${t('commands:e621.artist')}**: "${posts[0].tags.artist[0] || 'Unknown artist'}"\n**${t('commands:e621.desc')}**: "${posts[0].description.replace(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/, '') || 'No Description'}"\n **ID**: ${posts[0].id}\n[${t('commands:e621.post')}](https://e621.net/posts/${posts[0].id})`)
+      )
       channel.stopTyping(true)
     } catch (e) {
       channel.stopTyping(true)
