8250444c213e0d3c0d527b06de2c819b2d76315b
Doges <doges@switchblade.xyz>
2020-07-14 20:34:09 -0300

    🇧🇷 Fix standard

--- src/apis/sinesp.js
+++ src/apis/sinesp.js
@@ -2,17 +2,17 @@ const { APIWrapper } = require('../')
 const sinespApi = require('sinesp-api')
 
 module.exports = class Sinesp extends APIWrapper {
-    constructor () {
-        super({
-            name: 'sinesp'
-        })
-    }
+  constructor () {
+    super({
+      name: 'sinesp'
+    })
+  }
 
-    async searchPlate (plt) {
-        try {
-            return await sinespApi.search(plt)
-        } catch (e) {
-            return new Error('Not found')
-        }
+  async searchPlate (plt) {
+    try {
+      return await sinespApi.search(plt)
+    } catch (e) {
+      return new Error('Not found')
     }
+  }
 }
