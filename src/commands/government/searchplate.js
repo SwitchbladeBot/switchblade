ae22c6650dfc770dc16552f539c91b6ff39b5245
Doges <doges@switchblade.xyz>
2020-07-14 20:33:29 -0300

    🇧🇷 SearchPlate command

---
+++ src/commands/government/searchplate.js
@@ -0,0 +1,39 @@
+const { Command, CommandError, SwitchbladeEmbed, Constants } = require('../../')
+const { cleanContent } = require('../../utils/DiscordUtils')
+
+module.exports = class searchPlate extends Command {
+    constructor (client) {
+        super({
+            name: 'searchplate',
+            alias: [ 'searchPlate' ],
+            category: 'government',
+            requirements: {
+                apis: [ 'sinesp', 'gsearch' ]
+            },
+            parameters: [{
+                type: 'string',
+                full: true,
+                missingError: 'commands:sinesp.noplate'
+            }]
+        }, client)
+    }
+
+    async run ({ t, author, channel }, plt) {
+        channel.startTyping()
+        const plate = await this.client.apis.sinesp.searchPlate(plt)
+        try {
+            // vv  i know that's actually hardcoded :hahaa:
+            const carpic = await this.client.apis.gsearch.searchImage(`${plate.modelo} ${plate.anoModelo} a venda&imgSize=medium`)
+            channel.send(
+                new SwitchbladeEmbed()
+                    .setDescription(`**${t('commands:searchplate.plate')}**: ${plate.placa}\n**${t('commands:searchplate.model')}**: ${plate.marca} ${plate.modelo}\n**${t('commands:searchplate.year')}**: ${plate.anoModelo}\n**${t('commands:searchplate.color')}**: ${plate.cor}\n**${t('commands:searchplate.city')}**: ${plate.municipio} - ${plate.uf}\n**${t('commands:searchplate.status')}**: ${(plate.situacao !== 'Roubo/Furto') ? t('commands:searchplate.notstolen') : `**${t('commands:searchplate.stolen')}**`}\n**${t('commands:searchplate.chassis')}**: ${cleanContent(plate.chassi)}\n\n**${t('commands:searchplate.image')}**:`)
+                    .setImage(carpic.items[0].link)
+                    .setColor((plate.situacao !== 'Roubo/Furto') ? Constants.GUILD_ADDED_COLOR : Constants.GUILD_LOST_COLOR)
+            )
+            channel.stopTyping(true)
+        } catch (e) {
+            channel.stopTyping(true)
+            throw new CommandError(`${t('commands:e621.notFound')}`)
+        }
+    }
+}
