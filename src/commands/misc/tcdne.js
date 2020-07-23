5ccd7b784ebb0659b837a17bba8412e9c9f583e3
Doges <31023616+Doges@users.noreply.github.com>
2020-07-23 18:12:10 -0300

    Update src/commands/misc/tcdne.js
    Co-authored-by: Pedro Fracassi <pedroluizfracassi@gmail.com>

--- src/commands/misc/tcdne.js
+++ src/commands/misc/tcdne.js
@@ -4,7 +4,7 @@ module.exports = class TCDNE extends Command {
   constructor (client) {
     super({
       name: 'tcdne',
-      aliases: ['thiscatdoesnotexists'],
+      aliases: ['thiscatdoesnotexist'],
       category: 'misc'
     }, client)
   }
