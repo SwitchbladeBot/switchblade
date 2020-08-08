db9665a80d38d4bd3830b9de467f15b182a8ba0f
moruzerinho6 <zerinho6@gmail.com>
2020-08-08 12:49:02 -0300

    Better code, now in hoist order

--- src/commands/utility/rolelist.js
+++ src/commands/utility/rolelist.js
@@ -1,8 +1,7 @@
 const { Command, SwitchbladeEmbed, PaginatedEmbed, CommandError } = require('../../')
-
 function compare (a, b) {
-  if (a.position < b.position) return -1
-  if (a.position > b.position) return 1
+  if (a.position < b.position) return 1
+  if (a.position > b.position) return -1
   return 0
 }
 module.exports = class RoleList extends Command {
@@ -15,58 +14,26 @@ module.exports = class RoleList extends Command {
     }, client)
   }
 
-  async run ({ message, t, author, channel, language }) {
-    let rolesCache = message.guild.roles.cache.sort(compare)
-    const rolesText = []
-    const idsText = []
-    const embeds = []
-    const textPerEmbed = []
+  async run ({ message, t, author, channel }) {
+    const pagesArr = message.guild.roles.cache
+      .sort(compare)
+      .reduce((descriptionArr, role, id) => {
+        const line = `<@&${id}> \`\`<@&${role.name}:${id}>\`\``
+        const [lastDescription] = descriptionArr.slice(-1)
+        if (lastDescription && ([...lastDescription, line]).join('\n').length < 2048) {
+          lastDescription.push(line)
+        } else {
+          descriptionArr.push([line])
+        }
+        return descriptionArr
+      }, [])
+      .map(desc => new SwitchbladeEmbed(author).setDescription(desc.join('\n')))
 
-    rolesCache.forEach((role) => {
-      rolesText.push(`<@&${role.id}>`)
-      idsText.push(`\`\`<@&${role.id}>\`\``)
-    })
-
-    rolesCache.clear()
-    let i, l
-    for (i = 0, l = 0; i < rolesText.length; i++) {
-      const [roleText, idText] = [rolesText[i], idsText[i]]
-
-      if (i > 0 && textPerEmbed[l] && ((textPerEmbed[l][0].length + roleText.length + 1) >= 1024 || (textPerEmbed[l][1].length + idText.length + 1) >= 1024)) {
-        l++
-      }
-
-      if (textPerEmbed[l]) {
-        textPerEmbed[l][0] += `\n${roleText}`
-        textPerEmbed[l][1] += `\n${idText}`
-      } else {
-        textPerEmbed[l] = []
-        textPerEmbed[l][0] = roleText
-        textPerEmbed[l][1] = idText
-      }
+    if (pagesArr.length < 0) {
+      throw new CommandError(t('errors:guildHasNoRole'))
     }
 
-    rolesText.splice(0, rolesText.length)
-    textPerEmbed.forEach((group) => {
-      const currentEmbed = new SwitchbladeEmbed(author)
-      currentEmbed.addField(t('commands:rolelist.roles'), group[0], true)
-      currentEmbed.addField(t('commands:rolelist.ids'), group[1], true)
-      embeds.push(currentEmbed)
-    })
-
-    textPerEmbed.splice(0, textPerEmbed.length)
-    const pages = new PaginatedEmbed(t, author)
-
-    embeds.forEach((embed) => {
-      pages.addPage(embed)
-    })
-
-    embeds.splice(0, embeds.length)
-    try {
-      const msg = await channel.send(t('commands:rolelist.loading'))
-      pages.run(msg)
-    } catch (e) {
-      throw new CommandError(t('commands:move.couldntSendMessage'))
-    }
+    const pages = new PaginatedEmbed(t, author, pagesArr)
+    pages.run(channel)
   }
 }
