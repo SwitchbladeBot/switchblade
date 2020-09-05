3e0535852f85856d97f625c03c4e08f4791d743e
almeidx <almeidx@pm.me>
2020-09-05 21:05:48 +0100

    command base created

---
+++ src/commands/bot/commandsource.js
@@ -0,0 +1,57 @@
+const { Command, CommandError, SwitchbladeEmbed, GitUtils } = require('../../')
+const path = require('path')
+
+const regexpSpecialChars = /([[\]^$|()\\+*?{}=!.])/gi
+const quoteRegex = (text) => text.replace(regexpSpecialChars, '\\$1')
+const prefixRegex = (prefix) => new RegExp(`^${quoteRegex(prefix)}`)
+
+const REPOSITORY_URL = 'https://github.com/SwitchbladeBot/switchblade'
+
+module.exports = class CommandSource extends Command {
+  constructor (client) {
+    super({
+      name: 'commandsource',
+      aliases: ['cmdsource', 'source'],
+      category: 'bot',
+      parameters: [{
+        type: 'string', full: true, required: true, missingError: 'commands:commandsource.missingText'
+      }]
+    }, client)
+  }
+
+  async run ({ channel, author, t, prefix }, cmd) {
+    const branchOrHash = await GitUtils.getHashOrBranch()
+    console.log(branchOrHash)
+    if (!branchOrHash) throw new CommandError(t('commands:commandsource.branchNotUpToDate'))
+    const validCommands = this.client.commands.filter(c => !c.hidden)
+
+    cmd = cmd.replace(prefixRegex(prefix), '')
+    const command = cmd.split(' ').reduce((o, ca) => {
+      const arr = (Array.isArray(o) && o) || (o && o.subcommands)
+      if (!arr) return o
+      return arr.find(c => c.name === ca || (c.aliases && c.aliases.includes(ca)))
+    }, validCommands)
+
+    if (!command) throw new CommandError(t('commands:help.commandNotFound'))
+
+    const paths = ['src', 'commands', command.category, command.name]
+    console.log(process.cwd(), ...paths)
+    let cmdPath = path.join(process.cwd(), 'src', 'commands', command.category, command.name)
+    try {
+      require(cmdPath)
+    } catch (_) {
+      throw new CommandError(t('commands:commandsource.commandFileNotFound'))
+    }
+
+    const embed = new SwitchbladeEmbed(author)
+      .setTitle(command.name)
+      .setDescriptionFromBlockArray([
+        [
+          `${t('commands:commandsource.currentBranchOrCommit')} [${branchOrHash}](${REPOSITORY_URL}/tree/${branchOrHash})`,
+          `[${t('commands:commandsource.commandSource')}](${REPOSITORY_URL}/blob/${branchOrHash}/${paths.join('/')}.js)`
+        ]
+      ])
+
+    channel.send(embed)
+  }
+}
---
+++ src/utils/GitUtils.js
@@ -0,0 +1,25 @@
+const axios = require('axios')
+const fs = require('fs')
+
+module.exports = class GitUtils {
+  static async getHashOrBranch () {
+    const rev = fs.readFileSync('.git/HEAD').toString().trim()
+
+    if (rev.startsWith('ref:')) {
+      const branch = rev.replace(/^ref: refs\/heads\//, '')
+      const localHash = fs.readFileSync('.git/' + rev.substring(5)).toString().trim()
+
+      let res
+      try {
+        res = await axios.get(`https://api.github.com/repos/SwitchbladeBot/switchblade/commits/${branch}`)
+      } catch (_) {
+        return false
+      }
+      const originHash = res.data[0].sha
+
+      return localHash === originHash ? branch : localHash
+    } else {
+      return rev
+    }
+  }
+}
