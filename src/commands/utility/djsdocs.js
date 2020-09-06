ae4229c43fd7a8d9705d27e0bb150faae8eb88e4
DerpyXD <D3rpyXD@protonmail.ch>
2020-09-06 20:59:45 -0300

    command name changed, command aliases and sources documentation added

---
+++ src/commands/utility/djsdocs.js
@@ -0,0 +1,35 @@
+const { Command } = require('../..')
+const fetch = require('node-fetch')
+const VALID_SOURCES = ['stable', 'master', 'commando', 'rpc', 'akairo-master', 'collection']
+
+module.exports = class Docs extends Command {
+  constructor (client) {
+    super({
+      name: 'djsdocs',
+      aliases: ['djdocs'],
+      category: 'utility',
+      parameters: [{
+        type: 'string',
+        full: true,
+        clean: false,
+        missingError: 'commands:docs.noQueryProvided'
+      }, [{
+        type: 'string',
+        name: 'source',
+        aliases: ['src'],
+        required: false
+      }]]
+    }, client)
+  }
+
+  async run ({ channel, flags }, query) {
+    const { source } = flags
+    const src = VALID_SOURCES.includes(source) ? source : 'stable'
+
+    const queryParams = new URLSearchParams({ src, q: query })
+    const embed = await fetch(`https://djsdocs.sorta.moe/v2/embed?${queryParams.toString()}`)
+      .then(res => res.json())
+
+    channel.send({ embed }).then(() => channel.stopTyping())
+  }
+}
