4c02926afbebce5ad5bb1823c9155b75ea9ac516
pedrofracassi <pedroluizfracassi@gmail.com>
2020-09-07 12:48:04 -0300

    Update commandsource to use CommandParameter

--- src/commands/bot/commandsource.js
+++ src/commands/bot/commandsource.js
@@ -1,9 +1,6 @@
 const { Command, CommandError, SwitchbladeEmbed, GitUtils } = require('../../')
 const { sep } = require('path')
 
-const quoteRegex = (text) => text.replace(/([[\]^$|()\\+*?{}=!.])/gi, '\\$1')
-const prefixRegex = (prefix) => new RegExp(`^${quoteRegex(prefix)}`)
-
 const REPOSITORY_URL = 'https://github.com/SwitchbladeBot/switchblade'
 
 module.exports = class CommandSource extends Command {
@@ -13,29 +10,22 @@ module.exports = class CommandSource extends Command {
       aliases: ['cmdsource', 'source'],
       category: 'bot',
       parameters: [{
-        type: 'string', full: true, required: true, missingError: 'commands:commandsource.missingText'
+        type: 'command',
+        full: true,
+        required: true,
+        missingError: 'commands:commandsource.missingText',
+        acceptHidden: true
       }]
     }, client)
   }
 
-  async run ({ channel, author, t, prefix }, cmd) {
+  async run ({ channel, author, t }, command) {
     const branchOrHash = await GitUtils.getHashOrBranch()
     if (!branchOrHash) throw new CommandError(t('commands:commandsource.branchNotUpToDate'))
-    const validCommands = this.client.commands.filter(c => !c.hidden)
 
-    cmd = cmd.replace(prefixRegex(prefix), '')
-    const command = cmd.split(' ').reduce((o, ca) => {
-      const arr = (Array.isArray(o) && o) || (o && o.subcommands)
-      if (!arr) return o
-      return arr.find(c => c.name === ca || (c.aliases && c.aliases.includes(ca)))
-    }, validCommands)
-
-    if (!command) throw new CommandError(t('commands:commandsource.commandNotFound'))
-
-    const embed = new SwitchbladeEmbed(author)
+    channel.send(new SwitchbladeEmbed(author)
       .setTitle(command.name)
       .setDescription(`[${t('commands:commandsource.commandSource')}](${REPOSITORY_URL}/blob/${branchOrHash}/${command.path.split(sep).join('/')}) ([${branchOrHash}](${REPOSITORY_URL}/tree/${branchOrHash}))`)
-
-    channel.send(embed)
+    )
   }
 }
