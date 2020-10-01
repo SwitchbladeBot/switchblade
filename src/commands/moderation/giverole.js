1f8c60b185d3215bf54c6c45bbd6f3e0ceaf0fb8
Doges <31023616+Doges@users.noreply.github.com>
2020-10-01 10:49:15 -0300

    bruh²

--- src/commands/moderation/giverole.js
+++ src/commands/moderation/giverole.js
@@ -18,7 +18,7 @@ module.exports = class GiveRole extends Command {
 
   async run ({ channel, guild, author, t }, member, role) {
     const embed = new SwitchbladeEmbed(author)
-    if (member.roles.highest.position < role.position) {
+    if (member.roles.highest.position < role.position && author.roles.highest.position < role.position) {
       await member.roles.add(role).then(() => {
         embed
           .setTitle(t('commands:giverole.roleAdded'))
