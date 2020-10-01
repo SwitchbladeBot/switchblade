901f3544578ebd164850724134ffb749d9a7efdb
DraftMan <contact@draftman.fr>
2020-10-01 16:26:30 +0200

    Lint forgoted for api wrapper

--- src/apis/FlatHub.js
+++ src/apis/FlatHub.js
@@ -4,21 +4,21 @@ const fetch = require('node-fetch')
 const API_URL = 'https://flathub.org/api/v1/apps'
 
 module.exports = class FlatHub extends APIWrapper {
-  constructor() {
+  constructor () {
     super({
       name: 'flathub'
     })
   }
 
-  async list() {
+  async list () {
     return fetch(API_URL).then(r => r.json())
   }
 
-  async getApp(app_id) {
-    return fetch(`${API_URL}/${app_id}`).then(r => r.json())
+  async getApp (appId) {
+    return fetch(`${API_URL}/${appId}`).then(r => r.json())
   }
 
-  async search(query) {
+  async search (query) {
     return fetch(`${API_URL}/search/${query}`).then(r => r.json())
   }
 }
