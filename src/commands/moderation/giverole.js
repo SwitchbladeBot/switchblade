eb26c72651784c6dc00d624ac726218db4872ca0
Doges <31023616+Doges@users.noreply.github.com>
2020-10-01 08:58:09 -0300

    + GiveRole command

--- src/commands/moderation/giverole.js
+++ src/commands/moderation/giverole.js
@@ -18,15 +18,19 @@ module.exports = class GiveRole extends Command {
 
   async run ({ channel, guild, author, t }, member, role) {
     const embed = new SwitchbladeEmbed(author)
-    await member.roles.add(role).then(() => {
-      embed
-        .setTitle(t('commands:giverole.roleAdded'))
-    }).catch(err => {
+    console.log(role.position + '\n' + member.roles.highest.position)
+    if (member.roles.highest.position < role.position) {
+      await member.roles.add(role).then(() => {
+        embed
+          .setTitle(t('commands:giverole.roleAdded'))
+      }).catch(() => {
+        throw new CommandError(t('commands:giverole.error'))
+      })
+    } else {
       embed
         .setColor(Constants.ERROR_COLOR)
-        .setTitle(t('commands:giverole.error'))
-        .setDescription(`\`${err}\``)
-    })
+        .setTitle(t('commands:giverole.higherError'))
+    }
     channel.send(embed)
   }
 }
