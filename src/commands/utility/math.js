6841cea2a8b8ba35c710070d31c7aae3b5407da0
blueslimee <blueslimexdfodeu@gmail.com>
2020-10-01 01:12:59 -0400

    Safer math command

--- src/commands/utility/math.js
+++ src/commands/utility/math.js
@@ -1,15 +1,24 @@
 const { Command, CommandError, SwitchbladeEmbed } = require('../../')
-const { create, all } = require('mathjs')
-const math = create(all)
-math.import({
-  'import': function () { throw new CommandError('Function import is disabled') },
-  'createUnit': function () { throw new CommandError('Function createUnit is disabled') },
-  'evaluate': function () { throw new CommandError('Function evaluate is disabled') },
-  'parse': function () { throw new CommandError('Function parse is disabled') },
-  'simplify': function () { throw new CommandError('Function simplify is disabled') },
-  'derivative': function () { throw new CommandError('Function derivative is disabled') },
-  'format': function () { throw new CommandError('Function format is disabled') }
-}, { override: true })
+const { StaticPool } = require('node-worker-threads-pool')
+const pool = new StaticPool({
+  size: 2,
+  task: (expression) => {
+    const { create, all } = require('mathjs')
+    const math = create(all)
+
+    math.import({
+      'import': function () { throw new CommandError('Function import is disabled') },
+      'createUnit': function () { throw new CommandError('Function createUnit is disabled') },
+      'evaluate': function () { throw new CommandError('Function evaluate is disabled') },
+      'parse': function () { throw new CommandError('Function parse is disabled') },
+      'simplify': function () { throw new CommandError('Function simplify is disabled') },
+      'derivative': function () { throw new CommandError('Function derivative is disabled') },
+      'format': function () { throw new CommandError('Function format is disabled') }
+    }, { override: true })
+
+    return math.eval(expression)
+  }
+})
 
 module.exports = class Math extends Command {
   constructor (client) {
@@ -27,7 +36,7 @@ module.exports = class Math extends Command {
   async run ({ t, author, channel }, expression) {
     const embed = new SwitchbladeEmbed(author)
     try {
-      const result = math.eval(expression)
+      const result = await pool.createExecutor().setTimeout(10 * 1000).exec(expression)
       embed.setTitle(t('commands:math.result', { result }))
     } catch (error) {
       this.client.log(`Failed math calculation ${expression}\nError: ${error.stack}`, { tags: [this.name] })
