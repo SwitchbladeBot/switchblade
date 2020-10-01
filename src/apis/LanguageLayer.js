09c6b7096ca0005551b89c2705c1d6510a6f893f
Vicente015 <33096355+Vicente015@users.noreply.github.com>
2020-10-01 14:23:25 +0100

    fix lint

--- src/apis/LanguageLayer.js
+++ src/apis/LanguageLayer.js
@@ -3,20 +3,20 @@ const fetch = require('node-fetch')
 const API_URL = 'http://api.languagelayer.com/'
 
 module.exports = class LanguageLayerAPI extends APIWrapper {
-    constructor() {
-        super({
-            name: 'languagelayer',
-            envVars: ['LANGUAGELAYER_API_KEY']
-        })
-    }
+  constructor() {
+    super({
+      name: 'languagelayer',
+      envVars: ['LANGUAGELAYER_API_KEY']
+    })
+  }
 
-    detectText(params = {}) {
-        return this.request(`/detect`, `?${params}`)
-    }
-
-    request(endpoint, query = {}) {
-        let apikey = `&access_key=${process.env.LANGUAGELAYER_API_KEY}`
-        return fetch(API_URL + endpoint + apikey + query)
-            .then(res => res.json())
-    }
+  detectText(params = {}) {
+    return this.request(`/detect`, `?${params}`)
+  }
+ 
+  request(endpoint, query = {}) {
+    let apikey = `&access_key=${process.env.LANGUAGELAYER_API_KEY}`
+    return fetch(API_URL + endpoint + apikey + query)
+      .then(res => res.json())
+  }
 }
