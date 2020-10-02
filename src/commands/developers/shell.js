220d0fee966add7e7fa4164e35f81aee2fcb879e
Andre Jarrell <mail@andrejarrell.com>
2020-10-02 13:49:46 +1000

    added shell command

---
+++ src/commands/developers/shell.js
@@ -0,0 +1,23 @@
+const { exec } = require('child_process')
+
+module.exports = class Eval extends Command {
+  constructor (client) {
+    super({
+      name: 'shell',
+      category: 'developers',
+      hidden: true,
+      requirements: { devOnly: true },
+      parameters: [{
+        type: 'string', full: true, missingError: 'errors:missingParameters', showUsage: false
+      }]
+    }, client)
+  }
+
+  run ({ channel }, command) {
+    exec(command, (error, stdout, stderr) => {
+      if (stderr) return channel.send(`\`${command}\`\n\`\`\`${stderr}\`\`\``);
+      channel.send(`\`${command}\`\n\`\`\`${stdout}\`\`\``);
+    });
+  }
+}
+  
