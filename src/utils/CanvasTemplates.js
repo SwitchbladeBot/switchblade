01c8a0ccced1e23e3cdd880af59ccd91ab476c2b
Matheus Dias <methidezeeni@gmail.com>
2020-10-05 12:04:18 -0300

    >:(

--- src/utils/CanvasTemplates.js
+++ src/utils/CanvasTemplates.js
@@ -9,7 +9,7 @@ let CanvasUtils = {}
 try {
   Canvas = require('canvas')
   CanvasUtils = require('./CanvasUtils.js')
-} catch (e) { }
+} catch (e) {}
 
 const { createCanvas, Image } = Canvas
 const { ALIGN, measureText } = CanvasUtils
