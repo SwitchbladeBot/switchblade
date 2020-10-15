f13c2f6b101ad6f822c13c63e7bdbeda8c560e76
almeidx <almeidx@pm.me>
2020-10-15 18:12:13 +0100

    feat: added steam command

---
+++ src/commands/misc/steam.js
@@ -0,0 +1,68 @@
+const { SearchCommand, SwitchbladeEmbed, Constants, CommandError } = require('../..')
+const moment = require('moment')
+
+module.exports = class Steam extends SearchCommand {
+  constructor (client) {
+    super({
+      name: 'steam',
+      aliases: ['steamstore'],
+      requirements: { apis: ['steamstore'] },
+      embedColor: Constants.STEAM_COLOR,
+      embedLogoURL: 'https://i.imgur.com/PA1NIoK.png'
+    }, client)
+  }
+
+  async search (_, query) {
+    const res = await this.client.apis.steamstore.search(query)
+    return res.data.items
+  }
+
+  searchResultFormatter (i) {
+    return `[${i.name}](https://store.steampowered.com/app/${i.id})`
+  }
+
+  async handleResult ({ t, author, channel, language, guild }, { id }) {
+    const res = await this.client.apis.steamstore.info(id)
+    const {
+      short_description: description,
+      name,
+      is_free: isFree,
+      website,
+      publishers,
+      header_image: image,
+      required_age: age,
+      price_overview: priceInfo,
+      categories,
+      release_date: { coming_soon: comingSoon, date },
+      metacritic
+    } = res.data[id].data
+
+    if (age >= 18 && guild && !channel.nsfw) throw new CommandError(t('errors:nsfwOnly'))
+
+    moment.locale(language)
+
+    channel.send(
+      new SwitchbladeEmbed(author)
+        .setAuthor('Steam Store', this.embedLogoURL, 'https://store.steampowered.com/')
+        .setThumbnail(image)
+        .setTitle(name)
+        .setURL(website)
+        .setDescriptionFromBlockArray([
+          [
+            description.slice(0, 1024)
+          ],
+          [
+            isFree ? t('commands:steam.free') : priceInfo.final_formatted,
+            t('commands:steam.by', { publisher: publishers.join(',') }),
+            comingSoon ? t('commands:steam.comingSoon') : t('commands:steam.released', { ago: moment(new Date(date)).fromNow() })
+          ],
+          [
+            metacritic ? `\`${t('commands:steam.metacriticScore')}\`: [${metacritic.score}](${metacritic.url})` : ''
+          ],
+          [
+            categories.map((c) => `\`${c.description}\``).join(', ')
+          ]
+        ])
+    )
+  }
+}
