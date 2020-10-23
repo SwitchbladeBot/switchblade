0ea6a1b3acae3ff647c83beaa1a5b5fdafaedea1
Fernando Silva <fernando457829@gmail.com>
2020-10-23 15:29:55 +0000

    add basic games structures

---
+++ src/structures/game/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  Player: require('./player/Player'),
+  PlayerManager: require('./player/PlayerManager'),
+
+  Game: require('./Game'),
+  TwoPlayerGame: require('./TwoPlayerGame'),
+  Matrix: require('./Matrix')
+}
