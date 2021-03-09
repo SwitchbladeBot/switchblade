f1954c6b6b4897492395fdc35f48419c66a8e7f2
Doges <Doges@users.noreply.github.com>
2021-03-09 11:08:02 -0300

    vixi

--- src/commands/government/searchplate.js
+++ src/commands/government/searchplate.js
@@ -32,7 +32,7 @@ module.exports = class searchPlate extends Command {
       channel.send(
         new SwitchbladeEmbed()
           .setDescription(`**${t('commands:searchplate.plate')}**: ${plate.placa}\n**${t('commands:searchplate.model')}**: ${plate.extra.marca_modelo.modelo}\n**${t('commands:searchplate.year')}**: ${plate.anoModelo}\n**${t('commands:searchplate.color')}**: ${plate.cor}\n**${t('commands:searchplate.type')}**: ${plate.extra.tipo_veiculo.tipo_veiculo} (${plate.extra.quantidade_passageiro} passengers)\n**${t('commands:searchplate.maxtorque')}**: ${plate.extra.potencia}bhp\n**${t('commands:searchplate.engineCapacity')}**: ${plate.extra.cilindradas}cc\n**${t('commands:searchplate.nationality')}**: ${plate.extra.nacionalidade.nacionalidade}\n**${t('commands:searchplate.fuelType')}**: ${plate.extra.combustivel.combustivel}\n**${t('commands:searchplate.city')}**: ${plate.municipio} - ${plate.uf}\n**${t('commands:searchplate.status')}**: ${(plate.situacao !== 'Roubo/Furto') ? t('commands:searchplate.notstolen') : `**${t('commands:searchplate.stolen')}**`}\n**${t('commands:searchplate.chassis')}**: ${cleanContent(plate.extra.chassi, message)}\n\n**${t('commands:searchplate.image')}**:`)
-          .setImage(carpic.items[0].link && 'https://i.imgur.com/awMCwj3.jpg')
+          .setImage(carpic.items[0].link || 'https://i.imgur.com/awMCwj3.jpg')
           .setColor((plate.situacao !== 'Roubo/Furto') ? Constants.GUILD_ADDED_COLOR : Constants.GUILD_LOST_COLOR)
           .attachFiles(attach)
           .setThumbnail('attachment://plate.png')
