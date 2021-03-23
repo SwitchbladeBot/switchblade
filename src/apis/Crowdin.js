83f5296a399ffdca60995c40e26f949cf691e3af
David Paiva <pillgp@icloud.com>
2021-03-23 10:12:26 +0000

    Changing unzip to adm-zip and refactoring code

--- src/apis/Crowdin.js
+++ src/apis/Crowdin.js
@@ -1,6 +1,6 @@
 const { APIWrapper } = require('../')
 const axios = require('axios')
-const unzip = require('unzip')
+const Zip = require('adm-zip')
 
 const BASE_URL = 'https://api.crowdin.net/api/project/'
 
@@ -12,31 +12,18 @@ module.exports = class CrowdinAPI extends APIWrapper {
     })
   }
 
-  download () {
-    return this.request('/download/all.zip')
-  }
+  async downloadToPath (path) {
+    const { data } = await this.request('/download/all.zip')
 
-  async downloadToStream (stream) {
-    return new Promise(async (resolve, reject) => {
-      await this.download()
-        .then(res => {
-          res.data.pipe(stream)
-            .on('error', reject)
-            .on('close', resolve)
-            .on('end', resolve)
-        })
-    })
-  }
-
-  downloadToPath (path) {
-    return this.downloadToStream(unzip.Extract({ path }))
+    const zip = new Zip(data)
+    zip.extractAllTo(path)
   }
 
   async request (endpoint) {
     return axios({
       method: 'GET',
       url: BASE_URL + process.env.CROWDIN_PROJECT_ID + endpoint + `?key=${process.env.CROWDIN_API_KEY}`,
-      responseType: 'stream'
+      responseType: 'arraybuffer'
     })
   }
 }
