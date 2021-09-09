71a83709664da5c3a3c73d41b2e4910d977af933
Matheus Dias <methidezeeni@gmail.com>
2021-09-09 13:56:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,9 +26,7 @@ console.log(readFileSync('bigtitle.txt', 'utf8').toString())
 const Switchblade = require('./src/Switchblade.js')
 const client = new Switchblade(CLIENT_OPTIONS)
 
-if (process.env.HIDE_DEBUG_LOGS !== 'TRUE') {
-  client.on('debug', (...args) => console.log('debug', ...args))
-  client.on('rateLimit', (...args) => console.log('rateLimit', ...args))
-}
+client.on('debug', (...args) => console.log('debug', ...args))
+client.on('rateLimit', (...args) => console.log('rateLimit', ...args))
 
 client.initialize()
