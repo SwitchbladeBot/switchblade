de24bc8203e63da030436493a0f72719b8d2974c
Doges <31023616+Doges@users.noreply.github.com>
2021-10-13 20:37:09 -0300

    Flight Radar command

--- src/commands/misc/flightradar24.js
+++ src/commands/misc/flightradar24.js
@@ -22,17 +22,17 @@ module.exports = class flightradar24 extends Command {
       const embed = new SwitchbladeEmbed()
         .setTitle(`${data.identification.callsign} - ${data.aircraft.registration} - ${data.aircraft.model.text}`)
         .setURL(`https://fr24.com/${data.identification.id}`)
-        .setDescription(`**${t('commands:flightradar24.airline')}:** ${data.airline.name}`)
+        .setDescription(`**${t('commands:flightradar24.airline')}:** ${data.airline?.name || 'Unknown'}`)
         .setThumbnail(data.airline.code ? `https://images.flightradar24.com/assets/airlines/logotypes/${data.airline.code.iata}_${data.airline.code.icao}.png` : 'https://i.imgur.com/qlPnbh2.png')
         .setColor(data.status.live ? Constants.GUILD_ADDED_COLOR : Constants.IDLE_AIRCRAFT_COLOR)
         .setTimestamp(null)
-      if (data.status.live === true && data.airport.origin) {
+      if (data.status.live && data.airport.origin) {
         embed.addField(t('commands:flightradar24.origin'), `**${t('commands:flightradar24.departed')} <t:${data.firstTimestamp}:R>**\n[${data.airport.origin.name}](${data.airport.origin.website}) (${data.airport.origin.code.iata}/${data.airport.origin.code.icao})\n**${t('commands:flightradar24:locatedAt')}:** ${data.airport.origin.position.region.city} - ${data.airport.origin.position.country.name} (${data.airport.origin.timezone.offsetHours})`, true)
       }
-      if (data.status.live === true && data.airport.destination) {
+      if (data.status.live && data.airport.destination) {
         embed.addField(t('commands:flightradar24.destination'), `**${t('commands:flightradar24.expectedArrival')} <t:${data.status.generic.eventTime.utc}:R>**\n[${data.airport.destination.name}](${data.airport.destination.website}) (${data.airport.destination.code.iata}/${data.airport.destination.code.icao})\n**${t('commands:flightradar24:locatedAt')}:** ${data.airport.destination.position.region.city} - ${data.airport.destination.position.country.name} (${data.airport.destination.timezone.offsetHours})`, true)
       }
-      embed.setImage(data.aircraft.images.large[0].src)
+      embed.setImage(data.aircraft.images.large?.[0].src || data.aircraft.images.sideview)
         .setFooter(`${t('commands:flightradar24.dataBy')} Flightradar24 | ${t('commands:flightradar24.photoBy')} ${data.aircraft.images.large[0].source} | ${t('commands:flightradar24.copyright')} ${data.aircraft.images.large[0].copyright}`)
       channel.send(embed)
     } catch (e) {
