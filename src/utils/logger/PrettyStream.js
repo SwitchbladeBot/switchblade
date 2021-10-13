a81dbd8ebd5bd1572279929bd152513a74ccbe8b
Fernando Silva <fernando457829@gmail.com>
2021-10-13 20:36:25 -0300

    add error stack logging

--- src/utils/logger/PrettyStream.js
+++ src/utils/logger/PrettyStream.js
@@ -2,7 +2,13 @@ const bunyan = require('bunyan')
 
 module.exports = class PrettyStream {
   write (rec) {
-    const msg = `[${rec.time.toISOString()}] ${bunyan.nameFromLevel[rec.level]}: ${rec.tag ? `[${rec.tag}] ` : ''}${rec.msg}`
+    const msg = [
+      `[${rec.time.toISOString()}] `,
+      `${bunyan.nameFromLevel[rec.level]}: `,
+      rec.tag ? `[${rec.tag}] ` : '',
+      rec.msg,
+      rec.err ? `\n${rec.err.stack}` : ''
+    ].join('')
 
     if (rec.level >= 50) console.error(msg)
     else console.log(msg)
