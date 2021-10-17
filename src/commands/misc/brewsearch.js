a01c42daf2a9162d570d9fa5c7b7d2182ab66581
Eroxl <77178777+Eroxl@users.noreply.github.com>
2021-10-17 14:55:02 -0700

    Update src/commands/misc/brewsearch.js
    Co-authored-by: Fernando Silva <fernando457829@gmail.com>

--- src/commands/misc/brewsearch.js
+++ src/commands/misc/brewsearch.js
@@ -21,22 +21,22 @@ module.exports = class BrewSearch extends Command {
       const formulaeData = await this.client.apis.brew.search(formulae)
       channel.send(
         new SwitchbladeEmbed(author)
-          .setTitle(t('commands:brew.title', { name: formulaeData.data.full_name }))
+          .setTitle(t('commands:brew.title', { name: formulaeData.full_name }))
           .setDescriptionFromBlockArray([
             [
-              `**${t('commands:brew.description')}**: ${formulaeData.data.desc}`,
+              `**${t('commands:brew.description')}**: ${formulaeData.desc}`,
               `**${t('commands:brew.analytics')}**:\n${t('commands:brew.30d')}: \`${
-                formulaeData.data.analytics.install['30d'][formulaeData.data.name]
+                formulaeData.analytics.install['30d'][formulaeData.name]
               }\`\n${t('commands:brew.90d')}: \`${
-                formulaeData.data.analytics.install['90d'][formulaeData.data.name]
+                formulaeData.analytics.install['90d'][formulaeData.name]
               }\`\n${t('commands:brew.365d')}: ${
-                formulaeData.data.analytics.install['365d'][formulaeData.data.name]
+                formulaeData.analytics.install['365d'][formulaeData.name]
               }`,
               `**${t('commands:brew.links')}**:\n[${t(
                 'commands:brew.homebrewLink'
-              )}](https://formulae.brew.sh/formula/${formulaeData.data.name}) [${t(
+              )}](https://formulae.brew.sh/formula/${formulaeData.name}) [${t(
                 'commands:brew.homePage'
-              )}](${formulaeData.data.homepage})`
+              )}](${formulaeData.homepage})`
             ]
           ])
           .setColor(Constants.HOMEBREW_COLOR)
