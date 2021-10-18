5fb79b09e8b3b39c9a63c7dfbec850bbd8da7245
AsianCat54x <0xsapphir3@gmail.com>
2021-10-18 12:31:46 +0530

    Remove unused lines

--- shard.js
+++ shard.js
@@ -1,7 +1,4 @@
 const { ShardingManager } = require('discord.js')
-const dotenv = require('dotenv')
-
-dotenv.config({"path": "./.env.example"})
 const manager = new ShardingManager('./index.js', {
   token: process.env.DISCORD_TOKEN,
   totalShards: parseInt(process.env.SHARD_COUNT) || 'auto'
