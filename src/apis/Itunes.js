1ca785618c49179cdc0a314b6bdb6dcfcaf86035
AsianCat54x <0xsapphir3@gmail.com>
2021-10-22 07:49:14 +0530

    Make requested changes

--- src/apis/Itunes.js
+++ src/apis/Itunes.js
@@ -1,59 +1,29 @@
-const {APIWrapper} = require('..')
+const { APIWrapper } = require('..')
 const axios = require('axios')
 
-const MEDIA_WHITE_LIST = ['movie', 'podcast', 'music', 'musicVideo', 'audiobook', 'shortFilm',
-'tvShow', 'software', 'ebook', 'all']
-
 module.exports = class ITunes extends APIWrapper {
-  constructor() {
-    super({name: 'itunes'})
+  constructor () {
+    super({ name: 'itunes' })
   }
 
-  async search(query) {
-    query = query.split(" ")
-
-    const media = query.at(0)
-    
-    var term = query.splice(1 , query.length - 2).join(" ")
-
-    var country = query.splice(query.length - 1 , 1).join(" ")
-
-    console.log(media , " - " , term , " - " , country)
-
-    // Check if the country code is 2 letters 
-    if(country.length != 2){
-      term += " " + country
-
-      country = "US"
-    }
-
-    if (!MEDIA_WHITE_LIST.includes(media)) {
-      return [{
-        'errorMessage':
-            `Invaid term provided. The term has to be one of the following: ${
-                MEDIA_WHITE_LIST.join(' , ')}`
-      }]
-    }
-
-    console.log(media , term , country)
-
+  async search (media , term , country) {
     try {
-      const {data} = await axios.get(`https://itunes.apple.com/search`, {
+      const { data } = await axios.get('https://itunes.apple.com/search', {
         params: {
           media: media,
           term: term,
           country: country,
-          limit: 10,
+          limit: 10
         }
       })
 
-      if (data.results == 0) {
+      if (data.results === 0) {
         throw new Error()
       }
 
       return data.results
-    } catch {
-      return [{'errorMessage': 'No results found'}]
+    } catch(err) {
+      return []
     }
   }
 }
--- src/commands/misc/itunes.js
+++ src/commands/misc/itunes.js
@@ -1,90 +1,83 @@
-const {SwitchbladeEmbed, SearchCommand, Constants, CommandStructures} =
-    require('../../')
-const {Command, CommandParameters, StringParameter} = CommandStructures
+const { SwitchbladeEmbed, Constants, Command } = require('../../')
 const moment = require('moment')
 
-module.exports = class Itunes extends SearchCommand {
-  constructor(client) {
-    super(
-        {
-          name: 'itunes',
-          requirements: {apis: ['itunes']},
-          embedColor: Constants.ITUNES_COLOR,
-          embedLogoURL: 'https://i.imgur.com/K8uqCSw.png',
+const MEDIA_WHITE_LIST = ['movie', 'podcast', 'music', 'musicVideo', 'audiobook', 'shortFilm', 'tvShow', 'software', 'ebook', 'all']
+
+module.exports = class Itunes extends Command {
+  constructor (client) {
+    super({
+      name: 'itunes',
+      requirements: { apis: ['itunes'] },
+      parameters: [{
+        type: "string",
+        full: false,
+        name: "media",
+        whitelist: MEDIA_WHITE_LIST,
+        missingError: ({ t, prefix }) => {
+          return new SwitchbladeEmbed().setTitle(t('commands:itunes.noMedia'))
+            .setDescription([
+              this.usage(t, prefix),
+              '',
+              `__**${t('commands:itunes.availableMedia')}:**__`,
+              `**${MEDIA_WHITE_LIST.map(l => `\`${l}\``).join(', ')}**`
+            ].join('\n'))
         },
-        client);
+        required: true,
+      } , {
+        type: "string",
+        full: true,
+        name: "term",
+        required: true,
+      }, {
+        type: "string",
+        full: false,
+        name: "country",
+        required: false,
+      }]
+    },
+    client)
+
+    this.author = null
+
+    this.embedUrl = "https://i.imgur.com/U4jjk5F.png"
+
   }
 
-  async search(_, query) {
-    const data = await this.client.apis.itunes.search(query)
+  async run (context , media , term , country = "US") {
+    this.author = context.author
 
-    return await data;
+    const data = await this.client.apis.itunes.search(media , term , country)
+
+    this.parseResponse(context , await data , context.message.content)
   }
 
-  searchResultFormatter(i) {
-    if (!i.error) {
-      return `[${i.trackName}](${i.trackViewUrl}) - [${i.artistName}](${
-          i.artistViewUrl})`
-    } else {
-      return i.error
-    }
+  searchResultFormatter (i) {
+    return `[${i.trackName}](${i.trackViewUrl}) - [${i.artistName}](${i.artistViewUrl})`
   }
 
-  getRatingEmojis(rating) {
+  getRatingEmojis (rating) {
     return (this.getEmoji('ratingstar', '⭐').repeat(Math.floor(rating))) +
-        (this.getEmoji('ratinghalfstar')
-             .repeat(Math.ceil(rating - Math.floor(rating))))
+            (this.getEmoji('ratinghalfstar')
+              .repeat(Math.ceil(rating - Math.floor(rating))))
   }
+  
+  async parseResponse({ channel , author, t } , data , title) {
+    var description = ""
 
-  async handleResult({t, author, channel}, data) {
-    if (data.errorMessage) {
-      const embed = new SwitchbladeEmbed(author)
-                        .setColor(Constants.ERROR_COLOR)
-                        .setTitle(data.errorMessage)
+    var index = 1
 
-      channel.send(embed)
+    const formatNumber = (n) => Number(n) > 9 ? n : '0' + n
 
-      return
+    for(let item of data){
+      description += `\`${formatNumber(index)}\`: ${this.searchResultFormatter(item)} \n`
+
+      index++
     }
 
-
-    const stars = this.getRatingEmojis(data.averageUserRating)
-
-    const embed =
-        new SwitchbladeEmbed(author)
-            .setColor(this.embedColor)
-            .setAuthor(
-                t('commands:itunes.place'), this.embedLogoURL,
-                'https://www.apple.com/itunes/')
-            .setURL(data.trackViewUrl)
-            .setTitle(
-                t('commands:itunes.track',
-                  {name: data.trackName, artistName: data.artistName}))
-            .setThumbnail(data.artworkUrl100)
-            .setDescriptionFromBlockArray([
-              [data.description ? `Description: ${
-                                      data.description.replace(
-                                          new RegExp('<[^>]*>', 'g'), '')}` :
-                   data.longDescription ?
-                                  `Description: ${data.longDescription}` :
-                                  ''],
-              [
-                data.userRatingCount ? t('commands:itunes.ratingCount',
-                                         {count: data.userRatingCount || 0}) :
-                                       '',
-                data.averageUserRating ?
-                    `Rating: ${stars} (${
-                        Math.round(data.averageUserRating) || ''})` :
-                    ''
-              ],
-              [t('commands:itunes.price', {
-                price: data.formattedPrice || data.trackPrice,
-                currency: data.currency
-              })],
-              [t('commands:itunes.release',
-                 {date: moment(data.releaseDate).format('DD/MM/YYYY')})],
-              [`Genres: ${(data.genres || ['-']).join(' , ')}`]
-            ])
+    const embed = new SwitchbladeEmbed(author)
+                      .setThumbnail(this.embedUrl)
+                      .setDescription(description)
+                      .setTitle(t("commands:itunes.title" , { title: title }))
 
     channel.send(embed)
   }
