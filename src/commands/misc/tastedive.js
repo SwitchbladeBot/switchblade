f33344168290d7eee22fecabd705fbb3548b7dc4
Almeida <almeidx@pm.me>
2021-10-31 20:32:05 +0000

    standard

--- src/commands/misc/tastedive.js
+++ src/commands/misc/tastedive.js
@@ -1,4 +1,4 @@
-const { SwitchbladeEmbed, Command , CommandError} = require('../../')
+const { SwitchbladeEmbed, Command, CommandError } = require('../../')
 
 const MEDIA_WHITE_LIST = ['music', 'movies', 'shows', 'podcasts', 'books', 'authors', 'games']
 
@@ -12,12 +12,12 @@ module.exports = class TasteDive extends Command {
           type: 'string',
           full: false,
           whitelist: MEDIA_WHITE_LIST,
-          missingError: 'commands:tastedive.noText' 
+          missingError: 'commands:tastedive.noText'
         }, {
           type: 'string',
           full: true,
           required: true,
-          missingError: 'commands:tastedive.noText' 
+          missingError: 'commands:tastedive.noText'
         }
       ]
     }, client)
@@ -26,8 +26,8 @@ module.exports = class TasteDive extends Command {
   async run ({ channel, t, message }, media, term) {
     const data = Array.from(await this.client.apis.tastedive.search(media, term))
 
-    if(data.length == 0){
-      throw new CommandError(t("commands:tastedive.noResults"))
+    if (data.length === 0) {
+      throw new CommandError(t('commands:tastedive.noResults'))
     }
 
     channel.send(this.parseResponse(t, message.content, data))
