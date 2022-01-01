456c8a042baa383705d6f937cc711ae7fa6d6828
Doges <31023616+Doges@users.noreply.github.com>
2022-01-01 19:55:10 -0300

    created the branch but forgot to send the commit

--- src/commands/utility/guildinfo.js
+++ src/commands/utility/guildinfo.js
@@ -16,21 +16,26 @@ module.exports = class GuildInfo extends Command {
     }, client)
   }
 
-  run ({ t, author, channel, language }, guild = channel.guild) {
+  async run ({ t, author, channel, language }, guild = channel.guild) {
+    if (guild.members.cache.size !== guild.memberCount) {
+      guild.members.fetch({ withPresences: true })
+    }
     const embed = new SwitchbladeEmbed(author)
     moment.locale(language)
     channel.startTyping()
     embed.setTitle(guild.name)
       .setThumbnail(guild.iconURL({ dynamic: true }) ? guild.iconURL({ dynamic: true }) : `https://guild-default-icon.herokuapp.com/${guild.nameAcronym}`)
       .addField(t('commands:guildinfo.id'), guild.id, true)
-      .addField(t('commands:guildinfo.owner'), guild.owner, true)
-      .addField(t('commands:guildinfo.region'), t(`regions:${guild.region}`), true)
+      .addField(t('commands:guildinfo.owner'), `<@${guild.ownerID}>`, true)
       .addField(t('commands:guildinfo.channels', { count: MiscUtils.formatNumber(guild.channels.cache.size, language) }), [
         t('commands:guildinfo.textChannels', { count: MiscUtils.formatNumber(guild.channels.cache.filter(g => g.type === 'text' || g.type === 'category').size, language) }),
         t('commands:guildinfo.voiceChannels', { count: MiscUtils.formatNumber(guild.channels.cache.filter(g => g.type === 'voice').size, language) })
       ].join('\n'), true)
       .addField(t('commands:guildinfo.createdAt'), `${moment(guild.createdTimestamp).format('LLL')}\n(${moment(guild.createdTimestamp).fromNow()})`, true)
       .addField(t('commands:guildinfo.joinedAt'), `${moment(guild.joinedTimestamp).format('LLL')}\n(${moment(guild.joinedTimestamp).fromNow()})`, true)
+      .addField(t('commands:guildinfo.serverBoost', { count: MiscUtils.formatNumber(guild.premiumSubscriptionCount, language) }), [
+        t('commands:guildinfo.level', { count: MiscUtils.formatNumber(guild.premiumTier, language) })
+      ].join('\n'), true)
       .addField(t('commands:guildinfo.members', { count: MiscUtils.formatNumber(guild.members.cache.size, language) }), [
         `${this.getEmoji('streaming')} ${t('commands:guildinfo.streaming', { count: MiscUtils.formatNumber(guild.members.cache.filter(m => m.game === 'streaming').size, language) })}`,
         `${this.getEmoji('online')} ${t('commands:guildinfo.online', { count: MiscUtils.formatNumber(guild.members.cache.filter(m => m.presence.status === 'online').size, language) })}`,
--- src/utils/PermissionUtils.js
+++ src/utils/PermissionUtils.js
@@ -10,8 +10,8 @@ module.exports = class PermissionUtils {
   }
 
   static specialRole (client, user) {
-    const botGuild = client.guilds.cache.get(process.env.BOT_GUILD)
-    const member = botGuild && botGuild.members.cache.get(user.id)
+    const botGuild = client.guilds.fetch(process.env.BOT_GUILD)
+    const member = botGuild && botGuild.members.fetch(user.id)
     if (member) {
       return member.roles.cache.filter(r => r.hoist).sort((a, b) => b.position - a.position).first()
     }
